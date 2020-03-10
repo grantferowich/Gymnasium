edabe0ee10aa82aa28a596e8b9ac2f48df802f0c
Farmwell <g.ferowich@gmail.com>
2020-03-09 22:01:47 -0400

    nth fib

--- RemoveDuplicatesFromArray.js
+++ RemoveDuplicatesFromArray.js
@@ -6,9 +6,9 @@
 
 // create a new set, pass in array, a set object only allows unique values, spread operator converts back to array
 
-const removeDuplicatesTwo = arr => {
-  let a = Array.from(new Set(arr));
-  return a;
-};
+// const removeDuplicatesTwo = arr => {
+//   let a = Array.from(new Set(arr));
+//   return a;
+// };
 
-console.log(removeDuplicatesTwo([1, 1, 2, 2, 3, 3, 4, 4]));
+// console.log(removeDuplicatesTwo([1, 1, 2, 2, 3, 3, 4, 4]));
