8b57c11b5135dc621d551dcca7ca67d228228a5a
Farmwell <g.ferowich@gmail.com>
2020-03-11 18:25:51 -0500

    nonDuplicate

--- RemoveDuplicatesFromArray.js
+++ RemoveDuplicatesFromArray.js
@@ -1,14 +1,14 @@
-// const removeDuplicates = arr => {
-//   return [...new Set(arr)];
-// };
+const removeDuplicates = arr => {
+  return [...new Set(arr)];
+};
 
 // console.log(removeDuplicates([1, 1, 2, 2, 3, 3]));
 
 // create a new set, pass in array, a set object only allows unique values, spread operator converts back to array
 
-// const removeDuplicatesTwo = arr => {
-//   let a = Array.from(new Set(arr));
-//   return a;
-// };
+const removeDuplicatesTwo = arr => {
+  let a = Array.from(new Set(arr));
+  return a;
+};
 
 // console.log(removeDuplicatesTwo([1, 1, 2, 2, 3, 3, 4, 4]));
---
+++ nonDuplicate.js
@@ -0,0 +1,14 @@
+const nonDuplicate = array => {
+  let inputObject = {};
+  array.forEach(index => {
+    if (inputObject[index]) {
+      inputObject[index] = inputObject[index] + 1;
+    } else {
+      inputObject[index] = 1;
+    }
+  });
+  let uniques = Object.keys(inputObject);
+  return uniques;
+};
+
+console.log(nonDuplicate([1, 1, 1, 3, 3, 4, 555, 5, 5, 5]));
