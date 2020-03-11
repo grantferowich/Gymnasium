e850cd15d7cbb1d40a54ca0a0198d2eb7955584f
Farmwell <g.ferowich@gmail.com>
2020-03-11 18:42:58 -0500

    memoization with nfib

--- Recursion.js
+++ Recursion.js
@@ -1,9 +1,16 @@
-const nFib = n => {
-  if (n <= 2) {
-    return 1;
-  } else {
-    return nFib(n - 1) + nFib(n - 2);
+const nFib = (n, prevValues = []) => {
+  if (prevValues[n] != null) {
+    return prevValues[n];
   }
+
+  let result;
+  if (n <= 2) {
+    result = 1;
+  } else {
+    result = nFib(n - 1, prevValues) + nFib(n - 2, prevValues);
+  }
+  prevValues[n] = result;
+  return result;
 };
 
 console.log(nFib(3));
@@ -11,3 +18,4 @@ console.log(nFib(4));
 console.log(nFib(5));
 console.log(nFib(6));
 console.log(nFib(7));
+console.log(nFib(41));
