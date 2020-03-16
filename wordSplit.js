7f68d6add58c5ff961d8b63d3265e5ca4d1c3e00
Farmwell <g.ferowich@gmail.com>
2020-03-16 12:48:58 -0500

    word split problem

---
+++ wordSplit.js
@@ -0,0 +1,19 @@
+const wordSplit = strArr => {
+  const targetWord = strArr[0];
+  const dictionary = strArr[1];
+  dictionary.split(",");
+
+  let i = 1;
+  while (i < targetWord.length) {
+    let partOne = targetWord.slice(0, i);
+    let partTwo = targetWord.slice(i);
+    if (dictionary.includes(partOne && partTwo)) {
+      return partOne + "," + partTwo;
+    }
+    i++;
+  }
+
+  return console.log("not possible");
+};
+
+console.log(wordSplit(["hellocat", "apple,bat,cat,goodbye,hello,why"]));
