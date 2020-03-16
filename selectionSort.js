3e64eada60e07bf3efae81249456b47b3dd503fd
Farmwell <g.ferowich@gmail.com>
2020-03-16 07:47:37 -0500

    selection sort implementation with two for loops

--- selectionSort.js
+++ selectionSort.js
@@ -5,23 +5,39 @@ const swap = (a, x, y) => {
   a[y] = temp;
 };
 
-const selectionSort = a => {
-  //start with the initial index in the unsorted array
-  let startIndex = 0;
-  while (startIndex < a.length - 1) {
-    let smallestIndex = startIndex;
-    for (let j = startIndex + 1; j < a.length; j++) {
-      if (a[smallestIndex] > a[j]) {
+// const selectionSort = a => {
+//   //start with the initial index in the unsorted array
+//   let startIndex = 0;
+//   while (startIndex < a.length - 1) {
+//     let smallestIndex = startIndex;
+//     for (let j = startIndex + 1; j < a.length; j++) {
+//       if (a[smallestIndex] > a[j]) {
+//         smallestIndex = j;
+//       }
+//     }
+//     if (startIndex !== smallestIndex) {
+//       swap(a, startIndex, smallestIndex);
+//     }
+//     startIndex++;
+//   }
+
+//   return a;
+// };
+
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let smallestIndex = i;
+    for (let j = smallestIndex + 1; j < arr.length; j++) {
+      if (arr[smallestIndex] > arr[j]) {
         smallestIndex = j;
       }
     }
-    if (startIndex !== smallestIndex) {
-      swap(a, startIndex, smallestIndex);
-    }
-    startIndex++;
-  }
 
-  return a;
+    if (smallestIndex !== i) {
+      swap(arr, i, smallestIndex);
+    }
+  }
+  return arr;
 };
 
 console.log(
