f06e80fb06e3569bd7aa2d35ce403a0343fbf311
Farmwell <g.ferowich@gmail.com>
2020-03-17 15:09:53 -0500

    summation sequence in js

---
+++ summationSequence.js
@@ -0,0 +1,24 @@
+const summation = n => {
+  let sum = 0;
+  let i = 1;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+const summationSequence = (start, length) => {
+  let arr = [start];
+  let i = 0;
+  let sum = 0;
+
+  while (arr.length < length) {
+    let last = arr.length - 1;
+    arr.push(summation(arr[last]));
+  }
+
+  return arr;
+};
+
+console.log(summationSequence(3, 4)); // => [3, 6, 21, 231]
+console.log(summationSequence(5, 3)); // => [5, 15, 120])
