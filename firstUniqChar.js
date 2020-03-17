d528505f6fcd1022fd2f5c3851bf592c6ef0904e
Farmwell <g.ferowich@gmail.com>
2020-03-17 11:43:35 -0500

    firstUniqChar in a string

---
+++ firstUniqChar.js
@@ -0,0 +1,18 @@
+const firstUniqChar = s => {
+  let currentIdx = 0;
+
+  while (currentIdx < s.length) {
+    for (let i = 1; i < s.length; i++) {
+      remainder = s.slice(i);
+      if (!remainder.includes(s[currentIdx])) {
+        return currentIdx;
+      } else currentIdx++;
+    }
+    return -1;
+  }
+};
+
+console.log(firstUniqChar("leetcode")); //=> 0
+console.log(firstUniqChar("loveleetcode")); //=> 2
+console.log(firstUniqChar("aaaaaaaa")); //=> -1
+console.log(firstUniqChar("aaaaaaaaaaab")); //=> 10
