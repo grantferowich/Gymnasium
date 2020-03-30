ec7051bd9c47c6b1b28ad17f7e426de4d105a115
Farmwell <g.ferowich@gmail.com>
2020-03-30 09:19:14 -0500

    oop basic refresher

--- oop/index.js
+++ oop/index.js
@@ -1,3 +1,31 @@
+// 4 Pillars of Object Oriented Programming
+// 	Encapsulation
+// 	Abstraction
+// 	Inheritance
+// 	Polymorphism
+
+// FROM procedural programming (spaghetti code) -> OOP
+
+// What is encapsulation?
+// 1 Variables - properties of an Object
+// 	2 Functions - methods of an Object
+// 	3 Fewer parameters in functions thanks to OOP
+// 	4 group relatedness together to reduce complexity
+
+// What is abstraction?
+// 	Hide some properties (private methods) and metas from the outside
+// 	Simplify interface of Object
+// 	Reduce the impact of changing code
+
+// What is inheritance?
+// 	Kill redundant code
+
+// What is polymorphism?
+// 	Poly - many; morph - form
+// 	Refactor ugly switch case statements
+
+// Member functions of a class are methods. A property holds values, while a method defines some logic.
+
 // const circle = {
 //   radius: 1,
 //   location: {
