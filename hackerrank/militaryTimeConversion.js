2ad4104a087d66a9f6af210123fab404f82eaf10
Grant <g.ferowich@gmail.com>
2022-11-25 09:01:56 -0500

    psuedocoded the solution for militaryTimeConversion

--- hackerrank/militaryTimeConversion.js
+++ hackerrank/militaryTimeConversion.js
@@ -2,6 +2,11 @@
 // Expect 1:00:00AM to return 13:00:00
 // Expect 07:30:00AM to return 07:30:00
 // Expect 7:30:00PM to return 19:30:00
+
+//Solution 1: Ascertain the pm or am value. 
+//Create two possible scenarios for returning based on whether period is am or pm
+//For the am period return the hour value as is unless the hour value is 12, in which case 00 must be returned
+//For the pm perioud add 12 to the hour value, leaving minutes and seconds as is 
  
 const militaryTimeConversion = (s) => {
     let period = (s[s.length-2]+s[s.length-1]).toLowerCase();
