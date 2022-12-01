92ba7b607295dbc32a90b5fb840f3e6a6deb32e8
Grant <g.ferowich@gmail.com>
2022-12-01 12:04:25 -0600

    implemented five recursion steps to solve for the number of bit arrays from a given number

---
+++ outco/closestValue.js
@@ -0,0 +1,40 @@
+
+/*
+ * ## Closest Value
+ *
+ * *Given a sorted bit array of integers, and a target value, find the number in the array that is closest to the target.*
+ *
+ * **Parameters**
+ * Input: arr {Array of Integers}
+ * Input: target {Integer}
+ * Output: {Integer}
+ *
+ * **Constraints**
+ * If there are two numbers tied for the closest value, return the lowest value.
+ *
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `[1, 2, 3, 5, 5, 7, 9, 10, 11], 6 --> 5`
+ * `[1, 2, 3], 8 --> 3`
+ * `[1, 10, 22, 59, 67, 72, 100], 70 --> 72`
+ */
+
+
+
+// initialize a startIdx and endIdx 
+// pick the middle idx as the midpoint of the array
+// if the difference between the target and the element at the middle index is positive 
+// set the startIdx to middle idx + 1
+// set the difference as the result of the target number less the element at the middle index
+// if the array at the midddle idx is equal to the number return 0 
+
+
+function closestValue(arr, target) {
+
+
+  
+    
+
+}
---
+++ outco/nonConsecutiveOnes.js
@@ -0,0 +1,68 @@
+//NonConsecutiveOnes.js
+// given a number, n, return the number of bit strings
+// of n length that DO NOT contain consecutive 1's
+        //  (build, depth)
+//               ""          [0]
+//            /      \
+// .         0        1       [1]
+//          / \      / \       
+//         00 01    10 11      [2]
+
+
+// initialize state variables: array of strings
+// return state variables: qrray of strings
+// define and invoke recursive helper method
+// base case(s)
+// recursive case(s)
+
+// base case: ("", 0)
+
+// recursive case: 
+// left 
+// build + "0"
+
+// right
+// build + "1" 
+
+// increment count if the build does not include "11"
+
+// Input: 2
+// Output: ["00", "01", "10"]
+
+// Input: 3
+// Output: ["000", "001", "010", "100", "101"]
+
+const nonConsecutiveOnes = (number) => {
+    if (typeof number !== "number"){
+        return 
+    }
+    let result = [];
+    const findCombinations = (build, depth) => {
+        // terminate recursion
+        if (depth > number){
+            return 
+        }   
+
+        
+        if (!build.includes("11")){
+            if (build.length === number){
+                result.push(build)
+            }           
+        }   
+      
+        //right
+        findCombinations(build + "1", depth + 1);
+        //left
+        findCombinations(build + "0", depth + 1);
+    }
+    
+    findCombinations("", 0);
+    return console.log(result)
+}
+
+nonConsecutiveOnes(2) // ["00", "01", "10"]
+nonConsecutiveOnes(3) // ["000", "001", "010", "100", "101"]
+
+
+
+
