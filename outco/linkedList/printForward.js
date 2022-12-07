6cd0763be1ba7aa7454de4093d45629631ecaf57
Grant <g.ferowich@gmail.com>
2022-12-07 13:51:48 -0600

    defined and tested printForward.js

---
+++ outco/linkedList/printForward.js
@@ -0,0 +1,36 @@
+/*
+Input:
+4
+1
+5
+7
+10
+
+Output:
+1
+5
+7
+10
+
+*/
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function printForward(node) {
+    let current = node;
+    
+    while (current){
+        console.log(current.data);
+        current = current.next;
+    }
+
+};
+
