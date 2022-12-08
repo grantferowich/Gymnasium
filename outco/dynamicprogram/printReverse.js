f9af0e0c1bd663eb56f2a3a74949abd2585e9906
Grant <g.ferowich@gmail.com>
2022-12-08 16:34:51 -0600

    defined second solution for printing an array in reverse

--- outco/dynamicprogram/printReverse.js
+++ outco/dynamicprogram/printReverse.js
@@ -12,6 +12,26 @@ const printReverse = (arr) => {
     traverse(0);
 }
 
-printReverse([1,2,3])
-printReverse([])
-printReverse([9])
+// printReverse([1,2,3])
+// printReverse([])
+// printReverse([9])
+
+
+// here is another way to approach the solution
+
+const printReverse2 = (arr) => {
+
+    const traverse = (depth) => {
+        if (depth < 0){
+            return;
+        }
+        console.log(arr[depth]);
+        traverse(depth - 1);
+    }
+    
+    traverse(arr.length -1)
+}
+
+printReverse2([1,2,3])
+printReverse2([])
+printReverse2([9])
