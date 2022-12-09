ae7d191d6d9db6595b6752a6ee460f6df511f027
Grant <g.ferowich@gmail.com>
2022-12-09 13:57:01 -0600

    defined flattened and implemented isArray

---
+++ outco/dynamicProgramming/recursion/flatten.js
@@ -0,0 +1,28 @@
+function flatten(array){
+    let flattened = [];
+  
+    const makeFlat = (subarray) => {
+        // if the type of the element is an array itself
+        // make recursive call
+      for (let x = 0; x < subarray.length; x++){
+        if (Array.isArray(subarray[x])){
+            makeFlat(subarray[x]);
+        } else {
+            flattened.push(subarray[x])
+        }
+      }
+  
+    }  
+    makeFlat(array);
+    return flattened;
+}
+
+console.log(flatten([1,[2,3,[4]]])) // [1,2,3,4]
+
+
+// let x = [1,2,3]
+// let y = {name: "Grant"}
+// let z = 
+// console.log(typeof x)
+// console.log(Array.isArray(x)) //true 
+// console.log(typeof y)
