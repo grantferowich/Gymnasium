bbd9fa552e49103e5fac5e0d78b51110856e4b27
Grant <g.ferowich@gmail.com>
2022-12-10 12:26:54 -0600

    appended testing status note to rotateArray.js

--- rotateArray.js
+++ rotateArray.js
@@ -12,4 +12,25 @@ const rotateArray = (arr, k) => {
   return arr;
 };
 
-console.log(rotateArray([2, 3, 4, 5, 6, 7, 8], 3));
+console.log(rotateArray([2, 3, 4, 5, 6, 7, 8], 3));  // [ 6, 7, 8, 2, 3, 4, 5]
+
+// input: array, k
+// output: array
+
+// return the array rotated k positions [1,2,3], k=1 => [3,2,1]
+// constraints: 
+// edge cases: 
+ 
+// Successfully tested the solution on 12/10/22
+const rotateArray2 = (array, k) => {
+  let x = 0;
+  while ( x < k){
+    // (removal) store the last element popped off the end of the array
+    let element = array.pop();
+    // (insertion) append the element to the front of the array
+    array.unshift(element) 
+    x++
+  }
+  return array
+}
+console.log(rotateArray2([2, 3, 4, 5, 6, 7, 8], 3)); 
