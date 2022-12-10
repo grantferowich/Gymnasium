6ef971e4046fd2285fecbea36082dc583416bc41
Grant <g.ferowich@gmail.com>
2022-12-10 15:42:29 -0600

    appended test status of pigLatin.js

--- appAcademy/pigLatin.js
+++ appAcademy/pigLatin.js
@@ -6,17 +6,28 @@ var t0 = performance.now();
 // - for words that start with a nonvowel,
 // move all letters before the first vowel to the end of the word and add 'ay'
 
+// input: a word
+// output: a word 
+// constraints: O(N) run time, O(N) space complexity
+// edge cases: return "" for empty strings
+
+// Succesfully tested the solution on 12/10/22.
 const pigLatin = (word) => {
 
+  if (word.length === 0) {
+    return ""
+  }
+
   let arr = word.split('')
+
+  // if the array at index 0 is a vowel
   if (arr[0] == "a" || arr[0] =="e"|| arr[0] == "i" || arr[0] == "o" || arr[0] == "u"){
     return word + "yay"
   } 
 
-  // find index of first vowel 
-  // slice the array from 0 up to but not including the index of the first vowell
-  // append the slice to the end of the word
-  // append "ay" to the end of that word
+
+
+  // loop until a vowel is found at index i
 
   for (let i = 0; i < arr.length; i++) {
         if (arr[i] == "a" || arr[i] =="e"|| arr[i] == "i" || arr[i] == "o" || arr[i] == "u") {
@@ -28,6 +39,7 @@ const pigLatin = (word) => {
 };
 
 console.log(pigLatin("apple")); // => "appleyay"
+console.log(pigLatin("")); // => ""
 console.log(pigLatin("eat")); // => "eatyay"
 console.log(pigLatin("banana")); // => "ananabay"
 console.log(pigLatin("trash")); // => "ashtray"
