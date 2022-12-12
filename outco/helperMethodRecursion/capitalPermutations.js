d5b1422322cc2449df380e7e7ba6c11bbc7bef0d
Grant <g.ferowich@gmail.com>
2022-12-12 16:57:28 -0600

    stated capital permutations problem

---
+++ outco/helperMethodRecursion/capitalPermutations.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * Capital Permutations
+ * 
+ * Given a string str of lowercase alphabetical characters, return the set of all permutations of those characters in upper AND lowercase.
+ * Advanced
+ * Solve the same problem, except now you may have number characters in your string (which don't have a lowercase or uppercase, but should still be included in your result) and capital letters, that need to be lowercased.
+ * 
+ * 
+ * Input: "abc"
+ * Output: ["ABC", "ABc", "AbC", "aBC", "Abc", "aBc", "abC", "abc"]
+ * 
+ * Advanced:
+ * Input: "A1d3"
+ * Output: ["A1D3", "a1D3", "A1d3", "a1d3"]
+ * 
+ * Time Constraints: O(2^N) (exponential)
+ * Space Constraints: O(2^N) 
+ * 
+ *
+ * 
+ */
+
+const capitalPermutations = (string) => {
+
+    let permutations = []
+    const generatePermutations = (str, depth) => {
+
+        
+    
+    }
+
+    generatePermutations("", 0);
+    return permutations; 
+}
