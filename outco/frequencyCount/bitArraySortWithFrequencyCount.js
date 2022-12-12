6a33f55caf6cfc4afb185d23972b41fc49917b4a
Grant <g.ferowich@gmail.com>
2022-12-12 10:42:09 -0600

    stated bit array sort problem

---
+++ outco/frequencyCount/bitArraySortWithFrequencyCount.js
@@ -0,0 +1,20 @@
+/**
+ * Challenge 1: Sort a Bit Array
+ * Given a bit array, return it sorted in-place (a bit array is simply an array that contains only bits, either 0 or 1).
+ * 
+ * See if you can solve this in O(N) time and O(1) auxiliary space.
+ * Try to solve this using a frequency count rather than using multiple pointers, or using a comparison sort function.
+ * Input : [0, 1, 1, 0, 1, 1, 1, 0]
+ * Output : [0, 0, 0, 1, 1, 1, 1, 1]
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+const bitArraySortWithFrequencyCount = (array) => {
+
+}
