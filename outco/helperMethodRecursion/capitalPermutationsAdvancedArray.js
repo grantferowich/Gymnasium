1184f1599ac91d27bb73b84e85274c1ab997f01d
Grant <g.ferowich@gmail.com>
2022-12-13 12:34:13 -0600

    stated problem

--- outco/helperMethodRecursion/capitalPermutationsAdvancedArray.js
+++ outco/helperMethodRecursion/capitalPermutationsAdvancedArray.js
@@ -1,3 +1,27 @@
+/**
+ * 
+ * Capital Permutations - Advanced 
+ * 
+ * Given a string str of lowercase alphabetical characters, return the set of all permutations of those characters in upper AND lowercase.
+ * Advanced
+ * Solve the same problem, except now you may have number characters in your string (which don't have a lowercase or uppercase, but should still be included in your result) and capital letters, that need to be lowercased.
+ * 
+ * Advanced:
+ * Input: "A1d3"
+ * Output: ["A1D3", "a1D3", "A1d3", "a1d3"]
+ * 
+ * Time Constraints: O(2^N) (exponential)
+ * Space Constraints: O(2^N) 
+ * 
+ * Diagram: build from bottom up 
+ * 
+ *                   ""  0   
+ *            /                  \
+ *          "a"                   "A"  1   
+ *        /     \             /           \
+ *     "ab"     "aB"       "Ab"           "AB"   2
+ *     /  \      /  \      /    \         /   \
+ * "abc" "abC" "aBc" "aBC" "Abc" "AbC" "ABc" "ABC"   3
 // successfully tested capitalPermutationsAdvancedArray on 12/13/22
 
 
