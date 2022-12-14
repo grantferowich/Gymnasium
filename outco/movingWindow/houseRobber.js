7e25af7504cb2d6bfbcb454d90fa0e17829900a9
Grant <g.ferowich@gmail.com>
2022-12-14 16:16:39 -0600

    devised solution for houseRobber and passed two test cases

--- outco/movingWindow/houseRobber.js
+++ outco/movingWindow/houseRobber.js
@@ -1,5 +1,5 @@
 /**
- * 198 House Robber
+ * 198 House Robber 
  * 
  * You are a professional robber planning to rob houses along a street.
  * Each house has a certain amount of money stashed, the only constraint stopping you from robbing each
@@ -14,18 +14,32 @@
  * input: [2, 7, 9, 3,1]
  * output: 12. Rob 2 + 9 +1.
  * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ * Successfully passed two test cases 12/14/22. 
  * 
  */
+
+const houseRobber = (array) => {
+
+    let local = 0;
+    let ultimate = 0;
+    let right = 0;
+    let currentIdx = 0;
+
+    while (right < array.length){
+        let value = array[right];
+        local = value;
+        for (let x = 0; x < array.length; x++){
+            if (x - right > 1 && x - currentIdx > 1){
+                local += array[x];
+                currentIdx = x;
+            }
+        }
+        ultimate = Math.max(local, ultimate);
+        right++
+    }
+
+    return ultimate
+}
+
+console.log(houseRobber([1,2,3,1])) // 4
+console.log(houseRobber([2, 7, 9, 3,1])) // 12
