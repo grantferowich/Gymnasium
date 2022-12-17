a118d8f3c62a8babd65f7c19bafd7bddd7279509
Grant <g.ferowich@gmail.com>
2022-12-17 14:56:45 -0600

    initialized linked list class

---
+++ outco/linkedList/classLinkedList.js
@@ -0,0 +1,74 @@
+class ListNode {
+    constructor(value = null) {
+     this.value = value
+     this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+  // Time Complexity: O(1)
+  // Auxiliary Space Complexity: O(1)
+    append(value){
+    this.insert(value, this.length)
+    }
+
+  // Time Complexity: O(N)
+  // Auxiliary Space Complexity: O(N)
+    insert(value, index){
+    
+    // check index input is valid
+    if ( index < 0 || index > this.length){
+      return
+    }
+
+    // instantiante the new node
+    let newNode = new ListNode(value);
+
+    if (this.length === 0){
+      // do stuff when the list is empty
+      this.head = newNode;
+      this.tail = newNode
+    } else if (index === 0){
+      // special case when insertion location is the start of the list 
+      newNode.next = this.head;
+      this.head = newNode 
+    } else if (index === this.length){
+      // special case when insertion locattion is the end of the list
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      // do stuff when the insertion location is anywhere else
+      // init reference 
+      let prev = this.head;
+      for (let x = 0; x < index - 1; x++){
+        // roll the reference continuously until reaching the insertion index
+        prev = prev.next;
+      }
+      // move the newNode's pointer to the next node
+      newNode.next = prev.next;
+      prev.next = newNode;
+    }
+    this.length++
+    }
+}
+
+
+
+
+
+
+// let x = new LinkedList()
+// console.log(x) // Expected Output:  LinkedList { length: 0, head: null, tail: null }
+
+let y = new LinkedList()
+y.head = 4;
+y.tail = 8
+console.log(y)
+y.insert(1,0);
+console.log(y)
--- outco/linkedList/classListNode.js
+++ outco/linkedList/classListNode.js
@@ -1,12 +1,13 @@
-class ListNode {
-    constructor(value = null) {
-     this.value = value
-     this.next = null;
-    }
+export default class ListNode {
+  constructor(value = null) {
+   this.value = value
+   this.next = null;
   }
+}
 
 let x = new ListNode()
 console.log(x) // ListNode { value: null, next: null}
 
 let y = new ListNode(4)
 console.log(y) // ListNode { value: 4, next: null}
+
