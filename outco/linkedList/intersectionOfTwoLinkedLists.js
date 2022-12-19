ab260308262e6dbc5a8b7c074a5dca9b1e928da0
Grant <g.ferowich@gmail.com>
2022-12-19 08:12:30 -0600

    implemented an O(N) space complexity solution for intersection of two linked lists

--- outco/linkedList/intersectionOfTwoLinkedLists.js
+++ outco/linkedList/intersectionOfTwoLinkedLists.js
@@ -5,9 +5,9 @@ If there is no intersection between the two nodes, simply return null.
 
 Input:  headA {ListNode}, headB {ListNode}
 Output: {ListNode} or null
+
 Example
 Input: 	a1, b1
-
 
 A:          a1 → a2
                    ↘
@@ -29,6 +29,21 @@ B:     b1 → b2 → b3
 
 
 Output: null
+
+Input: a1, b1
+
+
+A:  a1   a3
+      \ /
+      c2
+      / \
+B:  b1  b3
+
+output: true
+the two linked lists share a node 
+
+
+
 Constraints
 Time Complexity: O(N)
 
@@ -36,7 +51,7 @@ Auxiliary Space Complexity: O(1)
 
 
 The approach 
-loop and check
+loop through each element of the first linked list
 
 */
 
@@ -49,12 +64,47 @@ class ListNode {
 }
   
 let head = new ListNode(1);
-head.next = new ListNode(5);
-head.next.next = new ListNode(7);
-head.next.next.next = new ListNode(10);
-head.next.next.next.next = new ListNode(9);
+head.next = new ListNode(1);
+head.next.next = new ListNode(12);
+head.next.next.next = new ListNode(1);
+// head.next.next.next.next = new ListNode(1);
+
+let head2 = new ListNode(0);
+head2.next = new ListNode(5);
+head2.next.next = new ListNode(12);
+head2.next.next.next = new ListNode(1);
+// head2.next.next.next.next = new ListNode(1);
+
+let head3 = new ListNode(99);
+head3.next = new ListNode(21);
+head3.next.next = new ListNode(43);
+head3.next.next.next = new ListNode(19);
+head3.next.next.next.next = new ListNode(100);
 
 const intersection = (a,b) => {
 
+    let currentA = a;
+    let currentB = b;
+    let hashA = {};
+    // store the node values of linked list A in a hash
+    while (currentA !== null ){
+            
+            hashA[currentA.val] = hashA[currentA.val] + 1 || 1;
+            currentA = currentA.next;
+    }
+
+    // scan each node in currentB to return true when there are instances of intersection
+    while (currentB !== null){
+        if (hashA[currentB.val] !== undefined){
+            console.log('found intersection at node: ', currentB.val)
+            return true
+        }
+        currentB = currentB.next
+    }   
+    console.log('the function did not find any instersecting nodes')
+    return false
 }
+
+console.log(intersection(head, head2)) // expected output: true
+console.log(intersection(head,head3)) // expected output: false
 
