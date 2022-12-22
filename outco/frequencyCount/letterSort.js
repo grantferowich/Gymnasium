d1f768f97673ffcf70d3a67d6b216a5d49baeb54
Grant <g.ferowich@gmail.com>
2022-12-22 14:27:57 -0600

    implemented O(N) solution for letterSort

--- outco/frequencyCount/letterSort.js
+++ outco/frequencyCount/letterSort.js
@@ -13,6 +13,9 @@
 * `hello --> ehllo`
 * `whiteboard --> abdehiortw`
 * `one --> eno`
+
+The solution I constructed is an N(log(N)) solution since the solution
+uses the .sort() function. 
 */
 
 function letterSort(string) {
@@ -21,5 +24,33 @@ function letterSort(string) {
  return sortedArray.join("");
 }
 
-console.log(letterSort("hello"))
-console.log(letterSort("grantjosephfrancisferowich"))
+// console.log(letterSort("hello"))
+// console.log(letterSort("grantjosephfrancisferowich"))
+
+// input: a string
+// output: sorted string
+
+
+
+// create stati array with 26 elements
+// find the base 26 location by subtracting 97 from the unicode value
+// store k-v pairs with occurrences as values
+
+const letterSort2 = (string) => {
+
+    let stringChars = string.toLowerCase();
+    let characters = new Array(26).fill(0);
+    let returnWord = "";
+
+    //generate hash with char code value as key and visits as value in k-v pair 
+    for (let i = 0; i < stringChars.length; i++){
+        characters[string[i].charCodeAt() - 97] += 1;
+    } 
+    for (let x = 0; x < characters.length ; x++){
+        returnWord += String.fromCharCode(x+97).repeat((characters[x]));
+    }
+    return returnWord;
+}
+
+
+console.log(letterSort2('hello'))
