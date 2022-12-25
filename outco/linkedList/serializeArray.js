712c49cd54c2c67278332c6f479654801037e5c7
Grant <g.ferowich@gmail.com>
2022-12-25 11:43:39 -0600

    defined and successfully tested serializeArray function

--- outco/linkedList/serializeArray.js
+++ outco/linkedList/serializeArray.js
@@ -1,9 +1,27 @@
 // Write a function which takes in an array and returns a linked list
 
+// Successfully tested the function on 12/25/2022.
+
 class ListNode {
     constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
+// [1,2,3] ==> 1 -> 2 -> 3
 
+const serializeArray = (array) =>{
+    if (array.length === 0){
+        return
+    }
+    let value = array[0];
+    let head = new ListNode(value);
+    let current = head;
+    for (let x = 1; x < array.length; x++){
+        current.next = new ListNode(array[x]);
+        current = current.next;
+    }
+    return head
+}
+
+console.log(serializeArray([1,2,3]))
