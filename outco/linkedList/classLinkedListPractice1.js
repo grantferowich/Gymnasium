21329c83f2a35416ae7e9a970ef87f37d9f33de1
Grant <g.ferowich@gmail.com>
2022-12-27 12:17:21 -0600

    defined delete

--- outco/linkedList/classLinkedListPractice1.js
+++ outco/linkedList/classLinkedListPractice1.js
@@ -13,13 +13,14 @@ class LinkedList{
     }
 
     append(value){
-
+        this.insert(value, this.length);
     }
 
     // special insertion cases
     // linked list is unpopulated with nodes => head and tail must change
     // insert at start => head must change
     // insert at end => tail must change
+    // insert in the middle => pointer of previous node must change, new node must point to relevant node
     insert(value, index){
         if (index < 0 || index > this.length){
             return;
@@ -39,9 +40,19 @@ class LinkedList{
             this.tail.next = newNode;
             this.tail = newNode
         } else {
-            
+            let prev = this.head;
+            for (let x = 0; x < index - 1; x++){
+                prev = prev.next;
+            }
+            newNode.next = prev.next;
+            prev.next = newNode
         }
+    }
 
+    delete(index){
+        if ( index < 0 || index >= this.length){
+            return;
+        }
 
     }
 }
