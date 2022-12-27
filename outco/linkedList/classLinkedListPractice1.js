29b27e8600c2f91edb684aa69c5d90d561004ee5
Grant <g.ferowich@gmail.com>
2022-12-27 10:30:49 -0600

    updated insertion for start and end of the linked list

--- outco/linkedList/classLinkedListPractice1.js
+++ outco/linkedList/classLinkedListPractice1.js
@@ -17,6 +17,7 @@ class LinkedList{
     }
 
     // special insertion cases
+    // linked list is unpopulated with nodes => head and tail must change
     // insert at start => head must change
     // insert at end => tail must change
     insert(value, index){
@@ -25,11 +26,21 @@ class LinkedList{
         }
 
         let newNode = new ListNode(value)
+        
         if (this.length === 0){
             this.head = newNode;
             this.tail = newNode;
             this.length++
-        } else if ( index)
+        } else if (index === 0){
+            newNode.next = this.head;
+            this.head = newNode;
+            this.length++;
+        } else if (index === this.length){
+            this.tail.next = newNode;
+            this.tail = newNode
+        } else {
+            
+        }
 
 
     }
