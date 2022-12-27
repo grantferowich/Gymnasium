3800c1834939541995ec0d281649e9ad9df1c521
Grant <g.ferowich@gmail.com>
2022-12-27 14:38:53 -0600

    defined treeBFS

--- outco/binarytree/bfsTraversal.js
+++ outco/binarytree/bfsTraversal.js
@@ -11,6 +11,33 @@
  * 
  * Steps
  * set current to queue.dequeue
- * push current children to queue
+ * push current children to queue (enqueue node.left and node.right)
  * push current value to result array
  *  */  
+
+
+const treeBFS = (root) => {
+    const queue = [];
+    const result = [];
+    queue.push(root);
+
+    if (root === null){
+        return result;
+    }
+
+    while (queue.length > 0){
+
+        let current = queue.shift()
+        result.push(current.value)
+
+        if (current.left !== null){
+            queue.push(current.left)
+        }
+        
+        if (current.right !== null){
+            queue.push(current.right)
+        }   
+    }
+    
+    return result;
+}
