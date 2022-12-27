521a9d4970219f830c28fde790ae4226863d4951
Grant <g.ferowich@gmail.com>
2022-12-27 12:42:44 -0600

    defined deletion for linked list

--- outco/linkedList/classLinkedListPractice1.js
+++ outco/linkedList/classLinkedListPractice1.js
@@ -49,10 +49,50 @@ class LinkedList{
         }
     }
 
+    // if there is one node => set head and tail to null
+    // if the first element is deleted => re-set the head node
+    // if the last element is deleted => re-set the tail node
+    // 
+    
+    //length = 4
+    // delete (1)
+    // change the pointer of prev
+    // set 1's pointer to null
+
+    //prev
+    // 0    1    2    3
+    // 1 -> 3 -> 5 -> 7
+
     delete(index){
         if ( index < 0 || index >= this.length){
             return;
         }
+
+        if (this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else if (index === 0){
+            this.head = this.head.next;
+        } else if (index === this.length - 1){
+            let prev = this.head;
+            for (let x = 0; x < this.length - 1; x++){
+                prev = prev.next;
+            }
+            this.tail = prev;
+            prev.next = null;
+        } else {
+            let prev = this.head;
+            for (let y = 0; y < index - 1; y++){
+                prev = prev.next;
+            }
+            prev.next = prev.next.next;
+            if (index === this.length -1){
+                this.tail = prev;
+            }
+        }
+
+
+        this.length--;
 
     }
 }
