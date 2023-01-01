21c3b32619db016ef09564543e3f523d90b4f422
Grant <g.ferowich@gmail.com>
2022-12-31 21:56:22 -0600

    implemented a stack with a singly linked list class and singly linked list node class

---
+++ outco/linkedList/linkedListToStack.js
@@ -0,0 +1,80 @@
+// stack follows last in first out
+// insertion occurs at the tail
+// deletion occurs at the tail
+
+class SinglyLinkedListNode{
+    constructor(value){
+        this.value = value === undefined ? null : value;
+        this.next = null
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0; 
+    }
+    
+    addToTail(value){
+        let xNode = new SinglyLinkedListNode(value);
+        if (this.length === 0){
+            this.head = xNode;
+            this.tail = xNode;
+        } 
+        if (this.length === 1){
+            this.head.next = xNode;
+            this.tail = xNode;
+        }
+        if (this.length > 1){
+            this.tail.next = xNode;
+            this.tail = xNode;
+        }
+        this.length++;
+    }
+    removeFromTail(){
+        if (this.length === 0){
+            return
+        }
+        if (this.length === 1){
+            let deleted = this.head;
+            this.head = null;
+            this.tail = null;
+            return deleted; 
+        }
+        if (this.length > 1){
+            let prev = this.head;
+            let deleted = this.tail;
+            for (let x = 0; x < this.length - 1; x++){
+                prev = prev.next;
+            }
+            this.tail = prev;
+            prev.next = null;
+            return deleted;
+        }
+    }
+}
+
+class Stack{
+    constructor(){
+        this.linkedList = new SinglyLinkedList()
+    }
+    push(value){
+        this.linkedList.addToTail(value)
+    }
+    pop(){
+        let node = this.linkedList.removeFromTail();
+        return node;
+    }
+}
+
+let s = new Stack()
+s.push(1)
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(5)
+s.push(8)
+s.push(13)
+s.push(21)
+console.log(s.pop()) // 21
--- outco/linkedList/validParentheses.js
+++ outco/linkedList/validParentheses.js
@@ -45,19 +45,19 @@ const validParentheses = (string) => {
     let stack = [];
     
     for (let x = 0; x < string.length; x++){
-        let symbol = string[x]
+        let symbol = string[x];
         // if the symbol is in the stack already,
         // then remove the instance of the symbol from the stack       
         if (symbol === "(" || symbol === "[" || symbol === "{"){
             stack.push(symbol)
         } else if  (symbol === ")" && stack.length !== 0 && stack[stack.length - 1] === "("){
-            stack.pop()
+            stack.pop();
         } else if (symbol === "]" && stack.length !== 0 && stack[stack.length - 1]=== "["){
-            stack.pop()
+            stack.pop();
         } else if (symbol === "}" && stack.length !== 0 && stack[stack.length - 1]=== "{"){
-            stack.pop()
+            stack.pop();
         } else {
-            return false
+            return false;
         }
     }
     return stack.length === 0
