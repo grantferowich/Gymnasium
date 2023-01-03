808bdb7d7c6fab7d5ace4ff0db35e1d5016eaa84
Grant <g.ferowich@gmail.com>
2023-01-02 21:26:41 -0600

    described methods required to support get and put methods for the lru cache

--- outco/linkedList/LRUCache.js
+++ outco/linkedList/LRUCache.js
@@ -17,11 +17,17 @@ capacity: 2
 
 // LRU Cache is a least recently used cache.
 The LRU Cache interface must support get and put operations in O(1) time.
- */
+To support the get and put methods 
+implement: 
+- remove
+- moveToHead
+- addToHead(node)
+- removeTail() 
+*/
 class DoublyLinkedListNode{
     constructor(key, value){
         this.key = key === undefined ? null : key;
-        this.value = value === undefined ? null : valu;
+        this.value = value === undefined ? null : value;
         this.next = null;
         this.prev = null;
     }
@@ -48,5 +54,45 @@ class LRUCache{
         // tell the interface this key WAS recently used
         this.moveToHead(node)
         return node.value;
+    }
+
+    put(key, value){
+        if (this.map.has(key)){
+            // retrieve key already extant in the map
+            const node = this.map.get(key);
+            node.value = value;
+        }
+        const xNode = new DoublyLinkedListNode(key, value);
+        this.map.set(key, xNode);
+        this.addToHead(xNode);
+        this.size++;
+
+        if (this.size > this.capacity){
+            this.removeTail();
+        }
+    }
+
+    remove(node){
+        this.map.delete(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        this.size--;
+    }
+
+    moveToHead(node){
+        this.remove(node);
+        this.addToHead(node);
+    }
+
+    addToHead(node){
+        node.prev = this.head;
+        node.next = this.head.next;
+        this.head.next.prev = node;
+        this.head.next = node;
+    }
+
+    removeTail(){
+        const tail = this.tail.prev;
+        this.remove(tail)
     }
 }
