6cec26a3a95219ccb97be74075af78ba2e28437c
Grant <g.ferowich@gmail.com>
2023-01-03 22:38:38 -0600

    defined bst insertion method

--- outco/trees/binarySearchTreeClass.js
+++ outco/trees/binarySearchTreeClass.js
@@ -6,17 +6,44 @@ class TreeNode{
     }
 }
 
+// instructions: insert 5
+//              insert 3
+//              
+//              1 - root
+//             / \ 
+//               5
+//
+
 class BinarySearchTree{
     constructor(){
         this.root = null;
         this.size = 0;
     }
 
+    // input: value
+    // output: no return value
     insert(value){
         let xNode = new TreeNode(value)
-        if (this.size === 0){
+        if (this.root === null){
             this.root = xNode;
+            this.size++;
+            return;
         }
-        
+
+        let parent = null;
+        let child = this.root;
+        // traverse subtree
+        while (child){
+            parent = child;
+            child = parent.value > value ? parent.left : parent.right;
+        }
+        // insert leaf
+        if (value < parent.value){
+            parent.left = xNode;
+        } 
+        if (value > parent.value){
+            parent.right = xNode;
+        }
+        this.size++;
     }
 }
