f9fbab8e3ebbf7de6c6161fa0c728486789b7272
Grant <g.ferowich@gmail.com>
2023-01-07 08:11:33 -0600

    successfully tested two inputs with graphIsATree method

--- outco/graphs/graphIsATree.js
+++ outco/graphs/graphIsATree.js
@@ -1,4 +1,4 @@
-
+// Successfully tested the function for the first time 1/7/23.
 // graph is a tree? 
 // A treee is a graph
 // a tree is acyclic
@@ -25,11 +25,62 @@ This is an unconnected graph.
 // By checking left and right nodes, starting from the root node,
 // all nodes must be processed. 
 // There cannot be any nodes leftover after passing all nodes through a queue. 
+// 
+
+
+class TreeNode{
+    constructor(value){
+        this.value = value === undefined ? null : value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+let graph3 =  {
+    0 : [1, 2, 3],
+    1 : [0],
+    2 : [0],
+    3 : [0],
+    4 : []
+  };
+// expected output: false
+
+let graph2 =
+ {
+   0 : [1, 2, 3],
+   1 : [0, 2],
+   2 : [0, 1],
+   3 : [0, 4],
+   4 : [3]
+ } // expected output: true
 
 
 const graphIsATree = (graph) => {
+
+    // island check
+    for (let vertex in graph){
+        let edges = graph[vertex];
+        if (!edges.length){
+            return false
+        }
+    }
     
+    let root = graph[0];
+    let queue = [root];
+    let visited = {};
 
-
-
+   
+    // breadth-first search on the root
+    while (queue.length > 0){ 
+        let currentNode = queue.shift();
+        let edges = currentNode;
+        for (let x = 0; x < edges.length; x++){
+            let node = edges[x];
+            queue.push(node);
+            visited[node] ? false : visited[node] = true;
+        }
+    }
+    return true
 }
+console.log(graphIsATree(graph3)) // expected output: false | actual output: false
+console.log(graphIsATree(graph2)) // expected output: true | actual output: true
