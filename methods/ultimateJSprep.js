778180e8a7d99557eb2c0f93b31b9078dc7f4a15
Grant <g.ferowich@gmail.com>
2023-01-07 10:43:05 -0600

    defined ultimate javascript prep

---
+++ methods/ultimateJSprep.js
@@ -0,0 +1,14 @@
+/**
+ * variables
+ * */ 
+var x = 4
+var x = 5
+console.log(x)
+// outputs 5 since var variables are re-declarable and updateable
+
+let y = 21
+// let y = 34
+console.log(y)
+// syntax error - y cannot be re-declared
+
+
