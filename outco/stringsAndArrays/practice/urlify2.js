8e528d10f0287fbb051b02be4c6437c575e765fe
Grant <g.ferowich@gmail.com>
2023-01-09 08:13:30 -0600

    improved testing and documentation

---
+++ outco/stringsAndArrays/practice/urlify2.js
@@ -0,0 +1,9 @@
+// write a method to replace all spaces in a string with '%20'
+// e.g. Mr. John Smith => Mr.%20John%20Smith
+
+// Solution 2: Use JavaScript's native .replace method. 
+const urlify2 = (s) => {
+    return s.replace(/ /g,"%20" )
+}
+
+module.exports = urlify2
---
+++ outco/trees/sortedArrayToBST.js
@@ -0,0 +1,48 @@
+/**
+ * 108. Convert Sorted Array to Binary Search Tree
+Easy
+Given an integer array nums where the elements are sorted in ascending order, 
+convert it to a height-balanced binary search tree.
+
+Example 1:
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also acceptable. 
+
+Example 2:
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ * 
+ *  The array is sorted, so we could use binary search. 
+ *  The value at the middle index will work for the root. 
+ *  */
+
+class TreeNode{
+    constructor(value){
+        this.value = value === undefined ? null : value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+let fibArray = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+const sortedArrayToBST = (array) => {
+
+    let middleIndex = parseInt(array.length/2);
+
+    let left = array.slice(0, middleIndex);
+    let right = array.slice(middleIndex, array.length-1);
+    
+    let rootValue = array[middleIndex];
+    let root = new TreeNode(rootValue);
+
+    let x = left.length-1;
+    
+    const traverseLeft = (node) => {
+
+    }
+}
