34efa8e17f89d1f56575edfa1665c173efd27f70
Grant <g.ferowich@gmail.com>
2023-01-09 09:56:53 -0600

    tested modified version of isPalindrome

---
+++ outco/stringsAndArrays/practice/isPalindromeX2.js
@@ -0,0 +1,14 @@
+const isPalindromeX2 = (s) => {
+    let status = false;
+    for (let i = 0; i < s.length; i++){
+        if (s[i] == s[s.length-1-i]){
+            status = true;
+        } else 
+            break;
+    }
+
+    console.log('status', status);
+    return status; 
+}
+
+console.log(isPalindromeX2('hannah'))
