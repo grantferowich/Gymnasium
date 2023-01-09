ae3ae6ed06854a0ec7f8c9acaa3a2480376b3c9b
Grant <g.ferowich@gmail.com>
2023-01-09 09:30:44 -0600

    documented regex conventions and tested several methods

--- outco/stringsAndArrays/regexReplace.js
+++ outco/stringsAndArrays/regexReplace.js
@@ -13,8 +13,8 @@ output: a string with spaces yet without punctuation
 // \s in regex is whitespace
 // replace all whitespace with empty chars
 const removeSpaces = (string) => {
-    string = string.replace(/[\s]/g, "")
-    return string
+    string = string.replace(/[\s]/g, "");
+    return string;
 }
 console.log(removeSpaces("Wake Forest University")) // expected output: WakeForestUniversity | actual output: WakeForestUniversity
 console.log(removeSpaces('saint charles')) // expected output: saintcharles | actual output: saintcharles
@@ -22,8 +22,26 @@ console.log(removeSpaces('saint charles')) // expected output: saintcharles | ac
 // \w is word, so, ^\w is not word
 // replace all things which are not words (i.e. punctuation) with empty chars
 const removePunctuation = (string) => {
-    string = string.replace(/[^\w]/g, "")
-    return string
+    string = string.replace(/[^\w]/g, "");
+    return string;
 }
 console.log(removePunctuation('Winston-Salem')) // expected output: WinstonSalem | actual: WinstonSalem
-console.log(removePunctuation('St.Charles')) // expected output: StCharles | actual: WinstonSalem
+console.log(removePunctuation('St.Charles')) // expected output: StCharles | actual: StCharles
+console.log(removePunctuation('wake forest university')) // expected output: wakeforestuniversity | actual: wakeforestuniversity
+
+
+// the "opening" for regex and "close" for regex consists in the / and / 
+// the stuff between / and / decides what to replace
+// the g flag indicates the scope is global, so, replace all instances, not just the first one 
+// the [] also is required for "opening" and "closing" the regex conditions
+// by writing ^ we write not
+// the \w means words
+// the \ slash is used to introduce conditions
+// so, this following statement says, "replace the stuff that is not a word with
+// empty chars then convert the whole thing to lowercase"
+const removeAllStuffExceptLowerCaseWords = (string) => {
+    string = string.replace(/[^\w]/g, "").toLowerCase()
+    return string;
+}
+
+console.log(removeAllStuffExceptLowerCaseWords('Wake Forest University')) // expected output: wakeforestuniversity | actual output: wakeforestuniversity
