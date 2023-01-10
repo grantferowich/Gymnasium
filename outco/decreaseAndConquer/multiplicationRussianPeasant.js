7e4e3c8b654b31d2b918c4de1513ca1c993bd222
Grant <g.ferowich@gmail.com>
2023-01-10 14:12:31 -0600

    defined peasant multiplication problem statement

--- outco/decreaseAndConquer/multiplicationRussianPeasant.js
+++ outco/decreaseAndConquer/multiplicationRussianPeasant.js
@@ -1,3 +1,31 @@
+/*
+Successfully tested the function 1/10/23.
+
+ * ## Multiplication Using Russian Peasant [Extra Credit]
+ *
+ * *Given two positive integers, return its product using Russian Peasant method of multiplication*
+ *
+ * Read up on how to apply the Russian Peasant method [here](https://en.wikipedia.org/wiki/Ancient_Egyptian_multiplication). It is also referred to as the Egyptian multiplication.
+ *
+ * **Parameters**
+ * Input: a {Integer}
+ * Input: b {Integer}
+ * Output: {Integer}
+ *
+ * **Constraints**
+ * Assume a <= b, and the value of a is N.
+ *
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `487, 734 --> 357458`
+ * `846, 908--> 768168`
+ * 
+ *
+ */
+
+
 const  multiplicationRussianPeasant = (a, b) => {
     let accumulator = 0;
     while ( a > 0 ){
@@ -9,10 +37,8 @@ const  multiplicationRussianPeasant = (a, b) => {
     }
     return accumulator
 }
-// let x = 2 % 2;
-// console.log(x)
-// console.log(2 mod 2)
 
 console.log(multiplicationRussianPeasant(487, 734)) // 357,458
 console.log(multiplicationRussianPeasant(2, 23)) // 46
 console.log(multiplicationRussianPeasant(8, 21)) 
+console.log(multiplicationRussianPeasant(13, 238))
