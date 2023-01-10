8f990bc198a3fefb7ea9165461d0f8d2a19c7628
Grant <g.ferowich@gmail.com>
2023-01-10 15:21:42 -0600

    analyzed complexity

--- outco/decreaseAndConquer/greaterValues.js
+++ outco/decreaseAndConquer/greaterValues.js
@@ -1,6 +1,8 @@
 /*
  * ## Greater Values
  *
+ * Successfully tested the solution 1/10/23.
+ * 
  * *Given an sorted array of integers, and a target value return the number of values greater the target.*
  *
  * **Parameters**
@@ -54,15 +56,17 @@
 // if the pointer at i is less than the target and the value at i+1 is greater than the target
 // stop the loop with the pointer set to i + 1
 
+// Complexity Analysis
+// Time complexity: O(log(N)). The function cuts the array in half to search for the target.
+// Aux Space analysis: O(1). There are a few state variables declared yet there are no extra linear data structures. 
+
 
 function greaterValues(arr, target) {
- 
+    if (target > arr[arr.length-1]){return 0}
     let startIdx = 0;
-    let endIdx = arr.length;
+    let endIdx = arr.length -1;
     let midIdx;
-
-    if (arr[arr.length -1] < target){ return console.log(0)}
-
+    
     while (startIdx <= endIdx){
         midIdx = startIdx + parseInt((endIdx - startIdx) / 2);
     //   console.log('midIdx: ', midIdx);
@@ -75,12 +79,13 @@ function greaterValues(arr, target) {
       }
     }
   
-    return console.log(arr.length - midIdx)
+    return arr.length - midIdx
   }
 
-greaterValues([1, 2, 3, 5, 5, 7, 9, 10, 11], 5) //  --> 4
-greaterValues([1,2,3], 4) // => 0
-greaterValues([1, 10, 22, 59, 67, 72, 100], 13) // => 5
+console.log(greaterValues([1, 2, 3, 5, 5, 7, 9, 10, 11], 5)) //  --> 4
+console.log(greaterValues([1,2,3], 4)) // => 0
+console.log(greaterValues([1, 10, 22, 59, 67, 72, 100], 13)) // => 5
 
-greaterValues([1, 10, 15, 18, 20, 22, 26, 29, 31, 35, 48, 59, 67, 72, 100], 20) //  => 11
-greaterValues([1, 10, 15, 18, 20, 22, 26, 29, 31, 35, 48, 59, 67, 72, 100], 60) //  => 3
+console.log(greaterValues([1, 10, 15, 18, 20, 22, 26, 29, 31, 35, 48, 59, 67, 72, 100], 20)) //  => 11
+console.log(greaterValues([1, 10, 15, 18, 20, 22, 26, 29, 31, 35, 48, 59, 67, 72, 100], 60)) //  => 3
+console.log(greaterValues([1, 10, 15, 18, 20, 22, 26, 29, 31, 35, 48, 59, 67, 72, 100], 900)) //  => 3
