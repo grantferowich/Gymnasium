8a33f34a19b5edff450b9bc6feee6a7b3d791e76
Grant <g.ferowich@gmail.com>
2023-01-09 18:07:03 -0600

    successfully tested one away function

--- outco/stringsAndArrays/practice/isOneAwayX1.js
+++ outco/stringsAndArrays/practice/isOneAwayX1.js
@@ -6,16 +6,30 @@
 // ("pales", "pale") => true [one removal different]
 
 
+
+
+const isOneAwayX1 = (string1, string2) => {
+    if (string1.length === string2.length){
+        return isOneEditAwayX1(string1, string2);
+    }
+    if (string1.length - string2.length === 1){
+        return isOneInsertionOrDeletionAwayX1(string1, string2);
+    }
+    if (string1.length - string2.legnth === -1){
+        return isOneInsertionOrDeletionAwayX1(string1, string2);
+    }
+    if (string1.length - string1.length > 1 || string1.length - string1.length < -1){
+        return false
+    }
+}
+
 const isOneEditAwayX1 = (string1, string2) => {
     let edits = 0
     let x = 0;
     while ( x < string1.length){
         let char1 = string1[x];
         let char2 = string2[x];
-        console.log(char1)
-        console.log(char2)
         if (char1 !== char2){
-            console.log('edits changing')
             edits++
         }
         x++;
@@ -27,20 +41,26 @@ const isOneEditAwayX1 = (string1, string2) => {
 
 }
 
-const isOneAwayX1 = (string1, string2) => {
-    if (string1.length === string2.length){
-        return isOneEditAwayX1(string1, string2);
+const isOneInsertionOrDeletionAwayX1 = (string1, string2) => {
+    let x = 0;
+    let y = 0
+    while ( x < string1.length && y < string2.length){
+        let char1 = string1[x];
+        let char2 = string2[y];
+        if (char1 !== char2){
+            if (x !== y){
+                return false; 
+            }
+            y++
+        } else {
+            x++;
+            y++;
+        }
+       
     }
-    if (string1.length - string2.length === 1){
-        return isOneInsertionOrDeletionAway(string1, string2);
-    }
-    if (string1.length - string2.legnth === -1){
-        return isOneInsertionOrDeletionAway(string1, string2);
-    }
-    if (string1.length - string1.length > 1 || string1.length - string1.length < -1){
-        return false
-    }
+    return true;
 }
+
 
 // if the two strings are the same length and more than one char
 // between the two strings is different
@@ -49,6 +69,9 @@ const isOneAwayX1 = (string1, string2) => {
 
 console.log(isOneAwayX1("pale", "bake")); // false since p and k must change | actual: false
 console.log(isOneAwayX1("pale", "bale")); // true | actual: true
+console.log(isOneAwayX1('pales', 'pales')) // true
+console.log(isOneAwayX1('apple', 'aple')) // true
+console.log(isOneAwayX1('wakeforestuniversity', 'wkeforestuniversity')) // true
 
 
 
---
+++ outco/stringsAndArrays/practice/isOneAwayX2.js
@@ -0,0 +1,48 @@
+
+const isOneAwayX2 = (string1, string2) => {
+    
+    if (Math.abs(string1.length - string2.length) > 1){
+        return false;
+    }
+
+    let edits = 0;
+    let x = 0;
+    let y = 0;
+
+    while (x < string1.length && y < string2.length){
+        let char1 = string1[x];
+        let char2 = string2[y];
+        
+        if (char1 !== char2){
+            if (edits === 1){
+                return false;
+            }
+            if (string1.length > string2.length){
+                x++;
+            } else if (string2.length > string1.length){
+                y++;
+            } 
+            else {
+                x++;
+                y++;
+            }
+            edits++;
+        }
+        x++
+        y++
+    }
+
+    if (x < string1.length || y < string2.length){
+        edits++;
+    }
+
+    return (edits == 1)
+}
+
+// console.log(isOneAwayX2("pale", "bake")); // false since p and k must change | actual: false
+// console.log(isOneAwayX2("pale", "bale")); // true | actual: true
+// console.log(isOneAwayX2('pales', 'pales')) // true
+// console.log(isOneAwayX2('apple', 'aple')) // true
+console.log(isOneAwayX2('apples', 'aple')) // false
+// console.log(isOneAwayX2('wakeforestuniversity', 'wkeforestuniversity'))  // true
+
