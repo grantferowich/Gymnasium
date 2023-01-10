ce4327c50c2cfe7769703a866c8cd8f86cd33924
Grant <g.ferowich@gmail.com>
2023-01-10 14:07:49 -0600

    successfully tested the peasant multiplication algorithm

--- outco/decreaseAndConquer/multiplicationRussianPeasant.js
+++ outco/decreaseAndConquer/multiplicationRussianPeasant.js
@@ -1,13 +1,18 @@
-function  multiplicationRussianPeasant(a, b) {
+const  multiplicationRussianPeasant = (a, b) => {
+    let accumulator = 0;
     while ( a > 0 ){
-      a = a % 2
-      b = b * 2
+        if (a % 2 === 1){
+            accumulator += b
+        }
+      a = parseInt(a / 2)
+      b = b * 2;
     }
-    return b
+    return accumulator
 }
 // let x = 2 % 2;
 // console.log(x)
 // console.log(2 mod 2)
 
-// console.log(multiplicationRussianPeasant(487, 734)) // 357,458
+console.log(multiplicationRussianPeasant(487, 734)) // 357,458
 console.log(multiplicationRussianPeasant(2, 23)) // 46
+console.log(multiplicationRussianPeasant(8, 21)) 
