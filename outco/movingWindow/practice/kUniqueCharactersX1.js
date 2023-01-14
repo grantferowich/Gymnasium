242e888e45689af9f4780c93177c91ce7c143894
Grant <g.ferowich@gmail.com>
2023-01-14 11:40:41 -0600

    collaborated on new solution to kUniqueCharacters and successfully tested the solution

---
+++ outco/movingWindow/practice/kUniqueCharactersX1.js
@@ -0,0 +1,40 @@
+
+
+const kUniqueChars = (s) => {
+    let k = parseInt(s[0]);
+    let str = s.slice(1);
+    let startOfWindow = 0;
+    let maxLength = 0;
+    let currentCount = 0;
+    let hash = {};
+    for (let endOfWindow = 0; endOfWindow < str.length; endOfWindow++) {
+        
+        // hunting phase
+        if (!hash[str[endOfWindow]]) {
+            currentCount++;
+            hash[str[endOfWindow]] = true;
+        }else{
+            hash[str[endOfWindow]]++;
+        }
+
+        // catchup phase
+        while (currentCount > k) {
+            if(hash[str[startOfWindow]] == 1){
+                currentCount--;
+                delete hash[str[startOfWindow]];
+            }else{
+                hash[str[startOfWindow]]--;
+            }
+            startOfWindow++;
+        }
+        maxLength = Math.max(maxLength, endOfWindow - startOfWindow + 1);
+    }
+    return maxLength;
+}
+
+
+
+const input = "3aabbxxyyy";
+console.log(kUniqueChars(input));
+const input2 = "2eceba";
+console.log(kUniqueChars(input2))
