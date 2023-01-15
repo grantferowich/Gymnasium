7caf322024798bb2a4cde71c9db333a34ff5def6
Grant <g.ferowich@gmail.com>
2023-01-15 14:05:06 -0600

    annotated robot paths

--- outco/matrix/robotPaths.js
+++ outco/matrix/robotPaths.js
@@ -1,4 +1,7 @@
 /* 
+
+Successfully tested the function 1/15/23.
+Robot Paths: track the total paths from 0,0 to the bottom right corner of the input matrix
         col
 row    [[0,0],  
         [0,0]] 
