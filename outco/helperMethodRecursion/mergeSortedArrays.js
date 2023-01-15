47e78236d57846ebefd45adf6b50c73f02abe08c
Grant <g.ferowich@gmail.com>
2023-01-14 18:05:17 -0600

    annotated the merge sorted arrays function

--- outco/helperMethodRecursion/mergeSortedArrays.js
+++ outco/helperMethodRecursion/mergeSortedArrays.js
@@ -14,26 +14,40 @@ function mergeSortedArrays(arr1, arr2) {
   let i = 0;
   let j = 0;
 
+  // loop over both input arrays
   while (i < arr1.length && j < arr2.length) {
+    // the array featuring a smaller element in the 0th place
+    // will be the array the new result array draws on for its 0th element
+    // push the element from arr1 if that element is smaller
       if (arr1[i] <= arr2[j]) {
           merged.push(arr1[i]);
           i++;
       } else {
+          // push the element from arr2 if the element from arr1 is NOT smaller
           merged.push(arr2[j]);
           j++;
       }
   }
 
+  // suppose arr2 is 3 elements and arr1 is 5, 
+  // i will be at 3
+  // arr.length would be 5
+  // append the rest of the arr1 elements to the result array
   while (i < arr1.length) {
       merged.push(arr1[i]);
       i++;
   }
 
+
+  // when arr2 is the longer array
+  // loop over arr2's elements
+  // appending them to the result array
   while (j < arr2.length) {
       merged.push(arr2[j]);
       j++;
   }
 
+  // return result
   return merged;
 }
 
@@ -44,71 +58,3 @@ console.log(mergeSortedArrays([1,2,3,4,5], [6,8,10]))
 console.log(mergeSortedArrays([1,4,5,7], [2, 3,5,6,9])) 
 
 
-// function merge(arr1, arr2){
-//     let merged = [];
-//     let maxLength = Math.max(arr1.length, arr2.length)
-  
-//     const combine = (idx1, idx2, depth) => {
-//       if (depth >= maxLength -1) { return }
-//       if (arr1[idx1] !== null) {
-//         merged.push(arr1[idx1])
-//       }
-//       if (arr2[idx2] !== null){
-//         merged.push(arr2[idx2])
-//       }
-//       merged.sort();
-//       combine(idx1+1, idx2+1, depth + 1)
-//     }
-  
-//     combine(0,0,0)
-//     return merged;
-// }
-
-
-
-// function mergeSortedArrays2(arr1, arr2){
-//   let merged = [];
-//   let arrayLength = arr1.length + arr2.length;
-
-//   const combine = (idx1, idx2, depth) => {
-   
-//     if (merged.length === arrayLength) { return; }
-
-//     if (arr1[idx1] < arr2[idx2]){
-//       merged.push(arr1[idx1]);
-//       merged.push(arr2[idx2]);
-//     }
-
-//     if (arr1[idx1] > arr2[idx2]){
-//       merged.push(arr2[idx2]);
-//       merged.push(arr1[idx1]);
-//     }
-
-//     if (arr1[idx1] === arr2[idx2]){
-//       merged.push(arr2[idx2]);
-//       merged.push(arr1[idx1]);
-//     }
-
-//     if (arr1[idx1] === undefined && arr2[idx2]){
-//       merged.push(arr2[idx2])
-//     }
-
-//     if (arr2[idx2] === undefined && arr1[idx1]){
-//       merged.push(arr1[idx1])
-//     }
-
-//     // if (arr1[idx1] !== null) {
-//     //   merged.push(arr1[idx1])
-//     // }
-
-//     // if (arr2[idx2] !== null){
-//     //   merged.push(arr2[idx2])
-//     // }
-
-//     // merged.sort();
-
-//     combine(idx1+1, idx2+1, depth + 1)
-//   }
-//   combine(0,0,0)
-//   return merged;
-// }
