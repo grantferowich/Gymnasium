acaa66fedd433c1a613f88b013c10bb2d3e4dcb6
Grant <g.ferowich@gmail.com>
2023-01-17 17:43:20 -0600

    succesfully tested solution for product except self

---
+++ outco/stringsAndArrays/practice/productOfArrayExceptSelf.js
@@ -0,0 +1,19 @@
+// successfully tested the function 1/17/23
+
+const productExceptSelf = (nums) => {
+    
+    let result = [];
+    for (let x = 0; x < nums.length; x++){
+        let newVal = 1;
+        for (let y = 0; y < nums.length; y++){
+            
+            if (y !== x){
+                newVal *= nums[y]
+            }
+        }
+        result.push(newVal)
+    }
+    return result
+};
+
+console.log(productExceptSelf([1,2,3,4])) // [24, 12, 8, 6]
