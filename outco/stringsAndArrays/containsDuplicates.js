490e3dc72d702c1db51e5bd0a9b8013d40fe6111
Grant <g.ferowich@gmail.com>
2023-01-17 17:29:39 -0600

    successfully tested and defined contains duplicate function

---
+++ outco/stringsAndArrays/containsDuplicates.js
@@ -0,0 +1,22 @@
+/* 
+Leetcode 217
+
+*/
+
+const containsDuplicate = (nums) => {
+    let counts = {}
+    
+    for (let x = 0; x < nums.length; x++){
+        let num = nums[x];
+        counts[num] = counts[num] + 1 || 1;
+    }
+
+    for (key in counts){
+        if (counts[key] > 1){ return true}
+    }
+    return false
+};
+
+console.log(containsDuplicate([1,2,3,1])) // true
+console.log(containsDuplicate([1,2,3,4])); // false
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2])); // true
