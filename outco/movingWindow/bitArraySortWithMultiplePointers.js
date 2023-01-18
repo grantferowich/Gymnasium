ac03f43eab209b44ba97bfc4d2afb3001d7c6fa0
Grant <g.ferowich@gmail.com>
2023-01-18 15:34:03 -0600

    rearranged location of bit array sort method

--- outco/movingWindow/bitArraySortWithMultiplePointers.js
+++ outco/movingWindow/bitArraySortWithMultiplePointers.js
@@ -13,29 +13,12 @@
 
 var t0 = performance.now();
 
-
-const bitArraySort = (array) => {
-    let x = 0;
-    for (let y = 0; y < array.length; y++){
-        if (array[y] === 0){
-            // use destructuring assignment to move the element at y 
-            // from the y index to the y-1 index
-            [array[x], array[y]] = [array[y], array[x]]
-            console.log(array)
-            x++;
-        }
-    }
-    return array
-}
-
 // console.log(bitArraySort([1,0,0,1])) // => [0,0,1,1]
 // console.log(bitArraySort([1,0,0,0,0,0,0,0,0,0,0,1])) // => [0,0,0,0,0,0,0,0,0,0,0,1,1]
 
 const bitArraySortWithTwoPointers = (array) => {
-
     let left = 0; 
     let right = array.length - 1;
-
     while (left < right){
         if (array[left] === 0){
             left++;
