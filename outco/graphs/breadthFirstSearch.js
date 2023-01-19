1380cbbd49d7b9c5c5da30d7be8e84850ca72445
Grant <g.ferowich@gmail.com>
2023-01-19 17:21:57 -0600

    successfully tested a basic implementation of bfs through a graph to find a target node

--- outco/graphs/breadthFirstSearch.js
+++ outco/graphs/breadthFirstSearch.js
@@ -5,6 +5,47 @@ class Node{
     }
 }
 
-const breadthFirstSearch = () => {
+// BFS uses a queue
+// this BFS implementation starts at a node and searches for another node 
+// BFS describes the traversal style 
+const breadthFirstSearch = (graph, startNode, targetNode) => {
+    
+    // instantiate the queue
+    let queue = [startNode];
+    let visited = new Set();
+    
+    // process the queue
+    while (queue.length > 0){
+        
+        // examine the first node in the queue
+        let currentNode = queue.shift();
+        
+        // found the target
+        if (currentNode === targetNode){
+            return true;
+        }
 
+        // keep track of visited nodes
+        visited.add(currentNode);
+        for (let neighbor of graph[currentNode]){
+            // if the neighbor has not been processed
+            // push the neighbor vertex into the queue for processing
+            if (!visited.has(neighbor)){
+                queue.push(neighbor)
+            }
+        }
+    }
+    return false;
 }
+
+let graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+};
+console.log(breadthFirstSearch(graph, 'A', 'C'));  // Output: true
+console.log(breadthFirstSearch(graph, 'A', 'G'));  // Output: false
+
