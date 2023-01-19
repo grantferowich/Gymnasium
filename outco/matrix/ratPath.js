164ea9c2116eb32f981eaddff9195a085fdc0fe5
Grant <g.ferowich@gmail.com>
2023-01-19 07:34:18 -0600

    expanded test coverage successfully and deleted unnecessary files

--- outco/matrix/ratPath.js
+++ outco/matrix/ratPath.js
@@ -1,3 +1,16 @@
 /* 
 Given a maze, represented by a matrix of bits (values 0 and 1), a rat must find a path from index [0][0] to [n-1][m-1]. The rat can only travel to the right or down, and can only travel on 0 values. 1 values represent walls.
+
+Input:   [[0, 0, 0, 1],
+          [0, 1, 0, 1],
+          [0, 1, 0, 0],
+          [0, 0, 1, 0]]
+
+Output:  [[0, 0],
+          [0, 1],
+          [0, 2],
+          [1, 2],
+          [2, 2],
+          [2, 3],
+          [3, 3]]
 */
