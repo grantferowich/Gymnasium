f6676208101c3ccfe08b779ee15f9a6405ac3d11
Grant <g.ferowich@gmail.com>
2023-01-22 10:35:41 -0600

    succesfully tested the rat path function

--- outco/matrix/ratPath.js
+++ outco/matrix/ratPath.js
@@ -13,4 +13,73 @@ Output:  [[0, 0],
           [2, 2],
           [2, 3],
           [3, 3]]
+
+// depth first search
+// store coords of successful path in an output accumulator array
+// the rat only goes down and to the left (binary decision)
+(x + 1, y) and (y + 1, x)
+the rat only travels on 0s
+
+
+base cases: 
+arrived at end of the maze
+cannot travel to a cell because 1 (path is blocked)
+out of bounds
+
+** note: you have to use backtracking like you did in robot paths
 */
+
+const ratPath = (matrix) => {
+    
+    let xTarget = matrix.length;
+    let yTarget = matrix[0].length;
+    let path = []
+    let output = [];
+
+    const travel = (row, col) => {
+    
+        if (row < 0 || col < 0 || row >= xTarget || col >= yTarget){
+            return;
+        }
+
+        
+        if (row === xTarget - 1 && col === yTarget - 1){
+            path.push([row, col])
+            output = [...path]
+            return;
+        }
+
+        if (matrix[row][col] === 0){
+            path.push([row,col])
+            matrix[row][col] = 1;
+            output.push([row, col]);
+            travel(row + 1, col);
+            travel(row, col + 1);
+            path.pop()
+        }
+    }
+    travel(0,0)
+    return output
+}
+
+console.log(ratPath([[0, 0, 0, 1],
+                     [0, 1, 0, 1],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, 0]]))
+
+// expected output: Output:  [[0, 0],
+        //   [0, 1],
+        //   [0, 2],
+        //   [1, 2],
+        //   [2, 2],
+        //   [2, 3],
+        //   [3, 3]]
+
+// actual output: [
+//   [ 0, 0 ], [ 0, 1 ],
+//   [ 0, 2 ], [ 1, 2 ],
+//   [ 2, 2 ], [ 2, 3 ],
+//   [ 3, 3 ]
+// ]
+
+console.log(ratPath([[0, 0, 0, 0]])) // [[ 0 , 0 ], [ 0 , 1], [ 0 , 2 ], [ 0 , 3], [ 0 , 3]]
