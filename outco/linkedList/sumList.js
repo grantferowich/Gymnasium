67103351c0ffd276fdafb2be4a73b1b836fb35bb
Grant <g.ferowich@gmail.com>
2023-01-25 10:35:08 -0600

    successfully tested the sumList function

---
+++ outco/linkedList/sumList.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(value) {
+    this.val = value;
+    this.next = null;
+  }
+}
+
+const sumList = (head) => {
+    let current = head;
+    let sum = 0
+    while (current){
+      let value = current.val;
+      sum += value;
+      current = current.next
+    }
+    return sum;
+};
+
+let listX = new Node(1)
+listX.next = new Node(1)
+listX.next.next = new Node(2)
+// 4
+console.log(sumList(listX))
