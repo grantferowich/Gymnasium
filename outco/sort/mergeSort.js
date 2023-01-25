f32d464212433cc687c7f8c2dacb6293554a07a9
Grant <g.ferowich@gmail.com>
2023-01-25 15:42:12 -0600

    successfully tested merge sort function

---
+++ outco/sort/mergeSort.js
@@ -0,0 +1,25 @@
+function mergeSort(input) {
+    if (input.length < 2){
+      return input;
+    }
+    const midpoint = parseInt(input.length / 2);
+    const left = input.slice(0, midpoint);
+    const right = input.slice(midpoint);
+    //helper method
+    const merge = (left, right) => {
+      let output = []
+      while (left.length && right.length){
+        if (left[0] < right[0]){
+          output.push(left.shift())
+        } else {
+          output.push(right.shift())
+        }
+      }
+      return output.concat(left.slice()).concat(right.slice())
+    }
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+console.log(
+  mergeSort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213])
+);
