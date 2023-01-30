5afe9ed12ab85dbfe66d1094f394baf1be4027b1
Grant <g.ferowich@gmail.com>
2023-01-29 18:05:57 -0600

    defined function requirements

--- outco/graphs/shortestRoute.js
+++ outco/graphs/shortestRoute.js
@@ -1,4 +1,16 @@
 /* 
+320 - Shortest Route
+Find the shortest route from one vertex to another in a given graph.
 Unfinished as of 1/18/23.
 https://github.com/OutcoSF/outco-class-163/tree/main/whiteboarding/w3_d1_graphs
+
+Complexity analysis: 
+Time complexity : O(V+E)
+Auxiliary space complexity: O(V)
+
+Input: a graph, a starting vertex, an ending vertex
+Output: number
+
+
 */
+
