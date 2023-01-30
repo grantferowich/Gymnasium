86b66b418b11a3e4fbfc69d54a13a04c2dd4562e
Grant <g.ferowich@gmail.com>
2023-01-30 08:27:26 -0600

    appended test status

--- outco/graphs/shortestRoute.js
+++ outco/graphs/shortestRoute.js
@@ -1,7 +1,9 @@
 /* 
+Successfully tested the function.
+
 320 - Shortest Route
 Find the shortest route from one vertex to another in a given graph.
-Unfinished as of 1/18/23.
+
 https://github.com/OutcoSF/outco-class-163/tree/main/whiteboarding/w3_d1_graphs
 
 Complexity analysis: 
