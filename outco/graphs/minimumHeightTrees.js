a0220e0b687eaaafde87f4c44560b9f4ca62f28a
Grant <g.ferowich@gmail.com>
2023-01-30 23:27:45 -0600

    appended test status

--- outco/graphs/minimumHeightTrees.js
+++ outco/graphs/minimumHeightTrees.js
@@ -1,5 +1,5 @@
 /* 
-Unfinished as of 1/18/23.
+Successfully tested the function 1/30/23.
 https://github.com/OutcoSF/outco-class-163/blob/main/whiteboarding/w3_d1_graphs/410_minimum_height_trees_HARD.md
 
 input: an array of edges as tuples
