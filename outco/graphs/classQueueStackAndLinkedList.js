ceb99f393947364ee765b16cfed93aaaa907d8dc
Grant <g.ferowich@gmail.com>
2023-02-01 14:49:25 -0600

    updated classes and appended graph class

--- outco/graphs/classQueueStackAndLinkedList.js
+++ outco/graphs/classQueueStackAndLinkedList.js
@@ -1,3 +1,84 @@
+// Graph, Linked List, and Queue Classes
+// Successfully tested all queue methods 2/1/23
+
+class Graph {
+    constructor() {
+     this.storage = {};
+    }
+   
+    // Time Complexity: O(1)
+    // Auxiliary Space Complexity: O(1)
+    // add vertex is basically add a key to a hash map
+    addVertex(id) {
+        if (this.storage[id]) { return false; }
+        this.storage[id] = [];
+        return true;
+    }
+   
+    // Time Complexity: O(V+E)
+    // Auxiliary Space Complexity: O(1)
+    // remove vertex is basically remove a key from a hash map
+    removeVertex(id) {
+          // vertex already exists
+     if (!this.storage[id]){ return false; }
+     for (let vertex in this.storage){
+        let edges = this.storage[vertex];
+        let index = edges.indexOf(id);
+        if (index >= 0){
+            edges.splice(index, 1)
+        }
+     }
+     delete this.storage[id];
+     return true;
+    }
+   
+    // Time Complexity: O(E)
+    // Auxiliary Space Complexity: O(1)
+    // add edge is basically add a value to the hash map
+    addEdge(id1, id2) {
+        let vertexList = [];
+        for (let vertex in this.storage){
+          vertexList.push(vertex);
+        } 
+        let index1 = vertexList.indexOf(id1.toString()); // 5
+        let index2 = vertexList.indexOf(id2.toString()); 
+        if (index1 === -1 || index2 === -1){ return false; }
+        this.storage[id1].push(id2);
+        return true;
+     }
+   
+    // Time Complexity: O(1)
+    // Auxiliary Space Complexity: O(1)
+    // remove edge is basically delete values from the hash map
+    removeEdge(id1, id2) {
+        if (this.storage[id1]){
+            let edges = this.storage[id1];
+            let index = edges.indexOf(id2);
+            edges.splice(index,1)
+            return true;
+          }
+          return false;
+    }
+   
+    // Time Complexity: O(1)
+    // Auxiliary Space Complexity: O(1)
+    // access to return boolean if key exists
+    isVertex(id) {
+      if (this.storage[id]){
+        return true;
+      }
+      return false;
+    }
+   
+    // Time Complexity: O(1)
+    // Auxiliary Space Complexity: O(E)
+    // access to return the values given a key
+    neighbors(id) {
+        if (!this.storage[id]){return null;}
+        return this.storage[id]
+    }
+}
+
 class ListNode {
     constructor(value){
         this.value = value;
@@ -106,25 +187,3 @@ class Queue {
         return this.linkedlist.head.value;
     }
 }
-
-class Stack{
-    constructor(){
-        this.linkedList = new SinglyLinkedList()
-    }
-    push(value){
-        this.linkedList.addToTail(value)
-    }
-    pop(){
-        let node = this.linkedList.removeFromTail();
-        return node;
-    }
-}
-
-let list = new Queue()
-list.enqueue(1)
-list.enqueue(1)
-list.enqueue(2)
-list.enqueue(3)
-// console.log(list.dequeue()) // 1
-// console.log(list)
-console.log(list.peek()) // 1
