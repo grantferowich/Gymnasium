d16c765eb8876ef977c401c3896ffa1ed9c861d0
Grant <g.ferowich@gmail.com>
2023-02-05 22:29:18 -0600

    successfully tested return Kth to last

--- outco/linkedList/returnKthToLast.js
+++ outco/linkedList/returnKthToLast.js
@@ -16,6 +16,40 @@ Input:
 Output: 5
 */
 
-const returnKthToLast = (list, k) => {
-    
+class ListNode{
+    constructor(value){
+        this.value = value === undefined ? null : value;
+        this.next = null;
+    }
 }
+
+const returnKthToLast = (list, k) => {
+
+    let count = 0;
+    let current = list;
+    while (current !== null){
+        count++;
+        current = current.next;
+    }
+    let target = count - (k-1)
+    current = list;
+    count = 0;
+    while (current !== null){
+        count++
+        if (count === target){
+            return current.value
+        }
+        current = current.next
+    }
+}
+
+const list1 = new ListNode(1);
+list1.next = new ListNode(1);
+list1.next.next = new ListNode(1)
+list1.next.next.next = new ListNode(2)
+list1.next.next.next.next = new ListNode(3)
+list1.next.next.next.next.next = new ListNode(3)
+list1.next.next.next.next.next.next = new ListNode(5)
+
+console.log(returnKthToLast(list1, 1)) // 5
+console.log(returnKthToLast(list1, 2)) // 3
