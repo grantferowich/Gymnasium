55533dc644090627819657a12473198afbd77139
Grant <g.ferowich@gmail.com>
2023-02-08 08:27:46 -0600

    successfully tested a function to find the two elements which equal the target input in a sorted arrayg

--- outco/movingWindow/twoSumII.js
+++ outco/movingWindow/twoSumII.js
@@ -1,9 +1,14 @@
 
 /* 
 
-Successfully tested the solution where every possible 
-combination is checked.
-The function was tested across two test cases 2/7/23.
+Successfully tested the solution 2/8/23. 
+The solution uses two pointers at the start and end of the sorted array.
+The solution checks whether the sum of the elements equals the target value.
+If the sum is less than the target value the start index increments. 
+If the sum is greater than the target the end index decrements. 
+
+Complexity analysis
+The solution has a time complexity of O(N) and a space complexity of O(1).
 
 
 // start a pointer at the middle index 
@@ -15,15 +20,7 @@ The function was tested across two test cases 2/7/23.
 
 console.log(twoSumII([2,7,11,15], 9)) // [ 1, 2 ]
 
-s = 0 
-m = 1
-v = 7
-r = target - v = 2
-e = 1
-indexOf(r) = 0
-indexOf(7) = 2
-
-
+console.log(twoSumII([ 5, 25, 75], 100)) // [2,3]
 
 */
 
@@ -33,27 +30,20 @@ var twoSumII = (numbers, target) => {
     let e = numbers.length -1;
 
     while (s < e){
-        
-        let m = parseInt((s - Math.max((e - s)/2)));
-        let v = numbers[m];
-        let r = target - v;
-        if (numbers.indexOf(r) > -1){
-            return [numbers.indexOf(v)+1, numbers.lastIndexOf(r)+1]
+        let sum = numbers[s] + numbers[e];
+        if (sum === target){
+            return [ s + 1, e + 1 ]
         }
-        if (r < v){
-            // look left
-            e = m - 1;
+        if (sum < target) {
             s++
-        } else {
-            // look right
-            s = m + 1;
-            e--;
+        }
+        if (sum > target){
+            e--
         }
     }
     return []
-
 };
 
-// console.log(twoSumII([ 2, 7, 11, 15], 9)) // [ 1, 2 ]
-// console.log(twoSumII([ 2, 3, 4 ], 6))  // [ 1 , 3 ]
-console.log(twoSumII([ 5, 25, 75], 100)) // [2,3]
+console.log(twoSumII([ 2, 7, 11, 15], 9)) // [ 1, 2 ]
+console.log(twoSumII([ 2, 3, 4 ], 6))  // [ 1 , 3 ]
+console.log(twoSumII([ 5, 25, 75], 100)) // [ 2, 3 ]
