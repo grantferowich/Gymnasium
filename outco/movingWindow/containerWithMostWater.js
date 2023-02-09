220c25022fdf9c404991ac028e5d752418cb0229
Grant <g.ferowich@gmail.com>
2023-02-09 13:58:08 -0600

    appended test status

--- outco/movingWindow/containerWithMostWater.js
+++ outco/movingWindow/containerWithMostWater.js
@@ -1,4 +1,5 @@
 /* 11. Container With Most Water
+Successfully tested the function 2/9/23
 Medium
 You are given an integer array height of length n. 
 There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
