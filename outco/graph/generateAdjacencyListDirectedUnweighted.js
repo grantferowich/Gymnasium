c05f70bab695d40941f04f8ccfdb9f291220c4dd
Grant <g.ferowich@gmail.com>
2023-02-13 08:50:14 -0600

    constructed and successfully tested the generateAdjacencyList(x) function

--- outco/graph/generateAdjacencyListDirectedUnweighted.js
+++ outco/graph/generateAdjacencyListDirectedUnweighted.js
@@ -8,21 +8,122 @@ This function generates a directed, unweighted adjacency list.
 
 
 */
-const generateAdjacencyListDirectedUnweighted = (arrayOfTuples) => {
-    const list = {};
-    for (let x = 0; x < arrayOfTuples.length; x++){
-        const id1 = arrayOfTuples[x][0];
-        const id2 = arrayOfTuples[x][1];
-        if (!list[id1]){
-            list[id1] = [];
+
+class Graph {
+    constructor() {
+     this.storage = new Map()
+    }
+   
+    // Time Complexity: O(1)
+    // Auxiliary Space Complexity: O(1)
+    // add vertex is basically add a key to a hash map
+    addVertex(id) {
+        if (this.storage.has(id)) { return false; }
+        this.storage.set(id, new Set())
+        return true;
+    }
+   
+    // Time Complexity: O(V+E)
+    // Auxiliary Space Complexity: O(1)
+    // remove vertex is basically remove a key from a hash map
+    removeVertex(id) {
+          // vertex already exists
+     if (!this.storage.has(id)){ return false;}
+     for (let vertex in this.storage){
+        let edges = this.storage.get(vertex);
+        let index = edges.indexOf(id);
+        // if the vertex being passed is an edge
+        if (index >= 0){
+            edges.delete(value)
         }
-        if (list[id1]){
-            list[id1].push(id2);
+     }
+     this.storage.delete(id);
+     return true;
+    }
+   
+    // Time Complexity: O(E)
+    // Auxiliary Space Complexity: O(1)
+    // add edge is basically add a value to the hash map
+  
+    addEdge(id1, id2) {
+  
+        if (!this.storage.has(id1)){
+            this.storage.addVertex(id1)
+        }
+        if (!this.storage.has(id2)){
+            this.storage.addVertex(id2)
+        }
+        //this.storage.get(x) returns a set
+        // this.add(x) generates a union between the set and the element
+        this.storage.get(id1).add(id2)
+        return true;
+    }
+    // Time Complexity: O(1)
+    // Auxiliary Space Complexity: O(1)
+    // remove edge is basically delete values from the hash map
+    removeEdge(id1, id2) {
+        if (this.storage.has(id1)){
+            if (this.storage.get(id1).has(id2)){
+                this.storage.get(id1).delete(id2)
+            }
+            // edges.splice(index,1)
+            return true;
+          }
+          return false;
+    }
+   
+    // Time Complexity: O(1)
+    // Auxiliary Space Complexity: O(1)
+    // access to return boolean if key exists
+    hasVertex(id) {
+      if (this.storage.has(id)){
+        return true;
+      }
+      return false;
+    }
+   
+    // Time Complexity: O(1)
+    // Auxiliary Space Complexity: O(E)
+    // access to return the values given a key
+    neighbors(id) {
+        console.log('hi')
+        if (!this.storage.has(id)){return null;}
+        return this.storage.get(id)
+    }
+  
+    vertices(){
+        return this.storage.keys()
+    }
+  
+    printVertices(){
+        let vertices = Array.from(this.storage.keys())
+        for (let x = 0; x < vertices.length; x++){
+            console.log(vertices[x])
         }
     }
-    return list;
+  }
+
+const generateAdjacencyListDirectedUnweighted = ( edges ) => {
+    let g = new Graph()
+    
+    let x = 0;
+    while (x < edges.length){
+        let v = edges[x][0]
+        let e = edges[x][1]
+        if (!g.storage.has(v)){
+            g.addVertex(v)
+        }
+        if (!g.storage.has(e)){
+            g.addVertex(e)
+        }  
+        g.addEdge( v, e );
+        x++
+    }
+    return g;
 }
 
 const edges = [[1,2], [1,3], [2,3]]
-const graph = generateAdjacencyListDirectedUnweighted(edges)
-console.log(graph) // { '1': [ 2, 3 ], '2': [ 3 ] }
+// const graph = generateAdjacencyListDirectedUnweighted(edges)
+const g = generateAdjacencyListDirectedUnweighted(edges) // { '1': [ 2, 3 ], '2': [ 3 ] }
+console.log(g)
+console.log(typeof g)
--- outco/graph/longestPath.js
+++ outco/graph/longestPath.js
@@ -5,15 +5,16 @@ Output: an integer
 
 class Graph {
     constructor() {
-     this.storage = {};
+     this.storage = new Map()
     }
    
     // Time Complexity: O(1)
     // Auxiliary Space Complexity: O(1)
     // add vertex is basically add a key to a hash map
     addVertex(id) {
-        if (this.storage[id]) { return false; }
-        this.storage[id] = [];
+        console.log(this.storage)
+        if (this.storage.has(id)) { return false; }
+        this.storage.set(id, new Set())
         return true;
     }
    
@@ -22,41 +23,45 @@ class Graph {
     // remove vertex is basically remove a key from a hash map
     removeVertex(id) {
           // vertex already exists
-     if (!this.storage[id]){ return false; }
+     if (!this.storage.has(id)){ return false;}
      for (let vertex in this.storage){
-        let edges = this.storage[vertex];
+        let edges = this.storage.get(vertex);
         let index = edges.indexOf(id);
+        // if the vertex being passed is an edge
         if (index >= 0){
-            edges.splice(index, 1)
+            edges.delete(value)
         }
      }
-     delete this.storage[id];
+     this.storage.delete(id);
      return true;
     }
    
     // Time Complexity: O(E)
     // Auxiliary Space Complexity: O(1)
     // add edge is basically add a value to the hash map
-
+  
     addEdge(id1, id2) {
-        let vertexList = [];
-        for (let vertex in this.storage){
-          vertexList.push(vertex);
-        } 
-        let index1 = vertexList.indexOf(id1.toString()); // 5
-        let index2 = vertexList.indexOf(id2.toString()); 
-        if (index1 === -1 || index2 === -1){ return false; }
-        this.storage[id1].push(id2);
+  
+        if (!this.storage.has(id1)){
+            this.storage.addVertex(id1)
+        }
+        if (!this.storage.has(id2)){
+            this.storage.addVertex(id2)
+        }
+        //this.storage.get(x) returns a set
+        // this.add(x) generates a union between the set and the element
+        this.storage.get(id1).add(id2)
         return true;
     }
     // Time Complexity: O(1)
     // Auxiliary Space Complexity: O(1)
     // remove edge is basically delete values from the hash map
     removeEdge(id1, id2) {
-        if (this.storage[id1]){
-            let edges = this.storage[id1];
-            let index = edges.indexOf(id2);
-            edges.splice(index,1)
+        if (this.storage.has(id1)){
+            if (this.storage.get(id1).has(id2)){
+                this.storage.get(id1).delete(id2)
+            }
+            // edges.splice(index,1)
             return true;
           }
           return false;
@@ -65,8 +70,8 @@ class Graph {
     // Time Complexity: O(1)
     // Auxiliary Space Complexity: O(1)
     // access to return boolean if key exists
-    isVertex(id) {
-      if (this.storage[id]){
+    hasVertex(id) {
+      if (this.storage.has(id)){
         return true;
       }
       return false;
@@ -77,20 +82,22 @@ class Graph {
     // access to return the values given a key
     neighbors(id) {
         console.log('hi')
-        if (!this.storage[id]){return null;}
-        return this.storage[id];
+        if (!this.storage.has(id)){return null;}
+        return this.storage.get(id)
     }
-
-    vertices() {
-        let keys = []
-        this.storage.forEach((value, key) => {
-            keys.push(key)
-        })
-        console.log(keys)
-        return keys;
-
+  
+    vertices(){
+        console.log('hi')
+        return this.storage.keys()
     }
-}
+  
+    printVertices(){
+        let vertices = Array.from(this.storage.keys())
+        for (let x = 0; x < vertices.length; x++){
+            console.log(vertices[x])
+        }
+    }
+  }
 
 class ListNode {
     constructor(value){
@@ -196,8 +203,10 @@ const generateAdjacencyListDirectedUnweighted = (arrayOfTuples) => {
 function longestPath(graph) {
     let result = 0
     let visited = new Set();
-    // console.log('graph', graph)
-    let vertices = graph.vertices
+    console.log('graph0', graph)
+    graph = generateAdjacencyListDirectedUnweighted(graph)
+    console.log('graph1', graph)
+    const vertices = graph.vertices()
     console.log(vertices);
     
   
@@ -223,5 +232,5 @@ function longestPath(graph) {
   
 
 const input1 = [[1,2],[2,3],[1,3]]
-const graph1 = generateAdjacencyListDirectedUnweighted(input1)
-console.log(longestPath(graph1));
+// const graph1 = generateAdjacencyListDirectedUnweighted(input1)
+console.log(longestPath(input1));
