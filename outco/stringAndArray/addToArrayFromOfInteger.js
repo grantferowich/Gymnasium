9a13270a7f45c04dd596b56f51d51bc07209f95a
Grant <g.ferowich@gmail.com>
2023-02-15 09:12:36 -0600

    successfully tested the addToArrayFromInteger function

---
+++ outco/stringAndArray/addToArrayFromOfInteger.js
@@ -0,0 +1,25 @@
+/* 
+input: num is an array of integers, and k, an integer
+return a number 
+ */
+
+const addToArrayForm = (num, k) => {
+    if (num === ''){ return}
+    // let number = '';
+    let x = 0;
+    let number = BigInt(num.join(''))
+
+    let sum = (number + BigInt(k))
+
+    sum = sum.toString()
+    return sum.split('')
+};
+
+// let num1 = [2,3,0];
+// let k1 = 9;
+// console.log(addToArrayForm(num1, k1)) // [2,3,9]
+
+let num2 = [1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,0,6,3]
+let k2 = 516;
+console.log(addToArrayForm(num2, k2)) // Expect: [1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,5,7,9]
+                                      // Actual: 
