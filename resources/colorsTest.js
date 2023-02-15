854019dd8f57b3fe646a00a4c57c0fb16706ce1a
Grant <g.ferowich@gmail.com>
2023-02-15 07:07:17 -0600

    successfully tested the colors library

---
+++ resources/colorsTest.js
@@ -0,0 +1,5 @@
+require('colors');
+
+console.log(('\nThis is a success message.'.green))
+console.log(('\nThis is an error message.'.red))
+console.log('\nThis is a yellow warning message.'.yellow)
