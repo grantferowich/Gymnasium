66ea9396aafd9ad4ca008a4c5ba40451f0cd658a
grantferowich <g.ferowich@gmail.com>
2023-02-18 10:55:51 -0600

    succesfully tested enqueue, peek, and isEmpty class methods of simply queue class

--- outco/stackAndQueue/simpleQueueClass.js
+++ outco/stackAndQueue/simpleQueueClass.js
@@ -32,4 +32,56 @@ class QueueNode{
     }
 }
 
-class Queue()
+class Queue{
+    constructor(){
+        // first and last will be nodes
+        this.first = null;
+        this.last = null
+    }
+
+    isEmpty(){
+        return this.first === null;
+    }
+    peek(){
+        if (this.first){
+            return this.first.data
+        }
+        return 'Error: When attempting to peek, the queue was in fact empty.'
+    }
+    enqueue(value){
+        if (value === undefined || value === null){ return 'input value undefined'}
+        let qNode = new QueueNode(value);
+        if (this.first === null){
+            console.log('enqueue case1..')
+            this.first = qNode
+            return true
+        }
+        if (this.first && !this.last){
+            console.log('enqueue case2..')
+            this.first.next = qNode
+            this.last = qNode
+            return true
+        } 
+        if (this.first && this.last){
+            console.log('enqueue case3..')
+            this.last.next = qNode
+            this.last = qNode
+            return true
+        }
+
+    }
+    dequeue(){
+
+    }
+
+}
+/* TESTS */
+let q = new Queue()
+// console.log(q.enqueue(undefined))
+console.log(q)
+// q.enqueue(11)
+// q.enqueue(23)
+// console.log(q)
+// console.log(q.peek()) // 11
+// console.log(q.isEmpty()) // false
+// console.log(q.isEmpty()) 
