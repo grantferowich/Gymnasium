3647d1afed0e997fcc620b33d9ccd70f234670b0
grantferowich <g.ferowich@gmail.com>
2023-02-18 11:50:22 -0600

    clarified output

--- outco/stackAndQueue/simpleQueueClass.js
+++ outco/stackAndQueue/simpleQueueClass.js
@@ -27,9 +27,9 @@
 
 require('colors');
 
-console.log(('\nThis is a success message.'.green))
-console.log(('\nThis is an error message.'.red))
-console.log('\nThis is a yellow warning message.'.yellow)
+// console.log(('\nThis is a success message.'.green))
+// console.log(('\nThis is an error message.'.red))
+// console.log('\nThis is a yellow warning message.'.yellow)
 
 class QueueNode{
     constructor(data = null){
