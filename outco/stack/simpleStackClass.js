6df4b4e1c9cf18bd5bcd9cef9860d6b011eff8ec
grantferowich <g.ferowich@gmail.com>
2023-02-18 08:31:26 -0600

    successfullyt tested simple stack class peek, push, and pop methods

--- outco/stack/simpleStackClass.js
+++ outco/stack/simpleStackClass.js
@@ -9,15 +9,24 @@ class StackNode{
 
 class Stack{
     constructor(){
-        this.top = null
+        this.top = new StackNode()
     }
 
     pop(){
         if (this.top === null){ return 'empty stack exception'}
+        let value = this.top.data
+        this.top = this.top.next
+        return value
+    }
+
+    peek(){
+        return this.top.data
     }
 
     push(value){
-        this.top = value;
+        let xNode = new StackNode(value)
+        this.top.next = this.top
+        this.top = xNode
 
     }
 }
@@ -26,4 +35,12 @@ class Stack{
 // stack.next = new StackNode(2)
 let stack = new Stack()
 // console.log(stack)
-console.log(stack.pop()) // empty stack exception 
+// console.log(stack.pop()) // empty stack exception 
+stack.push(11)
+stack.push(23)
+// console.log(stack)
+// console.log(stack.top.data)
+// console.log('popping...',stack.pop())
+// console.log(stack.top)
+// console.log(stack)
+console.log(stack.peek())
