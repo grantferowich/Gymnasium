86183a794c979dffa7455b8d91c6c601fb41b4a0
grantferowich <g.ferowich@gmail.com>
2023-02-18 08:48:49 -0600

    successfully expanded test coverage of the simply stack class

--- outco/stack/simpleStackClass.js
+++ outco/stack/simpleStackClass.js
@@ -1,37 +1,39 @@
 class StackNode{
-    constructor(data=null, top=null){
+    constructor(data=null){
         this.data = data;
         this.next = null;
-        this.top = top
     }
 }
 
 
 class Stack{
     constructor(){
-        this.top = new StackNode()
+        // top will be a node
+        this.top = null
     }
 
     pop(){
         if (this.top === null){ return 'empty stack exception'}
-        let value = this.top.data
+        let value = this.top
         this.top = this.top.next
         return value
     }
 
     peek(){
-        return this.top.data
+        return this.top
     }
 
     push(value){
         let xNode = new StackNode(value)
-        this.top.next = this.top
+        // connect the new stack node to the current top
+        xNode.next = this.top
+        // set the top node as a new node 
         this.top = xNode
 
     }
 
     isEmpty(){
-        return this.top.data === null
+        return this.top === null
     }
 }
 
@@ -42,12 +44,20 @@ let stack0 = new Stack()
 // console.log(stack)
 // console.log(stack.pop()) // empty stack exception 
 stack.push(11)
-stack.push(23)
+// stack.push(23)
 // console.log(stack)
 // console.log(stack.top.data)
 // console.log('popping...',stack.pop())
 // console.log(stack.top)
 // console.log(stack)
 // console.log(stack.peek())
-console.log(stack.isEmpty())
-console.log(stack0.isEmpty())
+// console.log(stack.isEmpty())
+// console.log(stack0.isEmpty())
+// console.log(stack)
+// console.log(stack.top)
+// console.log(stack.top.data)
+// stack.pop()
+// console.log(stack)
+// console.log(stack.isEmpty())
+stack.push(23)
+console.log(stack)
