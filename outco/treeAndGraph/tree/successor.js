0034f96e4bdc2e6997de9c673961dc7c6e09a61e
grantferowich <g.ferowich@gmail.com>
2023-02-21 05:43:17 -0600

    defined successor diagram

---
+++ outco/treeAndGraph/tree/successor.js
@@ -0,0 +1,25 @@
+/* 
+
+SUCCESSOR
+CTCI 4.6
+
+Input: a bst node
+output: the left most element of the right subtree
+
+Patterns: In-order DFS
+
+Test:
+
+        8
+       / \ 
+      5  34
+         / \ 
+        23  57
+
+suppose node = (8)
+successor node = (23)
+
+
+*/
+
+const successor
