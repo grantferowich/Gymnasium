c25912cbfb1aabe55c166d35b230e0f61dfb29b9
grantferowich <g.ferowich@gmail.com>
2023-02-27 11:58:20 -0600

    defined question statement

---
+++ outco/stringAndArray/oddManOut.js
@@ -0,0 +1,9 @@
+
+
+/* 
+ODD MAN OUT
+
+You're given an unsorted array of integers where every integer appears exactly
+twice,except for one integer which appears only once. Write an algorithm (in a
+language of your choice) that finds the integer that appears only once.
+*/
