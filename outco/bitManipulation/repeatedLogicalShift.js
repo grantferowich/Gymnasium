eb82e793107b7d6c3cfc626c0a797f73ce612e1e
grantferowich <g.ferowich@gmail.com>
2023-02-28 17:06:23 -0600

    successfully tested repeatedLogicalShift(x,count)

--- outco/bitManipulation/repeatedLogicalShift.js
+++ outco/bitManipulation/repeatedLogicalShift.js
@@ -2,3 +2,17 @@
 
 REPEATED LOGICAL SHIFT
 */
+
+const repeatedLogicalShift = (x , count) => {
+    let z = 0;
+    while (z < count){
+        x >>>= 1 // aritmetic shift by 1 
+        z++
+    }
+    return x
+}
+
+const data = repeatedLogicalShift(-932424444444, 800)
+console.log('data', data) // data 0
+
+/* The logical shift operator will push 0s into every bit slot in the 64-bit word. */
