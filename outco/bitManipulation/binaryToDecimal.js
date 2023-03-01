b41fbda043677920aef36939b09ad8d69e0ce39c
grantferowich <g.ferowich@gmail.com>
2023-03-01 09:00:09 -0600

    improved test efficiency

--- outco/bitManipulation/binaryToDecimal.js
+++ outco/bitManipulation/binaryToDecimal.js
@@ -11,8 +11,12 @@ const binaryToDecimal = (binaryStr) => {
 /* TESTS */
 const binaryStr1 = '00000001';
 const result1 = binaryToDecimal(binaryStr1)
-console.log('result1', result1) // 1
+console.log('result 1:', result1) // result 1: 1
 
 const binaryStr2 = '00000101';
 const result2 = binaryToDecimal(binaryStr2)
-console.log('result2', result2) // 5
+console.log('result 2:', result2) // result 2: 5
+
+const binaryStr3 = '00011101';
+const result3 = binaryToDecimal(binaryStr3)
+console.log('result 3:', result3) // result 2 29
