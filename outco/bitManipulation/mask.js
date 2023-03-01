9fd5cd64eb024956988689f1831ebe2b7ec69691
grantferowich <g.ferowich@gmail.com>
2023-03-01 14:38:13 -0600

    improved test coverage

--- outco/bitManipulation/mask.js
+++ outco/bitManipulation/mask.js
@@ -3,13 +3,15 @@ Mask
 
 Successfully tested some masked values 3/01/23. 
 A note on 32-bit integers in JavaScript (courtesy of ChatGPT)
-However, when performing bitwise operations such as shifting or manipulating individual bits, 
-JavaScript does convert numbers to 32-bit signed integers. 
-This is because JavaScript uses the ECMAScript specification, 
-which defines bitwise operations on 32-bit signed integers.
+However, when performing bitwise operations such as shifting 
+or manipulating individual bits, JavaScript does convert
+numbers to 32-bit signed integers. This is because JavaScript 
+uses the ECMAScript specification, which defines bitwise 
+operations on 32-bit signed integers.
 
-So, while JavaScript uses 64-bit floating-point numbers to represent numbers in general,
-it does use 32-bit signed integers for bitwise operations.
+So, while JavaScript uses 64-bit floating-point numbers to 
+represent numbers in general, it does use 32-bit signed 
+integers for bitwise operations.
 
 */
 
@@ -32,15 +34,16 @@ console.log('negateMaskA:', negateMaskA)
 
 const maskB = decimalToBinary(3,32)
 console.log('maskB:', maskB)
-// maskB: 00000011
+// maskB: 00000000000000000000000000000011
 
 const maskC = decimalToBinary(4,32)
 console.log('maskC:', maskC)
-// maskB: 00000100
+// maskC: 00000000000000000000000000000100
 
 const maskD = decimalToBinary(-4,32)
 console.log('maskD:', maskD)
 console.log('maskD.length:', maskD.length)
+// maskD: 11111111111111111111111111111100
 
 const x = maskA << 3
 // console.log('mask', mask)
