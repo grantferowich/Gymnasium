d547e6bc99ccb6215c36dbe0c15c79ff622f0694
grantferowich <g.ferowich@gmail.com>
2023-03-01 10:48:15 -0600

    successfully defined match angles

--- outco/bitManipulation/getBit.js
+++ outco/bitManipulation/getBit.js
@@ -1,24 +1,37 @@
 /* 
 Get Bit
 
+input: a binary number, x
+output: true if the bit value at the xth index is 1
+        false if the bit value at the xth index is 0
 
 */
 
 
+//get bit returns whether or not the value at the xth index of the num is a 1 (true) or 0 (false)
 const getBit = (num, x) => {
-    return ((num && (1 << x)) !== 0)
+    let mask = 1 << x
+    console.log(`Num is ${num} while mask is ${mask}`)    
+    return ((num && mask) !== 0)
 }
 
-let data = getBit(00010000, 5) // 1
-// console.log('data', data)
+const data = getBit(0, 0) 
+console.log('data 1:', data) 
 
-let data2 = getBit(00010000, 1) // 0
-// console.log('data', data2)
+// Num is 0 while mask is 1
+// data 1: false
 
+const data2 = getBit(1,1)
+console.log('data 2:', data2) 
 
-// let num = 00010000
-// let x = 0
-console.log(00000000 << 1) // 0
-console.log(1 << 1) // 2
-console.log(00000100 << 1) // 16
+// Num is 1 while mask is 2
+// data 2: true
 
+const data3 = getBit(1,3)
+console.log('data 3:', data3) 
+
+// Num is 1 while mask is 8
+// data 3: true
+
+const data4 = getBit(16,0)
+console.log('data 4:', data4) 
---
+++ outco/stringAndArray/matchParens.js
@@ -0,0 +1,52 @@
+/* 
+
+Given a string of angle brackets, angles, such as "<<><>><<<>", write a function that adds angle brackets to the begginning and end 
+to make all the angle brackets match and return it. 
+
+The angle brackets "match" if for every < there is a corresponding >
+appearing after it in the string, and for ever > there is a corresponding < appearing before it in the string.
+
+angles: "><<><"
+output: "<><<><>>"
+
+*/
+// input: string containing <<>>
+// output: string containing <<>>
+// loop over the string
+// push < and > to stack
+// if top of stack is < and next is >, then pop the stack and continue iteration
+// if the top fo the stack is < and next is <, then push a > to the string
+//
+// input: "<<"
+// output: "<><>"
+// 
+// const solution = (angles) => {
+//     stack = ""
+
+//     for (let x = 0; x < angles.length; x++){
+//         let symbol = angles[x]
+// //         let stackTop = stack[stack.length -1]
+
+// //         if (stack[stack.length - 2] === "<" && stackTop === ">"){
+// //             console.log('hi 1')
+// //             continue
+// //         }
+// //         if (symbol === "<" && stackTop === ">"){
+// //             console.log('hi')
+// //             stack += symbol
+// //             stack += ">"
+// //         }
+        
+//         if (symbol === "<" && stack[stack.length-1] === "<"){
+//             console.log('hi2')
+//             stack += symbol
+//             stack += ">"
+            
+//         }
+
+    
+
+//     }
+//     return stack;
+// };
+
