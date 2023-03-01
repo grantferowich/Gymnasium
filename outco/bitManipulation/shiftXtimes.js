5e313845290e7c6df529d27dee1fa6fee788d31c
grantferowich <g.ferowich@gmail.com>
2023-03-01 15:42:07 -0600

    successfully represented numbers after performing bitwise shift operations on the numbers

--- outco/bitManipulation/shiftXtimes.js
+++ outco/bitManipulation/shiftXtimes.js
@@ -8,6 +8,7 @@ Shifting right one spot is the same as doubling the value.
     2 >> 1 => 4
     9 >> 1 => 18
     49 >> 1 => 98
+    1 >> 1 => 2
 
 */
 
@@ -45,3 +46,23 @@ console.log('a decimal:', a)
 
 console.log('a binary:', decimalToBinary(a, 32))
 // a binary: 00000000000000000000000001100010
+
+const shift1 = 1 << 1
+const shift2 = 1 << 2
+const shift3 = 1 << 3
+const shift4 = 1 << 4
+const shift5 = 1 << 5
+const shift6 = 1 << 6
+console.log('shift1:', shift1)
+console.log('shift2:', shift2)
+console.log('shift3:', shift3)
+console.log('shift4:', shift4)
+console.log('shift5:', shift5)
+console.log('shift6:', shift6)
+
+console.log('shift 1 binary:', decimalToBinary(shift1, 32))
+console.log('shift 2 binary:', decimalToBinary(shift2, 32))
+console.log('shift 3 binary:', decimalToBinary(shift3, 32))
+console.log('shift 4 binary:', decimalToBinary(shift4, 32))
+console.log('shift 5 binary:', decimalToBinary(shift5, 32))
+console.log('shift 6 binary:', decimalToBinary(shift6, 32))
