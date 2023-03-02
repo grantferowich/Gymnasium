7dbc7da6b3db000e18df73a60b76bcd5a9b85da3
grantferowich <g.ferowich@gmail.com>
2023-03-02 08:46:51 -0600

    restructured subdirectories

--- resources/PerformanceCalcs.js
+++
@@ -1,8 +0,0 @@
-const { performance } = require("perf_hooks");
-
-var t0 = performance.now();
-
-doSomething(); // <---- The function you're measuring time for
-
-var t1 = performance.now();
-console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
--- resources/colorsTest.js
+++
@@ -1,5 +0,0 @@
-require('colors');
-
-console.log(('\nThis is a success message.'.green))
-console.log(('\nThis is an error message.'.red))
-console.log('\nThis is a yellow warning message.'.yellow)
