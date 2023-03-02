8b507e44c995f249decd25e2f44994e6077128bf
grantferowich <g.ferowich@gmail.com>
2023-03-01 19:13:35 -0600

    removed loc

--- outco/helperMethodRecursion/mergeSortedArrays.js
+++ outco/helperMethodRecursion/mergeSortedArrays.js
@@ -14,7 +14,6 @@ function mergeSortedArrays(arr1, arr2) {
   let merged = [];
   let i = 0;
   let j = 0;
-
   // loop over both input arrays
   while (i < arr1.length && j < arr2.length) {
     // the array featuring a smaller element in the 0th place
@@ -29,7 +28,6 @@ function mergeSortedArrays(arr1, arr2) {
           j++;
       }
   }
-
   // suppose arr2 is 3 elements and arr1 is 5, 
   // i will be at 3
   // arr.length would be 5
@@ -38,8 +36,6 @@ function mergeSortedArrays(arr1, arr2) {
       merged.push(arr1[i]);
       i++;
   }
-
-
   // when arr2 is the longer array
   // loop over arr2's elements
   // appending them to the result array
@@ -47,14 +43,10 @@ function mergeSortedArrays(arr1, arr2) {
       merged.push(arr2[j]);
       j++;
   }
-
   // return result
   return merged;
 }
 
-
-
-// merge([1, 4, 7], [2, 3, 6, 9]) => [1, 2, 3, 4, 6, 7, 9]
 console.log(mergeSortedArrays([1,2,3,4,5], [6,8,10])) // [ 1, 2, 3, 4, 5, 6, 8, 10]
 console.log(mergeSortedArrays([1,4,5,7], [2, 3,5,6,9])) // [ 1, 2, 3, 4, 5, 5, 6, 7, 9]
 
