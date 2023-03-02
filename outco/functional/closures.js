9c5de3f61c0a630dffdd1a6f4be8129f097d7e6a
grantferowich <g.ferowich@gmail.com>
2023-03-02 08:56:56 -0600

    improved functional documentation

--- outco/functional/closures.js
+++ outco/functional/closures.js
@@ -1,3 +1,26 @@
+/* 
+Closures
+In JavaScript, a closure is a feature that allows a function 
+to access and remember variables from its outer scope even
+after the outer function has returned. Essentially, a closure
+is a function with access to its own private variables 
+and the variables in the scope chain of its outer function.
+
+A closure is created when a function is defined inside 
+another function and the inner function has access 
+to the outer function's variables. The inner function 
+can then be returned or passed as an argument to another 
+function and still retain access to the outer function's 
+variables.
+
+Closures are often used to create private variables and 
+functions, as well as to implement callback functions, 
+event handlers, and other advanced programming patterns. 
+They can also be used to create modular and reusable code, 
+and to improve the performance of certain operations by 
+avoiding the creation of unnecessary variables and functions.
+(h/t ChatGPT)
+*/
 const summation = arr => {
   let sum = 1;
 
