43331e5114a250f5476c7d1571b09bd106dcec8b
grantferowich <g.ferowich@gmail.com>
2023-03-03 08:19:29 -0600

    successfully added functions

--- outco/bitManipulation/binaryToString-5.02.js
+++ outco/bitManipulation/binaryToString-5.02.js
@@ -3,17 +3,9 @@ Binary to String
 
 CTCI 5.02
 
-input: a number between 0 and 1 
-output: a binary representation of the number 
-
-Every decimal between 0 and 1 may be represented as
-the binary representation of the number * 100, 
-which produces the numerator of the final result.
-The denominator of the final result is given by the binary
-representation of 100, which is 1100100.
-
 Input: 0.16
 Output: 
+
 */
 
 const binaryToString = (binaryNumber) => {
@@ -24,4 +16,3 @@ const binaryToString = (binaryNumber) => {
 
 const data = binaryToString(0.16) 
 console.log('Output:', data)
-// Output = 10000/1100100
