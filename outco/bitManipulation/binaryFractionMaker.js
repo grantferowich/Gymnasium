6dd489c7da781fe34620b6f66d89f796fe5fa849
grantferowich <g.ferowich@gmail.com>
2023-03-03 08:12:36 -0600

    defined binary fraction maker tests

--- outco/bitManipulation/binaryFractionMaker.js
+++ outco/bitManipulation/binaryFractionMaker.js
@@ -1,10 +1,20 @@
 /* 
 Binary Fraction Maker
 
-I originally wrote this function for question 5.02 in
-Cracking the Coding Interview.
+*I originally wrote this function for question 5.02 in
+Cracking the Coding Interview.* 
+
+input: a number between 0 and 1 
+output: a binary representation of the number 
+
+Every decimal between 0 and 1 may be represented as
+the binary representation of the number * 100, 
+which produces the numerator of the final result.
+The denominator of the final result is given by the binary
+representation of 100, which is 1100100.
 
 Successfully tested the function 03/03/23.
+
 */
 
 const binaryToString = (binaryNumber) => {
