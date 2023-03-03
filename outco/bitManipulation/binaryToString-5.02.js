53fce51063f1d8929f88484d2ade7d6b176891c4
grantferowich <g.ferowich@gmail.com>
2023-03-03 09:13:54 -0600

    successfully tested binaryToString-5.02

--- outco/bitManipulation/binaryToString-5.02.js
+++ outco/bitManipulation/binaryToString-5.02.js
@@ -3,16 +3,29 @@ Binary to String
 
 CTCI 5.02
 
-Input: 0.16
-Output: 
+Input: 0.04, a decimalNumber
+Output: 0.100
 
 */
 
-const binaryToString = (binaryNumber) => {
-
+const binaryToString = (decimalNumber) => {
+    if (decimalNumber >= 1 || decimalNumber <= 0){
+        return 'ERROR'
+    }
+    let numberAfterDecimalInt = parseInt(decimalNumber.toString().split(".")[1])
+    let numberAfterDecimalStr = numberAfterDecimalInt.toString(2)
+    return "0."+ numberAfterDecimalStr
 }
 
 /* TESTS */
 
+
+const invalidData = binaryToString(-0.16) 
+console.log('Output 1:', invalidData)
+// Output: ERROR
+
 const data = binaryToString(0.16) 
-console.log('Output:', data)
+console.log('Output 2:', data)
+
+const data3 = binaryToString(0.04) 
+console.log('Output 3:', data3)
