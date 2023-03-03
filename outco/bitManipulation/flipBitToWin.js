63ef29f5d3073583370f23a462bb404e76a3753c
grantferowich <g.ferowich@gmail.com>
2023-03-03 15:49:01 -0600

    successfully tested getBitToWin

--- outco/bitManipulation/flipBitToWin.js
+++ outco/bitManipulation/flipBitToWin.js
@@ -1,6 +1,8 @@
 /* 
 Flip Bit to Win
 
+Successfully tested the function 3/23/23.
+
 Question: You have an integer and you can flip exactly one bit from a 0
 to a 1. Write code to find the length of the longest sequence of 1s you could 
 create.
