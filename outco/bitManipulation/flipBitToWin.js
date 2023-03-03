e0d70c729e6fca345a610dcd96999696f593f297
grantferowich <g.ferowich@gmail.com>
2023-03-03 11:57:13 -0600

    successfully tested the flipBitToWin(x) function

--- outco/bitManipulation/flipBitToWin.js
+++ outco/bitManipulation/flipBitToWin.js
@@ -12,14 +12,48 @@ Example:
 Input: 1775 ( or 11011101111)
 Output: 8
 
-
+Complexity analysis
+Time complexity: O(N)
+Space complexity: O(1)
 
 */
 
 const flipBitToWin = (decimalNumber) => {
-    let binaryNumber = decimalNumber.toString(2)
-    return binaryNumber
+    let binaryNumber = decimalNumber.toString(2);
+    let maxCount = -Infinity;
+    let flipBudget = 1;
+    let lPtr = 0;
+    let rPtr = 0
+    let result = 0
+    while (rPtr < binaryNumber.length){
+
+        // hunt phase 
+        if (binaryNumber[rPtr] !== "1"){
+            flipBudget--;
+        }
+
+        // catch up phase
+        if (flipBudget < 0){
+            result = rPtr - lPtr;
+            maxCount = Math.max(maxCount, result)
+            while (binaryNumber[lPtr] !== "0"){
+                lPtr++
+            } 
+            lPtr++
+            flipBudget++
+        }
+        rPtr++
+    }
+    return Math.max(maxCount, rPtr-lPtr + flipBudget) 
 }
 
-let decimal = 1775
+/*  TESTS */
+
+const decimal = 1775
 console.log(flipBitToWin(decimal)) // 8
+
+const decimal2 = 7
+console.log(flipBitToWin(decimal2)) // 4
+
+const decimal3 = 6
+console.log(flipBitToWin(decimal3)) // 3
