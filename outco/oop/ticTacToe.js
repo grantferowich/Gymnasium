ad29bc8b1b0f9fad85593da2144dad8e229a2aa2
grantferowich <g.ferowich@gmail.com>
2023-03-06 18:32:37 -0600

    instantiated class and ran game

--- outco/oop/ticTacToe.js
+++ outco/oop/ticTacToe.js
@@ -6,8 +6,6 @@ Class TicTacToe
     - number of rounds (the max number is the number of spaces on the board, 9)
     - a new board
 
-Class Board
-    - storage of places players can place Xs and 0s
 
 
 
@@ -30,6 +28,14 @@ class Board{
     constructor(){
         this.storage =[[' ', ' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']]
     }
+
+    printBoard(){
+        process.stdout.write(' 0 1 2\n')
+        process.stdout.write(' _ _ _\n')
+        this.storage.forEach((line, row) => {
+            process.stdout.write(`${row}`)
+        })
+    }
 }
 class TicTacToe{
     constructor(){
@@ -37,4 +43,20 @@ class TicTacToe{
         this.numberOfRounds = 9;
         this.board = new Board()
     }
+
+
+
+    runGame(){
+        process.stdout.write("Let's play Tic Tac Toe \n\n")
+        process.stdout.write("To play Tic Tac Toe, input two numbers: row column \n ")
+        process.stdout.write("For example: 0 0 \n\n")
+        // new method
+
+    }
 }
+
+// Instantiate class 
+let game = new TicTacToe()
+
+// Run the game
+game.runGame()
