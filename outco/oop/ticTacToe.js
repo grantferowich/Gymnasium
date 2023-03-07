837f4151cc53666219c4373f35bdef855663c712
grantferowich <g.ferowich@gmail.com>
2023-03-06 18:21:13 -0600

    defined board class methods

--- outco/oop/ticTacToe.js
+++ outco/oop/ticTacToe.js
@@ -1,3 +1,40 @@
 /* 
 Create a game called Tic Tac Toe.
+
+Class TicTacToe
+    - currentPlayer 
+    - number of rounds (the max number is the number of spaces on the board, 9)
+    - a new board
+
+Class Board
+    - storage of places players can place Xs and 0s
+
+
+
+    |-----------------------------|
+    | class Board                 |
+    |-----------------------------|
+    |- this.storage               |
+    |-----------------------------|
+    |- printBoard()               |
+    |- canPlacePiece(row,col)     |
+    |- placePiece(row,col,player) |
+    |- checkWinCondition(player)  |
+    |- checkDiagonals(player)     |
+    |- checkRows(player)          |
+    |- checkColumns(player)       |
+    |-----------------------------|
 */
+
+class Board{
+    constructor(){
+        this.storage =[[' ', ' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']]
+    }
+}
+class TicTacToe{
+    constructor(){
+        this.currentPlayer = 'X';
+        this.numberOfRounds = 9;
+        this.board = new Board()
+    }
+}
