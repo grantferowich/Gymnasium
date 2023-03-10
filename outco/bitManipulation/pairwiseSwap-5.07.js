436e77c3e211a1bd26d7257b5c87cf3f184c8caa
grantferowich <g.ferowich@gmail.com>
2023-03-10 11:39:01 -0600

    appended test status

--- outco/bitManipulation/pairwiseSwap-5.07.js
+++ outco/bitManipulation/pairwiseSwap-5.07.js
@@ -1,6 +1,10 @@
 /* 
 Pairwise Swap
 
+CTCI 5.07
+
+Successfully tested the function 3/10/23.
+
 Write a program to swap odd and even bits in an integer with
 as few instructions as possible (e.g., bit 0 and bit 1 are swapped,
 bit 2 and bit 3 are swapped, and so on.)
