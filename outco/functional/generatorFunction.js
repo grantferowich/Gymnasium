5a84f285a2a448ae03beae8bd938610214d19c7f
grantferowich <g.ferowich@gmail.com>
2023-03-12 17:14:17 -0500

    documented test expectations

--- outco/functional/generatorFunction.js
+++ outco/functional/generatorFunction.js
@@ -1,6 +1,8 @@
 /* 
 Generator Function
 
+Successfully tested the function 3/12/23.
+
 A generator function may be paused and resumed during
 execution.
 
@@ -13,7 +15,7 @@ function* increment() {
 }
 
 const generator = increment()
-console.log(generator.next().value); 
-console.log(generator.next().value); 
-console.log(generator.next().value); 
-console.log(generator.next().done); 
+console.log(generator.next().value); // 0
+console.log(generator.next().value); // 1
+console.log(generator.next().value); // 2
+console.log(generator.next().done); // true
