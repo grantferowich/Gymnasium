e0ac0074ad0d636ebe7f0d778332091156d17f1d
grantferowich <g.ferowich@gmail.com>
2023-03-13 18:02:25 -0500

    successfully tested implementation of longestSubstringWithoutRepeatingChars

---
+++ outco/movingWindow/minimumWindowSubstringXX.js
@@ -0,0 +1,14 @@
+/* 
+
+Minimum Window Substring
+
+// minimum, not longest
+could have duplicate letters, no duplicate 
+ 
+need to cover all letters from t
+Map<Letter, Frequency>
+t = AABC
+A:2, B:1, C:1
+
+*/
+
