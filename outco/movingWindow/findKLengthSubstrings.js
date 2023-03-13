1dd28e425f2ca84d73d77e8d3f6529d1510c7682
grantferowich <g.ferowich@gmail.com>
2023-03-13 08:04:55 -0500

    defined test cases for findKLengthSubstrings

--- outco/movingWindow/findKLengthSubstrings.js
+++ outco/movingWindow/findKLengthSubstrings.js
@@ -1,5 +1,6 @@
 /* 
 1100. Find K-Length Substrings With No Repeated Characters
+Medium
 Premium
 
 Given a string s and an integer k, return the number of substrings
@@ -8,10 +9,25 @@ in s of length k with no repeated characters.
 
 s = 'havefunonleetcode', k = 5
 output: 6
-
+Explanation: The six substrings are: 'havef',
+'avefu', 'vefun', 'efuno, 'etcod', 'tcode'.
 
 
 s = 'home', k = 5
 output: 0
 -> k cannot be larger than substring
 */
+
+const findKLengthSubstrings = (inputStr, kInt) => {
+
+
+
+}
+
+/* TESTS */
+
+let result1 = findKLengthSubstrings('havefunonleetcode', 5)
+console.log('Test 1:', result1)
+
+let result2 = findKLengthSubstrings('home', 5)
+console.log('Test 2:', result2)
