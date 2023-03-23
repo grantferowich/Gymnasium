5e96cd8b768645288c9ab565696ce73cb44345b4
grantferowich <g.ferowich@gmail.com>
2023-03-23 09:27:41 -0500

    appended test status

--- outco/functional/createA2DArray.js
+++ outco/functional/createA2DArray.js
@@ -1,5 +1,7 @@
 /* Create a 2-D Array 
 
+Successfully tested the method 03/23/2023.
+
 Suppose you want to create the following 2 x 10 array of arrays.
 [
   [ '', '', '', '', '', '', '', '', '', '' ],
