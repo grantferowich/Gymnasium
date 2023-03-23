50c13159c6fd6daed451811c2493a581c7a64854
grantferowich <g.ferowich@gmail.com>
2023-03-23 13:22:29 -0500

    appended test status

--- outco/dynamicProgramming/coins-8.11.js
+++ outco/dynamicProgramming/coins-8.11.js
@@ -3,6 +3,8 @@ Coins
 
 CTCI 8.11
 
+Successfully tested the function 03/23/2023.
+
 Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and
 pennies (1 cent), write code to calculate the number of ways of representing n cents.
 
