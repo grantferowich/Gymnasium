d281508298794ca210684ac396a1fcc24bc17588
grantferowich <g.ferowich@gmail.com>
2023-03-28 07:53:40 -0500

    successfully tested concatenation of array

---
+++ outco/stringAndArray/concatenationOfArray.js
@@ -0,0 +1,46 @@
+/* 
+Concatenation of Array
+
+Successfully tested the function 03/28/23
+https://leetcode.com/problems/concatenation-of-array/submissions/923604136/
+
+
+*/
+
+
+const getConcatenation = function(nums) {
+    let numsCopyArr = [...nums]
+    return nums.concat(numsCopyArr)
+};
+
+/* TESTS */
+const inputArr = [[1,2,1], [1,3,2,1]]
+
+const runTests = (array) => {
+    // generate date on which tests were run
+    const todayDate = new Date()
+    const dayInt = todayDate.getDate()
+    const monthInt = todayDate.getMonth() + 1
+    const yearInt = todayDate.getFullYear()
+
+
+    let xInt = 0
+    while (xInt < inputArr.length){
+        let testIntputInt = inputArr[xInt]
+        let resultInt = getConcatenation(testIntputInt)
+        console.log(`Test ${xInt}: ${resultInt}`)
+        xInt++
+    }
+    
+    console.log(`✓ Successfully tested the function ${monthInt}/${dayInt}/${yearInt}`);
+}
+
+runTests(inputArr) // 
+
+/* TEST RESULTS 
+
+Test 0: 1,2,1,1,2,1
+Test 1: 1,3,2,1,1,3,2,1
+
+*/
+
