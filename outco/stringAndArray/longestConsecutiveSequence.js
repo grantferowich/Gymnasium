6ad4e1b57e8fc2f999e2b4f3eb7d2ca2e6f4f4c3
grantferowich <g.ferowich@gmail.com>
2023-04-04 12:18:11 -0500

    saved alternative implementation of longestConsecutive

--- outco/stringAndArray/longestConsecutiveSequence.js
+++ outco/stringAndArray/longestConsecutiveSequence.js
@@ -25,10 +25,12 @@ const longestConsecutive = (numsArr) => {
     let xInt = 0
     let maxConsecutive = 1;
     let tempConsecutive = 1;
+    let numsMap = new Map()
     // get minInt and maxInt of arr
     while (xInt < numsArr.length){
         minInt = Math.min(minInt, numsArr[xInt])
         maxInt = Math.max(maxInt, numsArr[xInt])
+        numsMap.set(numsArr[xInt], true)
         xInt++
     }
     // console.log(`minInt ${minInt} maxInt ${maxInt}`)
@@ -36,10 +38,10 @@ const longestConsecutive = (numsArr) => {
     xInt = minInt
     while (xInt < numsArr.length){
         
-        if (numsArr.includes(xInt) && numsArr.includes(xInt+1)){
+        if (numsMap.has(numsArr[xInt]) && numsMap.has(numsArr[xInt] + 1)){
             tempConsecutive++
         }
-        if (!numsArr.includes(xInt+1)){
+        if (!numsMap.has(numsArr[xInt+1])){
             tempConsecutive = 1
         }
         if (tempConsecutive > maxConsecutive){
@@ -67,3 +69,29 @@ console.log(`Result 3: ${result3}`)
 const numsArr4 = [0,1,2,4,8,5,6,7,9,3,55,88,77,99,999999999]
 const result4 = longestConsecutive(numsArr4)
 console.log(`Result 4: ${result4}`)
+
+const numsArr5 = [-1000000000,1,2,3,9,1000000000]
+const result5 = longestConsecutive(numsArr5)
+console.log(`Result 5: ${result5}`)
+
+
+/* Alternative implementation courtesy ChatGPT */
+const longestConsecutive2 = (nums) => {
+    if (nums.length === 0) {
+        return 0;
+    }
+    const numSet = new Set(nums);
+    let maxConsecutive = 1;
+    for (const num of numSet) {
+        if (!numSet.has(num - 1)) { // only start counting from the smallest number in a consecutive sequence
+            let tempConsecutive = 1;
+            let currentNum = num;
+            while (numSet.has(currentNum + 1)) { // count consecutive numbers in sequence
+                tempConsecutive++;
+                currentNum++;
+            }
+            maxConsecutive = Math.max(maxConsecutive, tempConsecutive);
+        }
+    }
+    return maxConsecutive;
+};
