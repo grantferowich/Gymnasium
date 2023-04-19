ab6ff0d82fa6bc34fc6b4cdd335ca6af2da86706
grantferowich <g.ferowich@gmail.com>
2023-04-19 07:32:41 -0500

    defined approach

--- outco/frequencyCount/getDuplicatesX2.js
+++ outco/frequencyCount/getDuplicatesX2.js
@@ -16,4 +16,13 @@
  *  [1, 2, 4, 2] --> [2]
  *  [3, 2, 3, 2, 3, 3, 4] --> [3, 2]
  *  [1, 2, 3, 4] --> []
+ * 
+ *  init charFrequencyMap<char, frequencyInt>
+ *  loop over the array
+ *  add chars to the charFrequencyMap 
+ *  if the char exists in the map, increment the frequency integer by 1
+ * 
+ *  loop over each key-value pair in the map
+ *  if the frequency/value is greater than 1
+ *  append the key to the output array
  */
