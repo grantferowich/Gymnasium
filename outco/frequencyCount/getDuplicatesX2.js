39129e8ef83f93191cd797ca54c5fdbac79f8778
grantferowich <g.ferowich@gmail.com>
2023-04-19 07:29:57 -0500

    defined get duplicates question

---
+++ outco/frequencyCount/getDuplicatesX2.js
@@ -0,0 +1,19 @@
+/**
+ *  Get Duplicates
+ *
+ *  Given an array of values, return only the values that have duplicates in the
+ *  array
+ *
+ *  Parameters
+ *  Input: arr {Array}
+ *  Output: {Array}
+ *
+ *  Constraints
+ *  Time: O(N)
+ *  Space: O(N)
+ *
+ *  Examples
+ *  [1, 2, 4, 2] --> [2]
+ *  [3, 2, 3, 2, 3, 3, 4] --> [3, 2]
+ *  [1, 2, 3, 4] --> []
+ */
