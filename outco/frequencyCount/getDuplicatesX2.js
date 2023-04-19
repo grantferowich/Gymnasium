b529b2367005f6b2161e879f9f54d9aca9d5e1c4
grantferowich <g.ferowich@gmail.com>
2023-04-19 07:54:11 -0500

    successfully tested getDuplicatesX2 on 04/19/23

--- outco/frequencyCount/getDuplicatesX2.js
+++ outco/frequencyCount/getDuplicatesX2.js
@@ -26,3 +26,40 @@
  *  if the frequency/value is greater than 1
  *  append the key to the output array
  */
+
+const getDuplicates = (arr) =>{
+    let charFrequencyMap = new Map();
+    let outputArr = []
+    let xInt = 0;
+    while (xInt < arr.length){
+        let key = arr[xInt]
+        if (charFrequencyMap.has(key)){
+            let frequencyInt = charFrequencyMap.get(key)
+            charFrequencyMap.set(key, frequencyInt+1)
+        }
+        if (!charFrequencyMap.has(key)){
+            charFrequencyMap.set(key, 1)
+        }
+        xInt++
+    }
+
+    for (let [key, value] of charFrequencyMap){
+        if (value > 1){
+            outputArr.push(key)
+        }
+    }
+    return outputArr
+}
+
+/* TESTS */
+const data1 = [1, 2, 4, 2] // --> [2]
+const test1 = getDuplicates(data1)
+console.log(`Result 1: ${test1}`)
+
+const data2 =  [3, 2, 3, 2, 3, 3, 4] // --> [3, 2]
+const test2 = getDuplicates(data2)
+console.log(`Result 2: ${test2}`)
+
+const data3 = [1, 2, 3, 4] // --> []
+const test3 = getDuplicates(data3)
+console.log(`Result 3: ${test3}`)
