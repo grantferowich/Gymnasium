ca910fbb76450ca4ef1af235ee5d543a11ef4a50
grantferowich <g.ferowich@gmail.com>
2023-04-21 07:14:29 -0500

    successfully tested anagram palindrome

--- outco/frequencyCount/anagramPalindromeX2.js
+++ outco/frequencyCount/anagramPalindromeX2.js
@@ -22,4 +22,48 @@
  *  Examples
  *  `"carrace" --> true ("carrace" can be rearranged to "racecar")`
  *  `"cat" --> false`
+ * 
+ * init a charFrequencyMap
+ * init a variable flag
+ * if the is one instance of a frequency of 1 increment the flag by 1
+ * return flag <== 1
+ * 
  */
+
+const anagramPalindrome = (str1) =>{
+
+    let xInt = 0;
+    let charFrequencyMap = new Map();
+    let flagInt = 0;
+
+    while (xInt < str1.length){
+        let char = str1[xInt]
+        if (charFrequencyMap.has(char)){
+            let frequencyInt = charFrequencyMap.get(char)
+            charFrequencyMap.set(char, frequencyInt+1)
+        }
+
+        if (!charFrequencyMap.has(char)){
+            charFrequencyMap.set(char, 1)
+        }
+        xInt++
+    }
+
+    for (let [key, value] of charFrequencyMap){
+        if (value < 2){
+            flagInt++
+        }
+    }
+    return flagInt <= 1
+
+}
+
+/* TESTS */
+
+const data1 = 'carrace'
+const result1 = anagramPalindrome(data1)
+console.log(`Result 1: ${result1}`)
+
+const data2 = 'cat'
+const result2 = anagramPalindrome(data2)
+console.log(`Result 2: ${result2}`)
