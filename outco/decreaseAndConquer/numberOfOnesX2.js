e025f56cc08dafbaa8142aec622cc5149fa5f2db
grantferowich <g.ferowich@gmail.com>
2023-04-22 08:55:54 -0500

    successfully tested number of ones

--- outco/decreaseAndConquer/numberOfOnesX2.js
+++ outco/decreaseAndConquer/numberOfOnesX2.js
@@ -29,13 +29,14 @@ const numberOfOnes = (arr) => {
 
     while (leftPtrInt < rightPtrInt){
         let midPtrInt = Math.floor((rightPtrInt - leftPtrInt)/2) + leftPtrInt
-        // if (arr[midPtrInt] === 1 && arr[midPtrInt - 1] === 0){
-        //     return arr.length - midPtrInt
-        // }
 
         console.log(`midPtrInt ${midPtrInt}; leftPtrInt ${leftPtrInt}; rightPtrInt ${rightPtrInt}`)
         console.log(`arr[midPtrInt] ${arr[midPtrInt]}`)
+
         if (arr[midPtrInt] === 1){
+            while (arr[midPtrInt - 1] > 0 && arr[midPtrInt] === 1){
+                midPtrInt--
+            }
             return arr.length - midPtrInt
         }
         if (arr[midPtrInt] < 1){
@@ -55,10 +56,10 @@ const data1 = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
 const data2 = [0, 0, 0]
 const data3 = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1] 
 
-// const result1 = numberOfOnes(data1)
-// const result2 = numberOfOnes(data2)
+const result1 = numberOfOnes(data1)
+const result2 = numberOfOnes(data2)
 const result3 = numberOfOnes(data3)
 
-// console.log(`Result 1: ${result1}`)
-// console.log(`Result 2: ${result2}`)
+console.log(`Result 1: ${result1}`)
+console.log(`Result 2: ${result2}`)
 console.log(`Result 3: ${result3}`)
