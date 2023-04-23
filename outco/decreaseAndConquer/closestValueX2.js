fd1322154d40cd28cb29b971b13d743ed52aaeeb
grantferowich <g.ferowich@gmail.com>
2023-04-23 07:48:41 -0500

    defined question

---
+++ outco/decreaseAndConquer/closestValueX2.js
@@ -0,0 +1,21 @@
+/*
+ * ## Closest Value
+ *
+ * *Given a sorted bit array of integers, and a target value, find the number in the array that is closest to the target.*
+ *
+ * **Parameters**
+ * Input: arr {Array of Integers}
+ * Input: target {Integer}
+ * Output: {Integer}
+ *
+ * **Constraints**
+ * If there are two numbers tied for the closest value, return the lowest value.
+ *
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `[1, 2, 3, 5, 5, 7, 9, 10, 11], 6 --> 5`
+ * `[1, 2, 3], 8 --> 3`
+ * `[1, 10, 22, 59, 67, 72, 100], 70 --> 72`
+ */
