54a818cc96aad9cd8a1ba21392d605a69f6d3e4d
grantferowich <g.ferowich@gmail.com>
2023-04-25 07:32:40 -0500

    defined approach in psuedocode

--- outco/decreaseAndConquer/closestValueX2.js
+++ outco/decreaseAndConquer/closestValueX2.js
@@ -17,5 +17,13 @@
  * **Examples**
  * `[1, 2, 3, 5, 5, 7, 9, 10, 11], 6 --> 5`
  * `[1, 2, 3], 8 --> 3`
- * `[1, 10, 22, 59, 67, 72, 100], 70 --> 72`
+ * `[1, 10, 22, 59, 67, 72, 100], 70 --> 72`'
+ * 
+ * look at the middle number
+ * store the difference between the middle number and the target number as differenceInt
+ * if the present differenceInt is smaller than the ultimateDifferenceInt update ultimateDifferenceInt and closestInt
+ * if the middle number is greater than or equal to the target, reset leftPtrInt
+ * if the middle number is less than the target, reset rightPtrInt
  */
+
+
