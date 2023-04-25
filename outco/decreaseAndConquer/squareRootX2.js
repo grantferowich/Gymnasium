43d4211668a30485750502d8054f3b98479db889
grantferowich <g.ferowich@gmail.com>
2023-04-25 14:05:19 -0500

    defined question

---
+++ outco/decreaseAndConquer/squareRootX2.js
@@ -0,0 +1,24 @@
+/*
+ * ## Square Root
+ *
+ * *Given an positive integer, find the square root.*
+ *
+ * **Parameters**
+ * Input: value {Integer}
+ * Output: {Float}
+ *
+ * **Constraints**
+ * Do not use a native built in method.
+ * Ensure the result is accurate to 6 decimal places (0.000001)
+ *
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `4 --> 2.0`
+ * `98 --> 9.899495`
+ * `14856 --> 121.885192
+ * 
+ * 
+ * 
+ */
