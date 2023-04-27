a0cad69f72dc16e9e05dfaccdba2c7659b5cda2c
grantferowich <g.ferowich@gmail.com>
2023-04-26 21:41:35 -0500

    expanded test coverage

--- outco/decreaseAndConquer/squareRootX2.js
+++ outco/decreaseAndConquer/squareRootX2.js
@@ -32,13 +32,17 @@
  */
 
 const squareRoot = (int) => {
+    if (int === 0 || int === 1){
+        return int
+    }
+
     let leftInt = 0
     let rightInt = int
 
     if (int < 0){
         return 'The square root of a negative number does not exist.'
     }
-    
+
     while (leftInt < rightInt){
         let midInt = ((leftInt + rightInt)/2)
         let squareInt = midInt * midInt
@@ -62,16 +66,22 @@ const int1 = 4
 const int2 = 98
 const int3 = 14856
 const int4 = -4
+const int5 = 0
+const int6 = 1
 
 const result1Int = squareRoot(int1)
 const result2Int = squareRoot(int2)
 const result3Int = squareRoot(int3)
 const result4Int = squareRoot(int4)
+const result5Int = squareRoot(int5)
+const result6Int = squareRoot(int6)
 
 console.log(`Result 1: ${result1Int}`)
 console.log(`Result 2: ${result2Int}`)
 console.log(`Result 3: ${result3Int}`)
 console.log(`Result 4: ${result4Int}`)
+console.log(`Result 5: ${result5Int}`)
+console.log(`Result 6: ${result6Int}`)
 
 /* TEST RESULTS
 
@@ -79,5 +89,7 @@ Result 1: 2.000000
 Result 2: 9.899495
 Result 3: 121.885192 
 Result 4: The square root of a negative number does not exist.
+Result 5: 0
+Result 6: 1
 
 */
