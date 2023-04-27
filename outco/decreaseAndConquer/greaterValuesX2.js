f48ce8dbab126b65c5003ab020ddc915ca305ff7
grantferowich <g.ferowich@gmail.com>
2023-04-27 14:10:06 -0500

    defined question

---
+++ outco/decreaseAndConquer/greaterValuesX2.js
@@ -0,0 +1,21 @@
+/*
+ * ## Greater Values
+ *
+ * *Given a sorted array of integers, and a target value return the number of values greater the target.*
+ *
+ * **Parameters**
+ * Input: arr {Array of Integers}
+ * Input: target {Integer}
+ * Output: {Integer}
+ *
+ * **Constraints**
+ *
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `[1, 2, 3, 5, 5, 7, 9, 10, 11], 5 --> 4`
+ * `[1, 2, 3], 4 --> 0`
+ * `[1, 10, 22, 59, 67, 72, 100], 13 --> 5`
+ *
+ */
