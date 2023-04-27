79173758592f66d7611920dca967a04a262b44ce
grantferowich <g.ferowich@gmail.com>
2023-04-27 13:30:40 -0500

    successfully tested letterSortX2 on April 27, 2023

--- outco/frequencyCount/letterSortX2.js
+++ outco/frequencyCount/letterSortX2.js
@@ -29,4 +29,61 @@
  * get the charCodeInt with arrayLocationInt + 97
  * get charCodeStr from CharCodeInt with CharCodeFrom(charCodeInt)
  * while xInt is less than valueInt append charCodeStr to outputStr 
+ * 
  */
+
+
+const letterSort = (str) => {
+    let fixedArr = new Array(26).fill(0);
+    let outputStr = '';
+    let xInt = 0;
+
+    while (xInt < str.length){
+        let char = str[xInt];
+        let charCodeInt = char.charCodeAt(0);
+        
+        let arrayLocationInt = charCodeInt - 97;
+        let tempValInt = fixedArr[arrayLocationInt] 
+        tempValInt += 1
+        fixedArr[arrayLocationInt] = tempValInt
+        xInt++
+    }
+
+    xInt = 0 
+    while (xInt < fixedArr.length){
+        let valueInt = fixedArr[xInt]
+        let yPtrInt = 0
+        charCodeInt = 97 + xInt
+        let char = String.fromCharCode(charCodeInt)
+ 
+        if (valueInt > 0){
+            while (yPtrInt < valueInt){
+                outputStr += char
+                yPtrInt++
+            }
+        }
+        xInt++
+    }
+    return outputStr
+}
+
+/* TESTS */
+const str1 = 'hello'
+const str2 = 'whiteboard'
+const str3 = 'one'
+
+const resultStr1 = letterSort(str1)
+const resultStr2 = letterSort(str2)
+const resultStr3 = letterSort(str3)
+
+console.log(`Result 1: ${resultStr1}`)
+console.log(`Result 2: ${resultStr2}`)
+console.log(`Result 3: ${resultStr3}`)
+
+/* TEST RESULTS 
+
+Result 1: ehllo
+Result 2: abdehiortw
+Result 3: eno
+
+*/
