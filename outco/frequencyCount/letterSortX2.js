2ea45391fd240b49622622a77068394ce2844ec3
grantferowich <g.ferowich@gmail.com>
2023-04-26 21:43:03 -0500

    defined question

---
+++ outco/frequencyCount/letterSortX2.js
@@ -0,0 +1,18 @@
+/**
+ * Letter Sort
+ *
+ * Given a string of letters, return the letters in sorted order.
+ *
+ * Parameters
+ * Input: str {String}
+ * Output: {String}
+ *
+ * Constraints
+ * Time: O(N)
+ * Space: O(N)
+ *
+ * Examples
+ * `hello --> ehllo`
+ * `whiteboard --> abdehiortw`
+ * `one --> eno`
+ */
