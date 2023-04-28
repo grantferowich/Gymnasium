1552308543a0942a9af25274942fddc1bd8bf9fc
grantferowich <g.ferowich@gmail.com>
2023-04-28 07:56:44 -0500

    successfully tested printArray

---
+++ outco/helperMethodRecursion/printArrayX2.js
@@ -0,0 +1,31 @@
+/*
+ * 2a. Print each item in an array in order
+ *
+ * Input:   arr {Array}
+ * Output:  {undefined}
+ *
+ * Example: printArray([1,2,3]) =>
+ *          1
+ *          2
+ *          3
+ */
+
+const printArray = (arr) => {
+
+    const printOne = (arr, depth) => {
+        if (depth === arr.length){
+            return
+        }
+        console.log(arr[depth])
+        printOne(arr, depth + 1)
+    }
+    printOne(arr, 0)
+
+}
+
+/* TESTS */
+
+const arr1 = [1, 2, 3]
+
+const result1 = printArray(arr1)
+// console.log(`Result 1: ${result1}`)
