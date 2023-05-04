703b7c320ad9cf8c7dc12dc14fa3fe1d3b248429
grantferowich <g.ferowich@gmail.com>
2023-05-04 14:38:40 -0500

    defined question

---
+++ outco/movingWindow/consecutiveSubarraySumX2.js
@@ -0,0 +1,31 @@
+/* 
+222 - Consecutive Subarray Sum
+
+Given an array of positive integers and a target value, 
+return true if there is a subarray of consecutive 
+elements that sum up to this target value.
+
+Input: Array of integers, target value
+Output: Boolean
+
+Constraints: O(N) time complexity, O(1) auxiliary space complexity
+
+Input: [6,12,1,7,5,2,3], 14      	=>	Output: true (7+5+2)
+Input: [8,3,7,9,10,1,13], 50		=>	Output: false
+*/
+
+const consecutiveSubarraySum = (arr, targetInt) => {
+
+
+
+}
+
+/* Test */
+
+const arr1 = [6,12,1,7,5,2,3]
+const int1 = 14
+
+const arr2 = [8,3,7,9,10,1,13]  =>	Output: true (7+5+2)
+const int2 = 50		=>	Output: false
+
+/* Test Result */
--- outco/movingWindow/maxConsecutiveSumX2.js
+++ outco/movingWindow/maxConsecutiveSumX2.js
@@ -19,9 +19,10 @@
  */
 
 const maxConsecutiveSum = (arr) => {
-    let tempSum = - Infinity
-    let maxSum = -Infinity
-    let xInt = 0
+    
+    let tempSum = - Infinity;
+    let maxSum = -Infinity;
+    let xInt = 0;
 
     while (xInt < arr.length){
         tempSum = Math.max(tempSum + arr[xInt], arr[xInt]);
@@ -29,28 +30,32 @@ const maxConsecutiveSum = (arr) => {
         xInt++;
     }
 
-    return maxSum !== -Infinity ? maxSum : 0 
+    return maxSum !== -Infinity ? maxSum : 0;
+
 }
 
 /* Tests */
 
-const arr1 = [6, -1, 3, 5, -10]
-const arr2 = [5]
-const arr3 = []
+const arr1 = [6, -1, 3, 5, -10];
+const arr2 = [5];
+const arr3 = [];
+const arr4 = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
-
-const result1Int = maxConsecutiveSum(arr1)
-const result2Int = maxConsecutiveSum(arr2)
-const result3Int = maxConsecutiveSum(arr3)
+const result1Int = maxConsecutiveSum(arr1);
+const result2Int = maxConsecutiveSum(arr2);
+const result3Int = maxConsecutiveSum(arr3);
+const result4Int = maxConsecutiveSum(arr4);
 
 console.log(`Result 1: ${result1Int}`)
 console.log(`Result 2: ${result2Int}`)
 console.log(`Result 3: ${result3Int}`)
+console.log(`Result 4: ${result4Int}`)
 
 /* Test Results
 
 Result 1: 13
 Result 2: 5
 Result 3: 0
+Result 4: 6
 
 */
