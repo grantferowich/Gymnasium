f71bffb328952efa0737e1655ef69bb9c40f5a03
grantferowich <g.ferowich@gmail.com>
2023-05-04 10:07:15 -0500

    defined question

---
+++ outco/dynamicProgramming/coinSumX2.js
@@ -0,0 +1,58 @@
+/* // COIN SUM
+
+Attempted May 04, 2023.
+
+// how many ways can you use the coins to produce the target value?
+
+// Understanding
+// Input: integer array and integer
+// Output: integer
+// Test case: [1,2,3], 4
+// Expected output is 4 : 1 + 3, 2+2, 1 + 1 + 1 + 1, 1 + 1 + 2
+// Since there are four ways to sum the coins to reach 4
+
+                 4, [1,2,3]
+subtract       /            \       pop
+         1,[1,2,3]        4,[1,2]
+          /   \             /   \
+-2,[`1,2,3] 
+        /*
+  *  Problem:  Coin Sum
+  *
+  *            Given an array of coins and a target total, return how many
+  *            unique ways there are to use the coins to make up that total.
+  *
+  *  Input:    coins {Integer Array}, total {Integer}
+  *  Output:   {Integer}
+  *
+  *
+  *  Example:  Input:  [1,2,3], 4
+  *
+  *                    1+1+1+1
+  *                    1+3
+  *                    1+1+2
+  *                    2+2
+  *
+  *            Output: 4
+  *
+  *
+  *            Input:  [2,5,3,6], 10
+  *
+  *                    2+3+5
+  *                    5+5
+  *                    2+3+2+3
+  *                    2+2+6
+  *                    2+2+2+2+2
+  *
+  *
+  *            Output: 5
+  *
+  *    Note:   You have an unlimited number of each coin type. All coins in the
+  *            coin array will be unique.
+  *            Order does not matter. Ex: One penny and one nickel to create six
+  *            cents is equivalent to one nickel and one penny
+  * 
+  * // base cases: total is negative
+  * all the coins are used up and total is 0
+  * all the coins are used up
+ */
