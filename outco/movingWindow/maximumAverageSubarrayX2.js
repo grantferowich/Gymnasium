ecd0028b6170748f68acada596c1e9c142fe5526
grantferowich <g.ferowich@gmail.com>
2023-05-08 07:14:06 -0500

    defined question

---
+++ outco/movingWindow/maximumAverageSubarrayX2.js
@@ -0,0 +1,26 @@
+/* 
+MAXIMUM AVERAGE SUBARRAY
+https://leetcode.com/problems/maximum-average-subarray-i/
+
+Attempted May 08, 2023
+
+input = [1,12,-5,-6,50,3], k = 4
+
+currentAvg = (1 + 12 - 5  -6) / 4
+ultimateAvg = Math.max(ultimateAvg, currentAvg)
+output = 12.75
+max is (12 - 5 - 6 + 50)\
+
+
+
+*/
+
+const maximumAverageSubarray = (arr, kInt) => {
+
+}
+
+/* Tests */
+
+
+/* Test results */
+
