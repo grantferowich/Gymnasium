97ad03bce8fd9c73fd3c332970b4ad61e5c4fe54
grantferowich <g.ferowich@gmail.com>
2023-05-09 14:40:25 -0500

    defined question

---
+++ outco/movingWindow/minimumWindowSubstringX2.js
@@ -0,0 +1,43 @@
+
+/*
+ * Minimum Window Substring (Sliding Window)
+ *
+ * Wrote down: May 9, 2023
+ * Given a string, and a set of characters
+ * return the substring representing the SMALLEST
+ * window containing those characters.
+ *
+ * The characters needn't appear in the order in which they are given.
+ *
+ * If not all the characters are present in the string, return the empty string.
+ *
+ *
+ * Input: str   (String)
+ *        chars (String)
+ *
+ * Output: {String}
+ *
+ *
+ * Example:
+ *  Input: "ADOBECODEBANC", "ABC"
+ *  Output: "BANC"
+ *
+ *  Input: "HELLO WORLD", "FOO"
+ *  Output: ""
+ *
+ *
+ * Explanation:
+ *   Though there are many substrings containing all the characters
+ *   "BANC" is the shortest.
+ *
+ * Assume that there won't be repeated characters in the second string input.
+ *
+ * Ignore capitalization.
+ * (though taking it into account doesn't change the solution much)
+ *
+ * But as extra credit, how would you handle repeats?
+ * Meaning if you were given two "A" characters, a valid window MUST
+ * contain two "A"s
+ *
+ *
+ */
