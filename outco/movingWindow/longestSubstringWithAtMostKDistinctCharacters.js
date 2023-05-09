6647ad8305fe5629002eea620f8e31b01fdd6d6e
grantferowich <g.ferowich@gmail.com>
2023-05-09 09:14:53 -0500

    defined question

---
+++ outco/movingWindow/longestSubstringWithAtMostKDistinctCharacters.js
@@ -0,0 +1,17 @@
+/* 340-longest-substring-with-at-most-k-distinct-characters
+Question
+https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/description/
+
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+Example:
+
+Given s = “eceba” and k = 2,
+T is "ece" which its length is 3.
+
+console.log(kUniqueCharacters("2eceba")) // 3
+console.log(kUniqueCharacters("3aaabbb")) // -1
+console.log(kUniqueCharacters("3aabbxxyyy")) // 7
+
+
+*/
