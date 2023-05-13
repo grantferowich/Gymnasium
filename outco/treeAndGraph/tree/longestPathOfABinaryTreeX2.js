c1a3904a535036a9552c4efd28d93105b74314f5
grantferowich <g.ferowich@gmail.com>
2023-05-13 08:30:44 -0500

    defined longestPathOfABinaryTreeX2.js

---
+++ outco/treeAndGraph/tree/longestPathOfABinaryTreeX2.js
@@ -0,0 +1,23 @@
+/* 
+
+213 - Longest Path of a Binary Tree
+Given a binary tree node, return the number of nodes in the longest path between the root and a leaf node
+
+Attempted May 13, 2023.
+
+Input: Node in a Binary Tree
+Output: Integer
+Example
+Input: <BSTNode 1> =>   Output: 3 (from path 1--> 3--> 4)
+LongestPathBinaryTree
+
+Constraints
+Time Complexity: O(N)
+Auxiliary Space Complexity: O(N)
+Binary Tree Node has the following properties:
+
+value (Integer)
+left (Binary Tree Node, default null)
+right (Binary Tree Node, default null)
+
+*/
