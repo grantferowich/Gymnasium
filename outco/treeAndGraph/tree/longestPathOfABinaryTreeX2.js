c97f99c3d23cb7fbecdc438dce6c43a07c5b50b7
grantferowich <g.ferowich@gmail.com>
2023-05-13 08:38:55 -0500

    defined one test

--- outco/treeAndGraph/tree/longestPathOfABinaryTreeX2.js
+++ outco/treeAndGraph/tree/longestPathOfABinaryTreeX2.js
@@ -1,7 +1,8 @@
 /* 
 
 213 - Longest Path of a Binary Tree
-Given a binary tree node, return the number of nodes in the longest path between the root and a leaf node
+Given a binary tree node, return the number of nodes 
+in the longest path between the root and a leaf node.
 
 Attempted May 13, 2023.
 
@@ -19,5 +20,41 @@ Binary Tree Node has the following properties:
 value (Integer)
 left (Binary Tree Node, default null)
 right (Binary Tree Node, default null)
+
+
+*/
+
+class TreeNode{
+    constructor(valueInt){
+        this.valueInt = valueInt === undefined ? null : valueInt;
+        this.leftInt = null;
+        this.rightInt = null;
+    }
+}
+
+const longestPathOfABinaryTree = (rootNodeInt) => {
+    
+    let pathInt = 0;
+
+    const traverse = (nodeInt) => {
+
+    }
+
+    traverse(rootNodeInt)
+    return pathInt;
+}
+
+/* Tests */
+
+let binaryTree1 = new TreeNode(3);
+binaryTree.leftInt = new TreeNode(1);
+binaryTree.rightInt = new TreeNode(5);
+
+const resultInt1 = longestPathOfABinaryTree(binaryTree1)
+
+console.log(`Result 1: ${resultInt1}`);
+
+/* Test results 
+
 
 */
