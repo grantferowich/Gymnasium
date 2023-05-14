7bfdf648ff35d59a71867c52754a3310c93900de
grantferowich <g.ferowich@gmail.com>
2023-05-14 15:45:30 -0500

    defined invertABinaryTreeX2.js

---
+++ outco/treeAndGraph/tree/invertABinaryTreeX2.js
@@ -0,0 +1,27 @@
+/* 
+
+214 - Invert a Binary Tree
+Given a binary tree root node, invert the binary tree (mirror) and return back the root node.
+
+Attempted May 14, 2023.
+
+Input: Node in a Binary Tree
+Output: Node in a Binary Tree
+Example
+Input: InvertBinaryTree1
+
+Output: InvertbinaryTree2
+
+Constraints
+Time Complexity: O(N)
+Auxiliary Space Complexity: O(N)
+
+*/
+
+const invertABinaryTree = (rootNodeInt) => {
+    
+}
+
+/* Test */
+
+/* Test results */
