c52548212ca40a1e2d388d75ad2028abad19ffce
grantferowich <g.ferowich@gmail.com>
2023-05-14 16:02:48 -0500

    defined question

---
+++ outco/treeAndGraph/tree/validBinarySearchTreeX2.js
@@ -0,0 +1,22 @@
+/* 215 - Valid Binary Search Tree
+Given a binary tree root node, check if the tree is a valid binary search tree.
+
+Input: Node in a Binary Tree
+Output: Boolean
+Example
+Input: ValidBinarySearchTree
+
+Output: False
+
+Constraints
+Time Complexity: O(N)
+Auxiliary Space Complexity: O(N)
+Binary Tree Node has the following properties:
+
+value (Integer)
+right (Binary Tree Node, default null)
+left (Binary Tree Node, default null) */
+
+const validBinarySearchTree = (rootNodeInt) => {
+    
+}
