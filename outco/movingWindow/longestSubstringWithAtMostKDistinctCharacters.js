0cae75b131d619e8d5b69f0fe4f009f1a40cc156
grantferowich <g.ferowich@gmail.com>
2023-05-18 16:38:13 -0500

    succesfully tested longest substring with at most k distinct characters on May 18, 2023

--- outco/movingWindow/longestSubstringWithAtMostKDistinctCharacters.js
+++ outco/movingWindow/longestSubstringWithAtMostKDistinctCharacters.js
@@ -13,5 +13,56 @@ console.log(kUniqueCharacters("2eceba")) // 3
 console.log(kUniqueCharacters("3aaabbb")) // -1
 console.log(kUniqueCharacters("3aabbxxyyy")) // 7
 
+Four questions of SWP
+
+1 Under what condition does the runner move?
+2 When to read temp solution?
+3 How to choose the optimal solution?
+4 Under what condition does the catcher move?
+
+Under what condition does the runner move?
+there are less than or equal to kInt unique characters
+map.size is less or equal to kInt
+
+
+When to read temp solution?
+after shrinking the window
 
 */
+
+const longestSubstringWithAtMostKDistinctCharacters = (str) => {
+    
+    let kInt = str.slice(0,1)
+    let map = new Map();
+    let rightPtrInt = 1;
+    let leftPtrInt = 1;
+    let longestSubstringInt = 0
+    while (rightPtrInt < str.length){
+
+            let char = str[rightPtrInt];
+            map.set(char, (map.get(char) || 0) + 1);
+            rightPtrInt++;
+
+        while (map.size > kInt){
+
+            let char = str[leftPtrInt];
+            let frequencyInt = map.get(char);
+
+            if (frequencyInt === 1){
+                map.delete(char)
+            }
+            if (frequencyInt > 1){
+                map.set(char, frequencyInt - 1)
+            }
+            leftPtrInt++
+        }
+
+        longestSubstringInt = Math.max(longestSubstringInt, rightPtrInt - leftPtrInt)
+    }
+    return longestSubstringInt
+}
+
+
+console.log(longestSubstringWithAtMostKDistinctCharacters("2eceba")) // 3
+console.log(longestSubstringWithAtMostKDistinctCharacters("3aaabbb")) // -1
+console.log(longestSubstringWithAtMostKDistinctCharacters("3aabbxxyyy")) // 7
