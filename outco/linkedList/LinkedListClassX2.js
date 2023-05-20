7c13fe38f32753fa1bb0517d8c967e18db3aacbe
grantferowich <g.ferowich@gmail.com>
2023-05-20 08:24:27 -0500

    defined question

---
+++ outco/linkedList/LinkedListClassX2.js
@@ -0,0 +1,60 @@
+/*
+ *  Homework - Linked List
+ */
+
+/*
+ * Attempted on May 20, 2023.
+ *  ListNode class
+ *
+ *  Prompt:    Create a ListNode class
+ *             The ListNode class should contain the following properties:
+ *
+ *                 value:   {Integer} (initially null)
+ *                  next:   {ListNode} (initially null)
+ *
+ *               Example:   input: let sample1 = new ListNode(1)
+ *                          sample1.value     // 1
+ *                          sample1.next      // null
+ *
+ *               Example:   input: let sample2 = new ListNode()
+ *                          sample2.value     // null
+ *                          sample2.next      // null
+ *
+ *
+ *  LinkedList class
+ *
+ *  Prompt:     Create a LinkedList class
+ *              The LinkedList class should contain the following properties:
+ *
+ *                length:   {Integer}
+ *                  head:   {ListNode}
+ *                  tail:   {ListNode}
+ *
+ *              The LinkedList class should also contain the following methods:
+ *
+ *                append:   A method that appends a value to the end of the
+ *                          LinkedList.
+ *
+ *                          Input:     value {Integer}
+ *                          Output:    {undefined}
+ *
+ *                insert:   A method that inserts an integer value at a set
+ *                          index in the LinkedList (assume head index is 0).
+ *
+ *                          Input:     value {Integer}
+ *                          Input:     index {Integer}
+ *                          Output:    {undefined}
+ *
+ *                delete:   A method that removes a node at a specified index.
+ *
+ *                          Input:     index {Integer}
+ *                          Output:    {undefined}
+ *
+ *              contains:   A method that checks to see if a value is contained
+ *                          in the list.
+ *
+ *                          Input:     value {Integer}
+ *                          Output:    {Boolean}
+ *
+ */
+
