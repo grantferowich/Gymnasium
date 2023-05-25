3e69d0624fdce60a63875ea6486b85195ba54de9
grantferowich <g.ferowich@gmail.com>
2023-05-24 19:27:02 -0500

    defined question

---
+++ outco/stackAndQueue/evaluateReversePolishNotation.js
@@ -0,0 +1,48 @@
+/* 
+150. Evaluate Reverse Polish Notation
+Medium
+5.8K
+882
+Companies
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+
+Procedure
+1. Iterate through the array
+    2. if operand (number) -> push to stack
+    3. if operator (+/-*)
+        4. pop the last two operands
+        5. calculate using current operator
+        6. push result to stack
+*/
