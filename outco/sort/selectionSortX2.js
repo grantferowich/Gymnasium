dc52ddb45df7b619140b6748809f614d673d6ba5
grantferowich <g.ferowich@gmail.com>
2023-05-31 06:48:32 -0500

    appended test results to selection sort

--- outco/sort/selectionSortX2.js
+++ outco/sort/selectionSortX2.js
@@ -9,4 +9,51 @@
  *  Output:    {Array}
  *
  *  Example:   [3,9,1,4,7] --> [1,3,4,7,9]
+ * 
+ * 
+ * Step: Start with some key at 0 which states the first unsorted element in 
+ * the array
+ * loop from 0 to N - 1, keeping track of the minimum element
+ * Swap: when the loop has completed, set the minimum element at the location of the key,
+ * which is the first unsorted element in the array
+ * 
  * */
+
+const selectionSort = (arr) => {
+    let keyInt = 0;
+    let xInt = 0;
+
+    while (keyInt < arr.length){
+        xInt = keyInt;
+        let minLocationInt;
+        let minimumElementInt = Infinity
+        while (xInt < arr.length){
+            let currentElementInt = arr[xInt];
+            if (currentElementInt < minimumElementInt){
+                minimumElementInt = currentElementInt
+                minLocationInt = xInt
+            }
+            xInt++
+        }
+        [arr[keyInt], arr[minLocationInt]] = [arr[minLocationInt], arr[keyInt]]
+        keyInt++
+    }
+    return arr
+}
+
+/* Tests */
+const arr1 = [ 3, 9, 1, 4, 7]
+const arr2 = [ 23, 13, 8, 5]
+const arr3 = [ 34, 55,  36, 2]
+
+const result1Arr = selectionSort(arr1)
+const result2Arr = selectionSort(arr2)
+const result3Arr = selectionSort(arr3)
+
+console.log(`Result 1: ${result1Arr}`);
+console.log(`Result 2: ${result2Arr}`);
+console.log(`Result 3: ${result3Arr}`);
+/* Test results 
+
+
+*/
