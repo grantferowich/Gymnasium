d26823b28a164ccc4a86322edd42ab5865465a82
grantferowich <g.ferowich@gmail.com>
2023-06-01 16:08:38 -0500

    defined question

---
+++ outco/movingWindow/longestSubstringWithAtMost2UniqueCharacters.js
@@ -0,0 +1,24 @@
+/* 
+Longest Substring With At Most 2 Unique Characters
+
+Sliding window problem
+
+Started engineering on June 1, 2023 at 4pm.
+
+
+str1 = "eceba"
+output: 3
+
+str = "ebcdedgeecba"
+
+fast (runner): 0
+slow (catcher): 0
+temp: 
+result: 
+
+
+*/
+
+const longestSubstringWithAtMost2UniqueCharacters = (str) => {
+
+}
