ed5abf6e85dc4b33e41e7cf8f689b2d9393ba71f
grantferowich <g.ferowich@gmail.com>
2023-06-01 16:37:45 -0500

    defined questions

---
+++ outco/binaryHeap/binaryHeapX2.js
@@ -0,0 +1,78 @@
+/*
+ *  Homework - Binary Heap
+ *
+ *  Prompt: Create a Binary Heap class
+ *
+ *  The Heap will take in the following input:
+ *
+ *            type:   {String} - 'min' for minheap, 'max' for maxheap
+ *
+ *  The Heap will have the following properties:
+ *
+ *         storage:   {Array} - storage of numbers within heap
+ *            type:   {String} - indicates whether heap is a minheap or maxheap
+ *
+ *  The Heap will have the following methods:
+ *
+ *        compare:   compares two input numbers and returns if the heap
+ *                   condition is met.
+ *
+ *                   Input:      parent {Number} - index at parent
+ *                   Input:      child {Number} - index at child
+ *                   Output:     {Boolean}
+ *
+ *         insert:   inserts a number into the heap.
+ *
+ *                    Input:     {Number}
+ *                   Output:     {undefined}
+ *
+ *           peek:   returns the top item in the heap
+ *
+ *                   Input:      N/A
+ *                   Output:     {Number}
+ *
+ *         removePeak:   removes the top element from the heap
+ *
+ *                    Input:      N/A
+ *                   Output:     {Number}
+ */
+
+'use strict';
+
+
+class Heap {
+
+ constructor(type = 'min') {
+   // YOUR WORK HERE
+ }
+
+ // Time Complexity:
+ // Auxiliary Space Complexity:
+ compare(parent, child) {
+   // YOUR WORK HERE
+ }
+
+
+ // Time Complexity:
+ // Auxiliary Space Complexity:
+ insert(value) {
+   // YOUR WORK HERE
+ }
+
+
+ // Time Complexity:
+ // Auxiliary Space Complexity:
+ peek() {
+   // YOUR WORK HERE
+ }
+
+
+ // Time Complexity: O(log(N))
+ // Auxiliary Space Complexity: O(1)
+ // Time Complexity:
+ // Auxiliary Space Complexity:
+ removePeak() {
+   // YOUR WORK HERE
+ }
+
+}
