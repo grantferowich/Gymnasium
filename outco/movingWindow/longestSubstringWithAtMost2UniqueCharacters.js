c6133fb482f66092d4ec69927c401c6f0b7a5434
grantferowich <g.ferowich@gmail.com>
2023-06-01 16:36:04 -0500

    successfully tested longestSubstringWithAtMost2UniqueCharacters.js

--- outco/movingWindow/longestSubstringWithAtMost2UniqueCharacters.js
+++ outco/movingWindow/longestSubstringWithAtMost2UniqueCharacters.js
@@ -8,17 +8,70 @@ Started engineering on June 1, 2023 at 4pm.
 
 str1 = "eceba"
 output: 3
+why? "ece"
 
 str = "ebcdedgeecba"
 
-fast (runner): 0
+fast (runner): 0 1 2
 slow (catcher): 0
-temp: 
-result: 
+temp: 'bc'
+result: 3
+why? 'ded', 'gee', 'eec' 
 
+Expand while there are less than or equal to two distinct characters (move fastPtrInt)
+Contract while there are more than two distinctcharacters (move slowPtrInt)
 
 */
 
 const longestSubstringWithAtMost2UniqueCharacters = (str) => {
+    let rPtrInt = 0;
+    let lPtrInt = 0;
+    let resultInt = -Infinity;
+    let charMap = new Map();
+
+    while (rPtrInt < str.length){
+        let rChar = str[rPtrInt];
+        let frequencyInt = charMap.get(rChar);
+        charMap.set(rChar, ((frequencyInt + 1) || 0));
+
+        if (charMap.size > 2){
+            let lChar = str[lPtrInt];
+            let frequencyInt = charMap.get(lChar);
+            if (frequencyInt === 0){
+                charMap.delete(lChar)
+            }
+            if (frequencyInt !== 0){
+                charMap.set(lChar, frequencyInt - 1)
+            }
+            lPtrInt++
+        }
+
+        // update
+        if (rPtrInt - lPtrInt + 1 > resultInt){
+            resultInt = rPtrInt - lPtrInt + 1
+        }
+        
+        rPtrInt++
+    }
+    return resultInt;
 
 }
+
+/* Test */
+
+let str1 = 'grant' 
+let str2 = 'wakeforest'
+let str3 = 'mill' 
+const str4 = 'eceba'
+
+const result1Int = longestSubstringWithAtMost2UniqueCharacters(str1);
+const result2Int = longestSubstringWithAtMost2UniqueCharacters(str2);
+const result3Int = longestSubstringWithAtMost2UniqueCharacters(str3);
+const result4Int = longestSubstringWithAtMost2UniqueCharacters(str4);
+
+console.log(`Result 1: ${result1Int}`);
+console.log(`Result 2: ${result2Int}`);
+console.log(`Result 3: ${result3Int}`);
+console.log(`Result 4: ${result4Int}`);
+
+/* Test result */
