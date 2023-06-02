c59c33611ceaedfe8f88d962777314487f272181
grantferowich <g.ferowich@gmail.com>
2023-06-02 05:43:17 -0500

    appended test notes

--- outco/stringAndArray/RemoveDuplicatesFromArray.js
+++ outco/stringAndArray/RemoveDuplicatesFromArray.js
@@ -1,10 +1,22 @@
+/* 
+Remove duplicates from array
+Leetcode - Easy
+Successfully tested the function 6/02/2023
+
+create a new set, 
+pass in array, 
+a set object only allows unique values, 
+pread operator converts back to array
+
+*/
+
 const removeDuplicates = arr => {
   return [...new Set(arr)];
 };
 
 console.log(removeDuplicates([1, 1, 2, 2, 3, 3]));
 
-// create a new set, pass in array, a set object only allows unique values, spread operator converts back to array
+
 
 // const removeDuplicatesTwo = arr => {
 //   let a = Array.from(new Set(arr));
