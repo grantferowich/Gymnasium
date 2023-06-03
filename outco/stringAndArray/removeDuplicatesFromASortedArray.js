ebacec28853cbc89ce97a978bba050a902aeea51
grantferowich <g.ferowich@gmail.com>
2023-06-03 16:53:34 -0500

    successfully tested functions

--- outco/stringAndArray/removeDuplicatesFromASortedArray.js
+++ outco/stringAndArray/removeDuplicatesFromASortedArray.js
@@ -28,8 +28,6 @@ Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 Explanation: Your function should return k = 5, 
 with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
-
-
 */
 const removeDuplicates = (numsArr) => {
     let xInt = 0;
