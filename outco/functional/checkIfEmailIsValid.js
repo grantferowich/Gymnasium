d731baf7a11d366f8f18bbc23780f35705bdbfd7
grantferowich <g.ferowich@gmail.com>
2023-06-20 15:42:24 -0500

    successufully tested checkIfEmailIsValidg

---
+++ outco/functional/checkIfEmailIsValid.js
@@ -0,0 +1,13 @@
+
+const checkIfEmailIsValid = (emailStr) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailStr)
+}
+
+const emailStr1 = 'email.test';
+const emailStr2 = 'linkedlist@gmail.com';
+
+const result1ToF = checkIfEmailIsValid(emailStr1);
+const result2ToF = checkIfEmailIsValid(emailStr2);
+
+console.log('Result 1: ', result1ToF)
+console.log('Result 2: ', result2ToF)
