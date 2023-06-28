7e8fb8ef3a9f7ef322c753aebcee5ea22fc3eea8
grantferowich <g.ferowich@gmail.com>
2023-06-28 16:55:18 -0500

    successfully tested findTheIndexOfTheFirstOccurrenceInAString

--- outco/stringAndArray/findTheIndexOfTheFirstOccurrenceInAString.js
+++ outco/stringAndArray/findTheIndexOfTheFirstOccurrenceInAString.js
@@ -9,26 +9,49 @@ Easy
 Companies
 Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
- 
-
 Example 1:
-
 Input: haystack = "sadbutsad", needle = "sad"
 Output: 0
 Explanation: "sad" occurs at index 0 and 6.
 The first occurrence is at index 0, so we return 0.
 Example 2:
-
 Input: haystack = "leetcode", needle = "leeto"
 Output: -1
-Explanation: "leeto" did not occur in "leetcode", so we return -1. */
+Explanation: "leeto" did not occur in "leetcode", so we return -1. 
+
+Maintain a window which has a size equal to the length of the needle. 
+Expand the right pointer when the window size is smaller than the needle. 
+Contract the left pointer when the window size is greater than the needle.
+If the elements in the window equal the needle then 
+
+*/
 
 const findTheNeedle = (haystackStr, needleStr) => {
-    if (haystackStr.includes(needleStr)){
-        return haystackStr.indexOf(needleStr[0])
+    let rightPtrInt = 0;
+    let leftPtrInt = 0;
+    let windowArr = [];
+    let haystackArr = haystackStr.split("")
+
+    while (rightPtrInt < haystackArr.length){
+
+        let char = haystackArr[rightPtrInt];
+        windowArr.push(char);
+        rightPtrInt++;
+
+        if (windowArr.length === needleStr.length){
+            console.log('windowStr', windowArr.join(""))
+            let tempStr = windowArr.join("");
+            if (tempStr === needleStr){
+                return leftPtrInt;
+            }
+        }
+
+        if (windowArr.length >= needleStr.length){
+            windowArr.shift()
+            leftPtrInt++
+        }
     }
     return -1;
-
 }
 
 /* Test */
@@ -39,10 +62,10 @@ const haystackStr2 = "leetcode";
 const needleStr2 = "leeto"
 
 const haystackStr3 = "mississippi"
-const needleStr3 = "issipi"
+const needleStr3 = "issip"
 
 const resultInt1 = findTheNeedle(haystackStr1, needleStr1); // 0 
-const resultInt2 = findTheNeedle(haystackStr2, needleStr2); // 1
+const resultInt2 = findTheNeedle(haystackStr2, needleStr2); // -1
 const resultInt3 = findTheNeedle(haystackStr3, needleStr3); // 4
  
 console.log('Result 1:', resultInt1)
