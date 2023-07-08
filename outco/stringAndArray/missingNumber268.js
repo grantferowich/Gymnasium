56fe89c50c72f8a5c9d282df8b27ed9aaae0263d
grantferowich <g.ferowich@gmail.com>
2023-07-08 07:05:22 -0500

    successfully tested missingNumber268.js

--- outco/stringAndArray/missingNumber268.js
+++ outco/stringAndArray/missingNumber268.js
@@ -1,4 +1,7 @@
 /* 268. Missing Number
+
+Successfully tested the solution on July 8, 2023.
+Leetcode URL: https://leetcode.com/problems/missing-number/submissions/989263844/
 Easy
 9.8K
 3.2K
@@ -35,3 +38,39 @@ Finally, retrieve the key from the map whose value is true. Only one key will ha
 value of true. 
 
 */
+
+
+const missingNumber = (arr) => {
+    let intMap = new Map();
+    let iInt = 0
+    while (iInt <= arr.length){
+        intMap.set(iInt, true)
+        iInt++
+    }
+    iInt = 0
+    while (iInt < arr.length){
+        let elementInt = arr[iInt]
+        intMap.set(elementInt, false)
+        iInt++
+    }
+    for (const [int, trueOrFalse] of intMap){
+        if (trueOrFalse === true){
+            return int
+        }
+    }
+}
+
+/* Test */
+const arr1 = [0, 1, 2, 3, 4]; // the missing number is 5
+const arr2 = [3,0,1]; // the missing number is 2
+const arr3 = [0,1]; // the missing number is 2
+const arr4 = [9,6,4,2,3,5,7,0,1]; // the missing number is 8
+const result1Int = missingNumber(arr1);
+const result2Int = missingNumber(arr2);
+const result3Int = missingNumber(arr3);
+const result4Int = missingNumber(arr4);
+console.log('The first missing number :', result1Int);
+console.log('The second missing number :', result2Int);
+console.log('The third missing number :', result3Int);
+console.log('The fourth missing number :', result4Int);
+/* Test results */
