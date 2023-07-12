ec00ac7a0ae5095ac0c1924eecd7e52ebd41a950
grantferowich <g.ferowich@gmail.com>
2023-07-12 00:14:14 -0500

    defined the question

---
+++ ctci/arraysAndStrings-01/checkPermutation-01-02.js
@@ -0,0 +1,19 @@
+/* 
+Check Permutation
+CTCI 1.02
+
+Given two strings, write a method to decide if one is a permutation of another.
+
+const str1 = rat 
+const str2 = cat
+output: false!
+
+const str3 = taco
+const str4 = octa
+output: true
+
+const str5 = wakeforest
+const str6 = forestwake
+output: true!
+
+*/
