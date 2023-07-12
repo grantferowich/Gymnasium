f14c369c3020ed8577ad217c555ddf027cd7b76e
grantferowich <g.ferowich@gmail.com>
2023-07-12 12:58:58 -0500

    successfully tested tic-tac-win on July 12, 2023

--- ctci/Moderate-16/ticTacWin-16-04.js
+++ ctci/Moderate-16/ticTacWin-16-04.js
@@ -24,22 +24,49 @@ middle horizontal: matrix[1][0], matrix[1][1], matrix[1][2]
 lower horizontal: matrix[2][0], matrix[2][1], matrix[2][2]
 left vertical: matrix[0][0], matrix[1][0], matrix[2][0]
 middle vertical: matrix[0][1], matrix[1][1], matrix[2][1]
-right vertical: matrix[]
+right vertical: matrix[0][2], matrix[1][2], matrix[2][2]
+Right to left diagonal: matrix[0][0], matrix[1][1], matrix[2][2]
+left to right diagonal: matrix[2][0], matrix[1][1], matrix[0][2]
+
+// loop over the array of winning combinations
+// if each element in the combination is equal to the player symbol str
+// return true
+
+outside the loop return false
 */
 
 const ticTacWin = (playerSymbolStr, matrix) => {
     if (matrix.length === 0){
         return "invalid game board"
     }
-
-
+    let winningMovesArr = [
+        [matrix[0][0], matrix[0][1], matrix[0][2]],
+        [matrix[1][0], matrix[1][1], matrix[1][2]],
+        [matrix[2][0], matrix[2][1], matrix[2][2]],
+        [matrix[0][0], matrix[1][0], matrix[2][0]],
+        [matrix[0][1], matrix[1][1], matrix[2][1]],
+        [matrix[0][2], matrix[1][2], matrix[2][2]],
+        [matrix[0][0], matrix[1][1], matrix[2][2]],
+        [matrix[2][0], matrix[1][1], matrix[0][2]]
+    ]
+    let rowInt = 0;
+    while (rowInt < winningMovesArr.length){
+            if (playerSymbolStr === winningMovesArr[rowInt][0] && 
+                playerSymbolStr === winningMovesArr[rowInt][1] && 
+                playerSymbolStr === winningMovesArr[rowInt][2]){
+                    return true
+            }
+        rowInt++
+    }
+    return false
 }
 
 /* TESTS */
 
 const playerSymbolStr1 = "x";
 const matrix1 = [["x", "x", "x"],
-                ["", "o", "o" ],
-                ["", "", ""   ]];
+                 ["", "o", "o" ],
+                 ["", "", ""   ]];
+
 const result1ToF = ticTacWin(playerSymbolStr1, matrix1);
 console.log('Result 1: Player X won', result1ToF);
