2a7bce793644a90472a61bfae4b9a27fa5b8c9d5
grantferowich <g.ferowich@gmail.com>
2023-07-12 15:06:00 -0500

    successfully tested palindrome permutation on July 12, 2023

--- ctci/arraysAndStrings-01/palindromePermutation-01-04.js
+++ ctci/arraysAndStrings-01/palindromePermutation-01-04.js
@@ -10,4 +10,53 @@ dictionary words. You can ignore casing and non-character letters.
 
 const inputStr1 = "tact coa"
 output: true (taco cat, atco cta)
+
+Approach:
+build up a char map where the keys are chars
+and each key's value is the frequency of the char.
+Track number of odd frequencies.
+Loop over the values.
+If the number of odd frequencies is greater than 1 return false.
+Return true.
 */
+
+const palindromePermutation = (str) => {
+    let charMap = new Map();
+    let numberOfOddFrequenciesInt = 0;
+    let xInt = 0;
+    // remove spaces
+    str = str.replaceAll(' ','');
+    while (xInt < str.length){
+        let char = str[xInt]
+        if (charMap.has(char)){
+            let frequencyInt = charMap.get(char);
+            charMap.set(char, frequencyInt + 1);
+        }
+        if (!charMap.has(char)){
+            charMap.set(char, 1);
+        }
+        xInt++
+    }
+    for (const [keyStr, valueInt] of charMap){
+        if (valueInt % 2 !== 0){
+            numberOfOddFrequenciesInt++
+        }
+    }
+    return numberOfOddFrequenciesInt <= 1;
+}
+
+/* Tests */
+
+const str1 = "taco cat"
+const resultToF1 = palindromePermutation(str1);
+
+const str2 = "hannah"
+const resultToF2 = palindromePermutation(str2);
+
+const str3 = "hannahzx"
+const resultToF3 = palindromePermutation(str3);
+
+console.log('Result 1: ', resultToF1) // true
+console.log('Result 2: ', resultToF2) // true
+console.log('Result 3: ', resultToF3) // false 
+/* Test results */
