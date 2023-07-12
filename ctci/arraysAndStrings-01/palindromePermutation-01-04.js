2a799b5f995b021ef73ba0f1241b3da7c6394765
grantferowich <g.ferowich@gmail.com>
2023-07-12 14:56:28 -0500

    defined question

---
+++ ctci/arraysAndStrings-01/palindromePermutation-01-04.js
@@ -0,0 +1,13 @@
+/* 
+Palindrome Permutation
+CTCI 1.04
+
+Given a string, write a function to check if it is a permutation
+of a palindrome. A palindrome is a word or phrase that is 
+the same forwards and backwards. A permutation is a rearrangement 
+of letters. The palindrome does not need to be limite to just
+dictionary words. You can ignore casing and non-character letters.
+
+const inputStr1 = "tact coa"
+output: true (taco cat, atco cta)
+*/
