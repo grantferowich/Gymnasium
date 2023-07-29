fe9aa15ffc093df99a12818736110a3376d6f478
grantferowich <g.ferowich@gmail.com>
2023-07-29 10:12:34 -0500

    successfully tested house robber

--- outco/movingWindow/houseRobber.js
+++ outco/movingWindow/houseRobber.js
@@ -1,6 +1,9 @@
 /**
  * 198 House Robber 
  * 
+ * 
+ * Successfully tested the function on July 29,2023.
+ * 
  * You are a professional robber planning to rob houses along a street.
  * Each house has a certain amount of money stashed, the only constraint stopping you from robbing each
  * of them is that adjacent houses have security systems connected and it will automatically contact 
