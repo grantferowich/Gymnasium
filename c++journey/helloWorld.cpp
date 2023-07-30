514569db803eb6aa79cec3d06949378c1ac54ce2
grantferowich <g.ferowich@gmail.com>
2023-07-30 07:48:42 -0500

    logged the output of a control flow statement

--- c++journey/helloWorld.cpp
+++ c++journey/helloWorld.cpp
@@ -2,5 +2,16 @@
 
 int main() {
     std::cout << "Hello, world" << std::endl;
-    return 0;
+    
+    // data types
+    int ageInt = 30;
+    double pi = 3.141;
+    char letterChar = 'G';
+    
+    if (ageInt >= 21){
+        std::cout << "You can purchase different things." << std::endl;
+    } else {
+        std::cout << "You are a minor." << std::endl;
+    }
+
 }
