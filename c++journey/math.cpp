1ddcd287960c9d4847247e9480ff4a11e3310ba0
grantferowich <g.ferowich@gmail.com>
2023-07-30 09:59:20 -0500

    successfully tested multiplication with a helper method

---
+++ c++journey/math.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int multiply (int int1, int int2) {
+    return int1 * int2;
+}
+
+int main (){
+    int resultInt = multiply(3, 4);
+    std::cout << "Result: " << resultInt << std::endl;
+}
