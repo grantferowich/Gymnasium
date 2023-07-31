b2a97f306e957fb1f594c7ce424b2637f983c58a
grantferowich <g.ferowich@gmail.com>
2023-07-31 13:58:03 -0500

    implement nthFib with memoization

---
+++ c++journey/nthFibonacciMemoization.cpp
@@ -0,0 +1,32 @@
+/* 
+Nth Fibonacci
+Handle ~large~ inputs by storing the results 
+of subproblems
+*/
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int nthFibonacciMemoization(int gInt ){
+    unordered_map<string, int> intMap;
+    string keyStr = to_string(gInt);
+    
+    if (intMap.find(keyStr) != intMap.end()){
+        return intMap[keyStr];
+    }
+
+    if (gInt < 2){
+        return intMap[keyStr] = gInt;
+    }
+    
+  
+    return intMap[keyStr] = nthFibonacciMemoization(gInt - 1) + nthFibonacciMemoization(gInt - 2);
+}
+int main(){
+    int resultInt1 = nthFibonacciMemoization(10);
+    cout << "Result 1: " << resultInt1 << endl;
+    int resultInt2 = nthFibonacciMemoization(20);
+    cout << "Result 2: " << resultInt2 << endl;
+    return 0;
+}
