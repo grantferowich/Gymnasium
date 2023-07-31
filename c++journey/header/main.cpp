5feffa60c26859da9b3ca23302737ca8ca009963
grantferowich <g.ferowich@gmail.com>
2023-07-31 02:03:10 -0500

    c++ journey

--- c++journey/header/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "header.h"
-
-int main() {
-    JustAClass obj;
-    obj.justAFunction();
-    return 0;
-}
--- c++journey/intro/math.cpp
+++ c++journey/intro/math.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-
 int multiply (int int1, int int2) {
     return int1 * int2;
 }
-
 int main (){
     int resultInt = multiply(3, 4);
     std::cout << "Result: " << resultInt << std::endl;
