e940737fd906a75aac734137b9740a709c13c700
grantferowich <g.ferowich@gmail.com>
2023-08-01 06:36:44 -0500

    specified question

---
+++ c++journey/outco_in_cpp/graphs/thirdDegreeNeighbors.cpp
@@ -0,0 +1,25 @@
+/*
+ *  Third Degree Neighbors
+ *
+ *  Given an undirected graph represented by a list of edges and a start
+ *  vertex, return an array of the 3rd degree neighbors.
+ *
+ *  Parameters:
+ *
+ *  Input: edges: [[int]]
+ *  Input: start: int
+ *  Output: [int]
+ *
+ *  Example:
+ *
+ *  The following example with start vertex `1`:
+ *  Input: `{{1,2},{2,1},{1,3},{3,1},{2,4},{4,2},{3,4},{4,3},
+ *           {4,8},{8,4},{4,5},{5,4},{5,6},{6,5},{5,7},{7,5},
+ *           {6,7},{7,6},{8,7},{7,8},{8,9},{9,8}}`
+ *  Input: 1
+ *  Output: `[5,8]` or `[8,5]`     (order does not matter)
+ *  Picture here: https://res.cloudinary.com/outco/image/upload/v1519850256/graph-traversal/Paper.Graph_Traversal.2.png
+ *
+ *
+ *
+ */
