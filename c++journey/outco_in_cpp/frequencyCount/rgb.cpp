e0c80ceda425dda7d78767689fae304d0b0f877e
grantferowich <g.ferowich@gmail.com>
2023-08-01 15:00:20 -0500

    developed rgb

---
+++ c++journey/outco_in_cpp/frequencyCount/rgb.cpp
@@ -0,0 +1,67 @@
+/**
+ * RGB Set
+ *
+ * Given a string of characters where each character is either 'r', 'g', or 'b',
+ * determine the number of complete sets of 'rgb' that can be made with the
+ * characters.
+ *
+ * Parameters
+ * Input: str {string}
+ * Output: {int}
+ *
+ * Constraints
+ * Time: O(N)
+ * Space: O(1)
+ *
+ * Examples
+ * `'rgbrgb' --> 2`
+ * `'rbgrbrgrgbgrrggbbbbrgrgrgrg' --> 7`
+ * `'bbrr' --> 0`
+ * * Approach
+ 1. Initialize a character map <char, int>
+ 2. Count the number of rs, bs, and gs 
+ 3. Take the minimum number of charMap['r'], charMap['g'], charMap['b']
+ 4. 
+ */
+#include <array>
+#include <cctype>
+#include <cmath>
+#include <iterator>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <iostream>
+#include <array>
+#include <sstream>
+using namespace std;
+
+int rgb(string str){
+    unordered_map<char, int> charMap;
+    for (char c : str){
+        if (charMap.find(c) != charMap.end()){
+            charMap[c] = charMap[c] + 1;
+        }
+        if (charMap.find(c) == charMap.end()){
+            charMap[c] = 1;
+        }
+    }
+    int minimumInt = min(min(charMap['r'], charMap['g']), charMap['b']);
+    return minimumInt;
+}
+
+void test(){
+    int resultInt1 = rgb("rgbrgb");
+    cout << "Result 1: " << resultInt1 << endl;
+    int resultInt2 = rgb("rbgrbrgrgbgrrggbbbbrgrgrgrg");
+    cout << "Result 2: " << resultInt2 << endl;
+    int resultInt3 = rgb("bbrr");
+    cout << "Result 3: " << resultInt3 << endl;
+}
+
+int main(){
+    test();
+    return 0;
+}
