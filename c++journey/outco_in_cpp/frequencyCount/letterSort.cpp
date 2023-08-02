db33da42c857afa1f16667c4ba1b861c4023cc6a
grantferowich <g.ferowich@gmail.com>
2023-08-01 19:59:57 -0500

    develop letterSort on Aug 1 2023

--- c++journey/outco_in_cpp/frequencyCount/letterSort.cpp
+++ c++journey/outco_in_cpp/frequencyCount/letterSort.cpp
@@ -15,6 +15,12 @@
   * `hello --> ehllo`
   * `whiteboard --> abdehiortw`
   * `one --> eno`
+
+  So initialize an charArray with 26 characters filled with 0s.
+  Loop over the input string.
+  Find the index to update by doing the Unicode value less 26.
+  Increment the number at the index by 1.
+  Loop through the 
   */
 #include <array>
 #include <cctype>
@@ -29,7 +35,54 @@
 #include <iostream>
 #include <array>
 #include <sstream>
+#include <cctype>
 using namespace std;
-string letterSort(string wordStr){
 
+void printVector(const vector<int>& vec){
+    for (int valueInt : vec){
+        cout << valueInt << " ";
+    }
+    cout << endl;
+}
+
+string letterSort(string wordStr){
+    vector<int> alphabetVec(26, 0);
+    string outputStr;
+    for (char elementChar : wordStr){
+        elementChar = tolower(elementChar);
+        // get the unicode of the element
+        int unicodeInt = static_cast<int>(elementChar);
+        // increment the value at the index given by the unicode value - 95;
+        int indexInt = unicodeInt - 95;
+        alphabetVec[indexInt] += 1;
+    }
+    printVector(alphabetVec);
+    int gInt = 0;
+    for (int valueInt : alphabetVec){
+        while (valueInt > 0){
+            // convert unicode int to character
+            char charX = static_cast<char>(gInt + 95);
+            outputStr += charX;
+            valueInt--;
+        }
+        gInt++;
+    }
+    
+    return outputStr;
+}
+
+void test(){
+    string resultStr1 = letterSort("hello");
+    cout << "Result 1: " << resultStr1 << endl;
+    string resultStr2 = letterSort("whiteboard");
+    cout << "Result 2: " << resultStr2 << endl;
+    string resultStr3 = letterSort("one");
+    cout << "Result 3: " << resultStr3 << endl;
+    string resultStr4 = letterSort("wake forest");
+    cout << "Result 4: " << resultStr4 << endl;
+}
+
+int main(){
+    test();
+    return 0;
 }
