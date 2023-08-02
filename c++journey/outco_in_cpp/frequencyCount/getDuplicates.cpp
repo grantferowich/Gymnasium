85451096faecac7703869b4a43cc864a67c65849
grantferowich <g.ferowich@gmail.com>
2023-08-02 07:09:56 -0500

    define function

--- c++journey/outco_in_cpp/frequencyCount/getDuplicates.cpp
+++ c++journey/outco_in_cpp/frequencyCount/getDuplicates.cpp
@@ -17,3 +17,34 @@
 *  [3, 2, 3, 2, 3, 3, 4] --> [3, 2]
 *  [1, 2, 3, 4] --> []
 */
+#include <array>
+#include <cctype>
+#include <cmath>
+#include <iterator>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <iostream>
+#include <array>
+#include <sstream>
+#include <cctype>
+using namespace std;
+
+vector<int> getDuplicates(vector<int> vecX){
+
+    vector<int> outputVec;
+
+    return outputVec;
+}
+
+void test(){
+
+}
+
+int main(){
+    test();
+    return 0;
+}
