91fb9c9490ef204cd92e61aeb15d7d2874c50889
grantferowich <g.ferowich@gmail.com>
2023-08-02 07:18:20 -0500

    define tests

--- c++journey/outco_in_cpp/frequencyCount/getDuplicates.cpp
+++ c++journey/outco_in_cpp/frequencyCount/getDuplicates.cpp
@@ -33,6 +33,14 @@
 #include <cctype>
 using namespace std;
 
+/* 
+* 1. Initialize frequencyMap<int, int> and outputVec<int, int>.
+* 2. Loop over input vector to build up the frequency map.
+* 3. Loop through the frequencyMap.
+*       --> if the key's frequency is 2 or higher,
+*       --> append the key to the outputVec.
+* 4. Return the outputVec.
+ */
 vector<int> getDuplicates(vector<int> vecX){
 
     vector<int> outputVec;
@@ -40,9 +48,27 @@ vector<int> getDuplicates(vector<int> vecX){
     return outputVec;
 }
 
-void test(){
-
+void printVector(const vector<int>& vec){
+    for (int valueInt : vec){
+        cout << valueInt << " ";
+    }
+    cout << endl;
 }
+
+void test(){
+    vector<int> resultVec1 = getDuplicates({1, 2, 4, 2}); // {2}
+    vector<int> resultVec2 = getDuplicates({2, 2, 3, 3, 3, 3, 4}); // {2, 3}
+    vector<int> resultVec3 = getDuplicates({1, 2, 3, 4}); // {}
+    vector<int> resultVec4 = getDuplicates({1, 1, 5, 5, 8, 8}); // {1, 5, 8}
+    cout << "Result 1: " << endl;
+    printVector(resultVec1);
+    cout << "Result 2: " << endl;
+    printVector(resultVec2);
+    cout << "Result 3: " << endl;
+    printVector(resultVec3);
+    cout << "Result 4: " << endl;
+    printVector(resultVec4);
+}   
 
 int main(){
     test();
