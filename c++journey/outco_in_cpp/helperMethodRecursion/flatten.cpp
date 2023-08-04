0f96e010a050f2a4c5c759385fd053db6d31b8ca
grantferowich <g.ferowich@gmail.com>
2023-08-04 10:07:42 -0500

    define question

---
+++ c++journey/outco_in_cpp/helperMethodRecursion/flatten.cpp
@@ -0,0 +1,22 @@
+#include <array>
+#include <cmath>
+#include <iterator>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <iostream>
+#include <array>
+using namespace std;
+/**
+ * 2e. Flatten a two dimensional integer array/vector using Helper Method of Recursion
+ *
+ * Input:   two-dimensional int Array/vector
+ * Output:  Array/vector of ints
+ *
+ * Example: vector matrix{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+ *          Flatten.compute(matrix) =>
+ *          {1, 2, 3, 4, 5, 6, 7, 8, 9}
+ */
+ 
