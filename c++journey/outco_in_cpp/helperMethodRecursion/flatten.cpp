21bf591324d0dd05b43e94a6593f72934533fa05
grantferowich <g.ferowich@gmail.com>
2023-08-07 20:36:37 -0500

    developed and tested flatten

--- c++journey/outco_in_cpp/helperMethodRecursion/flatten.cpp
+++ c++journey/outco_in_cpp/helperMethodRecursion/flatten.cpp
@@ -8,6 +8,7 @@
 #include <unordered_map>
 #include <iostream>
 #include <array>
+#include <type_traits>
 using namespace std;
 /**
  * 2e. Flatten a two dimensional integer array/vector using Helper Method of Recursion
@@ -19,4 +20,26 @@ using namespace std;
  *          Flatten.compute(matrix) =>
  *          {1, 2, 3, 4, 5, 6, 7, 8, 9}
  */
- 
+void flatten(vector<vector<int> >& matrix, vector<int>& resultVec){
+    for (const vector<int>& row : matrix){
+        for (const int valueInt : row){
+            resultVec.push_back(valueInt);
+        }
+    }
+}
+vector<int> compute(vector<vector<int> > matrix){
+    vector<int> outputVec;
+    flatten(matrix, outputVec);
+    return outputVec;
+}
+void printVector(const vector<int>& vec){
+    for (int valueInt : vec){
+        cout << valueInt << " ";
+    }
+    cout << endl;
+}
+int main(){
+    vector<int> resultVec1 = compute({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    printVector(resultVec1);
+    return 0;
+}
