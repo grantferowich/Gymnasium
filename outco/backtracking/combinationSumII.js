4b555303acea462d810192a0dfbe2ef3a81c0db9
grantferowich <g.ferowich@gmail.com>
2023-08-12 10:39:04 -0500

    defined question and tests

---
+++ outco/backtracking/combinationSumII.js
@@ -0,0 +1,44 @@
+/* 40. Combination Sum II
+Medium
+9.3K
+233
+Companies
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+
+ 
+
+Example 1:
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+
+Example 2:
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+] */
+const combinationSumII = (arr, int) => {
+
+}
+
+/* Tests */
+const arr1 = [10,1,2,7,6,1,5];
+const int1 = 8;
+
+const arr2 = [2,5,2,1,2];
+const int2 = 5;
+
+const resultMatrix1 = combinationSumII(arr1, int1);
+const resultMatrix2 = combinationSumII(arr2, int2);
+/* Test results */
