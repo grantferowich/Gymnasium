ab118a13cf3ed8fa84dd5f2c09845480cd547272
grantferowich <g.ferowich@gmail.com>
2023-08-15 09:17:03 -0500

    append test status

---
+++ c++journey/outco_in_cpp/slidingWindow/bitFlip.cpp
@@ -0,0 +1,21 @@
+ /*
+ *  Problem 2: Bit Flip
+ *
+ *     Given an array of binary values (0 and 1) and N number of flips (convert
+ *     a 0 to a 1), determine the maximum number of consecutive 1's that can be
+ *     made.
+ *
+ *  Input: An Array of 1's and 0's, and an Integer N denoting the number of
+ *         flips
+ *  Output: Integer
+ *
+ *  Example: bitFlip([0,1,1,1,0,1,0,1,0,0], 2)
+ *  Result: 7
+ */
+
+// Time Complexity:
+// Auxiliary Space Complexity:
+int BitFlip(vector<int> arr, int N) {
+   //YOUR WORK HERE
+   return -1;
+}
