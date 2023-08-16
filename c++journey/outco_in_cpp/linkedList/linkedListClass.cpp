349a8e5390a3556d7d12ce63c694cbc13c56c20c
grantferowich <g.ferowich@gmail.com>
2023-08-16 06:24:42 -0500

    develop insert and append for linked list

---
+++ c++journey/outco_in_cpp/linkedList/linkedListClass.cpp
@@ -0,0 +1,161 @@
+/**
+ *  Homework - Linked List
+ *
+ *  Problem 1: ListNode class
+ *
+ *  Prompt:    Create a ListNode class
+ *
+ *             The ListNode class should contain the following public properties:
+ *
+ *                 value:   {Integer}
+ *                  next:   {ListNode} (initially NULL)
+ *
+ *               Example:   ListNode *sample = new ListNode(1)
+ *                          sample->value     // 1
+ *                          sample->next      // NULL
+ *
+ *
+ *  Problem 2: LinkedList class
+ *
+ *  Prompt:    Create a LinkedList class
+ *
+ *             The LinkedList class should contain the following public
+ *             properties:
+ *
+ *                length:   {Integer}
+ *                  head:   {ListNode}
+ *                  tail:   {ListNode}
+ *
+ *              The LinkedList class should also contain the following public
+ *              methods:
+ *
+ *                append:   A method that appends a value to the end of the
+ *                          LinkedList.
+ *
+ *                          Input:     {Integer}
+ *                          Output:    {Void}
+ *
+ *                insert:   A method that inserts an integer value at a set
+ *                          index in the LinkedList (assume head index is 0).
+ *
+ *                          Input:     value {Integer}
+ *                          Input:     index {Integer}
+ *                          Output:    {Void}
+ *
+ *                delete:   A method that removes a node at a specified index.
+ *
+ *                          Input:     index {Integer}
+ *                          Output:    {Void}
+ *
+ *              contains:   A method that checks to see if a value is contained
+ *                          in the list.
+ *
+ *                          Input:     value {Integer}
+ *                          Output:    {Boolean}
+ */
+
+#include <typeinfo>
+#include <type_traits>
+#include <iostream>
+using namespace std;
+
+class ListNode {
+   public:
+     int valueInt;
+     ListNode* nextNode;
+
+     ListNode(int inputInt) {
+       this->valueInt = inputInt;
+       this->nextNode = nullptr;
+     }
+};
+
+class LinkedList {
+  public:
+    int lengthInt = 0;
+    ListNode *headNode;
+    ListNode *tailNode;
+
+    LinkedList() {
+      this->headNode = nullptr;
+      this->tailNode = nullptr;
+    }
+
+    // Time Complexity:
+    // Auxiliary Space Complexity:
+    void insert_value(int valueInt, int indexInt){
+        ListNode *xNode = new ListNode(valueInt);
+        if (this->lengthInt == 0){
+            this->headNode = xNode;
+            xNode->nextNode = tailNode;
+            lengthInt++;
+            return;
+        }
+        if (this->lengthInt == 1){
+            this->headNode->nextNode = xNode;
+            xNode->nextNode = this->headNode;
+            lengthInt++;
+            return;
+        }
+        if (this->lengthInt > 1){
+            ListNode *node = this->headNode;
+            int currentIndexInt = 0;
+            while (node){
+                if (currentIndexInt == lengthInt - 1){
+                    node->nextNode = xNode;
+                    xNode->nextNode = this->tailNode;
+                }
+                node = node->nextNode;
+                currentIndexInt++;
+            }
+            lengthInt++;
+        }
+    }
+
+
+    // Time Complexity:
+    // Auxiliary Space Complexity:
+    void append_value(int valueInt){
+      this->insert_value(valueInt, lengthInt);
+    }
+
+
+    // Time Complexity:
+    // Auxiliary Space Complexity:
+
+    void delete_node(int index){
+      // YOUR WORK HERE
+    }
+
+
+    // Time Complexity:
+    // Auxiliary Space Complexity:
+    bool contains_value(int value){
+      // YOUR WORK HERE
+      return false;
+    }
+
+    void printLinkedList(){
+        ListNode *node = this->headNode;
+        cout << "Printing out the linked list: " << endl;
+        cout << "{" ;
+        while (node){
+            cout << node->valueInt << ", " << endl;
+            node = node->nextNode;
+        }
+        cout << "}" << endl;
+    }
+ };
+
+int main(){
+
+    LinkedList linkedListX;
+    linkedListX.append_value(3);
+    linkedListX.append_value(5);
+    linkedListX.append_value(8);
+    linkedListX.append_value(13);
+    linkedListX.append_value(21);
+    linkedListX.append_value(34);
+    linkedListX.printLinkedList();
+    return 0;
+}
