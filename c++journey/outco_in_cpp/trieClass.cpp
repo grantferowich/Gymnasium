88103193bb96f761d84a0d62906e999b0dd05426
grantferowich <g.ferowich@gmail.com>
2023-08-16 00:27:39 -0500

    trie

---
+++ c++journey/outco_in_cpp/trieClass.cpp
@@ -0,0 +1,105 @@
+/**
+ *  Homework - Trie
+ *
+ *  Problem 1: TrieNode class
+ *
+ *  Prompt:    Create a TrieNode class
+ *             The TrieNode class should contain the following properties:
+ *
+ *                 value:   {Char}
+ *                  children:   {Map}
+ *                   end:   {bool}
+ *
+ *
+ *  Problem 2:  Trie class
+ *
+ *  Prompt:     Create a Trie class
+ *              The Trie class should contain the following properties:
+ *
+ *                  root:   {TrieNode}
+ *
+ *              The Trie class should also contain the following methods:
+ *
+ *                insert:   A method that adds a word.
+ *
+ *                          Input:     word {string}
+ *                          Output:    void
+ *
+ *                isWord:   A method that checks whether a word is in the trie.
+ *
+ *                          Input:     word {string}
+ *                          Output:    bool
+ *
+ *              isPrefix:   A method that checks whether an input is a prefix of
+ *                          a word in the string.
+ *
+ *                          Input:     prefix {string}
+ *                          Output:    bool
+ *
+ *            startsWith:   A method that returns all words that starts with an
+ *                          input word.
+ *
+ *                          Input:     prefix {string}
+ *                          Output:    string[]
+ *
+ *          EXTRA CREDIT:
+ *                remove:   Removes a word from the trie.
+ *
+ *                          Input:     word {string}
+ *                          Output:    void
+ */
+
+#include <vector>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <typeinfo>
+using namespace std;
+
+// DO NOT EDIT
+// TrieNode class
+class TrieNode {
+  public:
+    char value;
+    unordered_map<char, TrieNode*> children;
+    bool end;
+
+    TrieNode(char val) {
+        value = val;
+        end = false;
+    }
+};
+
+class Trie {
+
+    public:
+      TrieNode *root = new TrieNode('\u0000');
+
+      bool insert(string word) {
+        // YOUR WORK HERE
+        return false;
+      }
+
+      bool isWord(string word) {
+        // YOUR WORK HERE
+        return false;
+      }
+
+      bool isPrefix(string prefix) {
+        // YOUR WORK HERE
+        return false;
+      }
+
+      vector<string> startsWith(string prefix) {
+        // YOUR WORK HERE
+        return vector<string>{};
+      }
+
+      void remove(string word) {
+        // YOUR WORK HERE
+      }
+
+};
+
