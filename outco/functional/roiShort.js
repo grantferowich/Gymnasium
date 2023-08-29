208405aebe4547107f644630270824589b81680d
grantferowich <g.ferowich@gmail.com>
2023-08-29 08:17:13 -0500

    developed short roi calculation

--- outco/functional/roiShort.js
+++ outco/functional/roiShort.js
@@ -6,33 +6,39 @@ const axios = require('axios');
 const calculateInitialPositionInt = async (APISymbolStr, quantityInt ,indexDateStr) => {     
     let historicalPriceAPIStr = "https://api.coingecko.com/api/v3/coins/"+APISymbolStr+"/history?date="+indexDateStr+"&localization=false";
     let historicalPriceInt = await axios.get(historicalPriceAPIStr).then((response) => response.data.market_data.current_price.usd);
-    let initialPositionValueInt = parseInt(historicalPriceInt * quantityInt);
-    return initialPositionValueInt;
+    let initialPositionValueInt = historicalPriceInt * quantityInt;
+    return initialPositionValueInt.toFixed(2);
 }
 
-const calculatePresentPositionInt = async (APISymbolStr, quantityInt, indexDateStr) => {
+const calculateDifferenceInt = async (APISymbolStr, quantityInt, indexDateStr) => {
     let historicalPriceAPIStr = "https://api.coingecko.com/api/v3/coins/"+APISymbolStr+"/history?date="+indexDateStr+"&localization=false";
     let historicalPriceInt = await axios.get(historicalPriceAPIStr).then((response) => response.data.market_data.current_price.usd);
     let presentPriceAPIStr = "https://api.coingecko.com/api/v3/simple/price?ids="+APISymbolStr+"&vs_currencies=usd";
     let presentPriceInt = await axios.get(presentPriceAPIStr).then((response) => response.data[APISymbolStr].usd); 
     let differenceInt = -1 * (presentPriceInt - historicalPriceInt) * quantityInt;
-    let initialPositionValueInt = await calculateInitialPositionInt(APISymbolStr, quantityInt, indexDateStr);
-    let presentPositionValueInt = initialPositionValueInt + differenceInt;
-    return presentPositionValueInt;
+    return differenceInt.toFixed(2)
 }
+
+const calculatePresentPositionInt = (initialPositionValueInt, differenceInt) => {
+    let currentPositionInt = parseFloat(initialPositionValueInt) + parseFloat(differenceInt)
+    return currentPositionInt.toFixed(2)
+}   
 
 const calculatePercentageROIInt = (initialInt, presentInt) => {
-    return ((presentInt - initialInt) / initialInt) * 100
+    let percentageInt = ((presentInt - initialInt) / initialInt) * 100;
+    return percentageInt.toFixed(2)
 }
 
-const callFunction = async () => {
+const callFunctions = async () => {
     const initialBitcoinPositionValueInt = await calculateInitialPositionInt("bitcoin", 1, "01-01-2023");
     console.log("Intial Bitcoin position value: $", initialBitcoinPositionValueInt);
-    const presentBasketValueInt = await calculatePresentPositionInt("bitcoin", 1, "01-01-2023");
-    console.log("Present Bitcoin position value: $", presentBasketValueInt)
-    const percentageROIInt = calculatePercentageROIInt(initialBitcoinPositionValueInt, presentBasketValueInt);
-    console.log("ROI in percentage terms:", percentageROIInt)
+    const differenceBetweenInitialAndCurrentInt = await calculateDifferenceInt("bitcoin", 1, "01-01-2023")
+    console.log("Difference between initial position and current position in $:", differenceBetweenInitialAndCurrentInt);
+    const presentPositionInt = calculatePresentPositionInt(initialBitcoinPositionValueInt, differenceBetweenInitialAndCurrentInt);
+    console.log("Present Bitcoin position value: $", presentPositionInt)
+    const percentageROIInt = calculatePercentageROIInt(initialBitcoinPositionValueInt, presentPositionInt);
+    console.log("ROI in % terms:", percentageROIInt)
 }
 
 
-callFunction();
+callFunctions();
