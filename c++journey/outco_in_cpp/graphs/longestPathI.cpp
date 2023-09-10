ec183d325a933e252e014dea4cfe103822404b93
grantferowich <g.ferowich@gmail.com>
2023-09-10 10:01:36 -0500

    start longest path

---
+++ c++journey/outco_in_cpp/graphs/longestPathI.cpp
@@ -0,0 +1,34 @@
+/*
+ *  Longest Path I
+ *
+ *  Given a DAG (directed acyclic graph), find the longest path in the graph.
+ *
+ *  Parameters:
+ *
+ *  Input: Graph: [[int]] (edge list)
+ *  Output: int
+ *
+ *  Example:
+ *
+ *  Input: {{1,2},{2,3},{1,3}}
+ *  Output: {1,2,3}
+ *
+ *  Input: {{6,5},{6,4},{5,4},{4,3},{2,3},{1,2},{4,1}}
+ *  Output: {6,5,4,1,2,3}
+ *
+ *  Resources:
+ *  - https://www.geeksforgeeks.org/find-longest-path-directed-acyclic-graph/
+ *
+ */
+
+
+
+vector<int> longestPathI(vector<vector<int>> edges) {
+   // YOUR WORK HERE
+   return vector<int>{};
+}
+
+int main(){
+    
+    return 0;
+}
