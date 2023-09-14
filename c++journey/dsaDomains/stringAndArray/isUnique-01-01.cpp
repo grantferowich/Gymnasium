d683ffb74eaed22adf203b885657f4cb5ecdcd63
grantferowich <g.ferowich@gmail.com>
2023-09-14 08:46:15 -0500

    isUnique w/ds

---
+++ c++journey/dsaDomains/stringAndArray/isUnique-01-01.cpp
@@ -0,0 +1,64 @@
+/* 
+Is Unique
+CTCI 1.01
+Started development Sept 14, 2023
+
+Determine if a string has all unique characters. 
+What if you cannot use additional data structures?
+ 
+- str1 = "wake"
+- output1 = true
+
+- str2 = "wake forest"
+- output2 = false
+why? There are two e characters.
+
+*/
+#include <iostream> 
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+bool isUnique(string inputStr){
+    unordered_map<char, int> charMap;
+    for (char xChar: inputStr){
+        if (charMap.find(xChar) == charMap.end()){
+            charMap[xChar] = 1;
+        } else {
+            int frequencyInt = charMap[xChar];
+            charMap[xChar] = frequencyInt + 1;
+        }
+    }
+
+    for (auto &pair: charMap){
+        if (pair.second > 1){
+            return false;
+        }
+    }
+    return true;
+}
+
+string printToFToString(bool inputToF){
+        if (inputToF == 1){
+            return "true";
+        }
+        if (inputToF == 0){
+            return "false";
+        }
+        return "false";
+}
+
+int main(){
+    string inputStr1 = "wake";
+    bool outputToF1 = isUnique(inputStr1);
+    string outputStr1 = printToFToString(outputToF1);
+    cout << "Result 1: " << outputStr1 << ". Expect true." << endl;
+
+    string inputStr2 = "wake forest";
+    bool outputToF2 = isUnique(inputStr2);
+    string outputStr2 = printToFToString(outputToF2);
+    cout << "Result 2: " << outputStr2 << ". Expect false." << endl;
+    return 0;
+}
+
+ 
