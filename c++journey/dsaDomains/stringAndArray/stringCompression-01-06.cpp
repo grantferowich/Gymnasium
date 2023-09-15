cc276d0213cbeeb3ddce322d3e6626e42ea0bec3
grantferowich <g.ferowich@gmail.com>
2023-09-15 07:27:27 -0500

    string compression

---
+++ c++journey/dsaDomains/stringAndArray/stringCompression-01-06.cpp
@@ -0,0 +1,49 @@
+/* Grant Ferowich
+String Compression
+Developed on Sept 15, 2023
+Cracking the Coding Interview
+Ch. 1 problem 6
+
+- input = "aabcccccdd"
+- output = "a2b1c5d2"
+ */
+#include <iostream> 
+#include <string>
+#include <sys/types.h>
+#include <unordered_map>
+#include <vector>
+#include <chrono>
+#include <iomanip> 
+#include <map>
+using namespace std;
+using namespace std::chrono;
+
+class Solution{
+    public:
+    string stringCompression(string inputStr){
+        string outputStr;
+        map<char, int> charMap;
+        for (char xChar: inputStr){
+            if (charMap.find(xChar) == charMap.end()){
+                charMap[xChar] = 1;
+            } else {
+                int frequencyInt = charMap[xChar];
+                charMap[xChar] = frequencyInt + 1;
+            }
+        }
+        for (auto pair : charMap){
+            outputStr += pair.first;
+            outputStr += to_string(pair.second);
+        }
+        return outputStr;
+    }
+};
+
+int main(){
+    Solution solutionX;
+    string inputStr1 = "aabcccccdd";
+    string outputStr1 = solutionX.stringCompression(inputStr1);
+    cout << "Result 1: " << outputStr1 << ". Expect a2b1c5d2." << endl;
+
+    return 0;
+}
