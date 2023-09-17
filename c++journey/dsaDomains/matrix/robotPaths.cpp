6fc985e203e7de583b3fbd05dd0dd22412fdc3d5
grantferowich <g.ferowich@gmail.com>
2023-09-17 12:38:44 -0500

    robot paths

---
+++ c++journey/dsaDomains/matrix/robotPaths.cpp
@@ -0,0 +1,34 @@
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+/*
+ *  Problem 1:  Robot Paths
+ *
+ *  Prompt:   Given a matrix of zeroes, determine how many unique paths exist
+ *            from the top left corner to the bottom right corner
+ *
+ *  Input:    An Array of Array of ints (matrix)
+ *  Output:   int
+ *
+ *  Example:  matrix = [[0,0,0,0],
+ *                      [0,0,0,0],
+ *                      [0,0,0,0]]
+ *
+ *            robotPaths(matrix) = 38
+ *
+ *
+ *            matrix = [[0,0,0],
+ *                      [0,0,0]]
+ *
+ *            robotPaths(matrix) = 4
+ *
+ *  Note:     From any point, you can travel in the four cardinal directions
+ *            (north, south, east, west). A path is valid as long as it travels
+ *            from the top left corner to the bottom right corner, does not go
+ *            off of the matrix, and does not travel back on itself
+ */
