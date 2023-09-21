99689ea80f689b2ba71ec96e4507204249aafd7c
grantferowich <g.ferowich@gmail.com>
2023-09-21 13:46:36 -0500

    dfs

--- c++journey/dsaDomains/matrix/robotPaths.cpp
+++ c++journey/dsaDomains/matrix/robotPaths.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <unordered_set>
 using namespace std;
 
 
@@ -34,9 +35,39 @@ using namespace std;
  */
 
  class Solution{
-    public:
-    int robotPaths(vector<vector<int> > matrix){
+   public:
 
+   void depthFirstSearch(int rowInt, int colInt, vector<vector<int> > &matrix, unordered_set<string> &vSet, int &outputInt){
+    
+    // OOB
+    if (rowInt < 0 || rowInt >= matrix.size() || colInt < 0 || colInt >= matrix[0].size()){
+        return;
+    }
+    string keyStr = to_string(rowInt)+"_"+to_string(colInt);
+    // already visited this 1
+    if (vSet.find(keyStr) != vSet.end()){
+        return;
+    }
+    // not a 1
+    if (matrix[rowInt][colInt] == 0){
+        return;
+    }
+
+        if (rowInt == matrix.size() && colInt == matrix[0].size()){
+      outputInt++;
+      return;
+    }
+    vSet.insert(keyStr);
+    depthFirstSearch(rowInt + 1, colInt, matrix, vSet, outputInt);
+    depthFirstSearch(rowInt - 1, colInt, matrix, vSet, outputInt);
+    depthFirstSearch(rowInt, colInt + 1, matrix, vSet, outputInt);
+    depthFirstSearch(rowInt, colInt - 1, matrix, vSet, outputInt);
+}
+    int robotPaths(vector<vector<int> > matrix){
+      int outputInt = 0;
+      unordered_set<string> visitedSet;
+      depthFirstSearch(0, 0, matrix, visitedSet, outputInt);
+      return outputInt;
     }
 
  };
