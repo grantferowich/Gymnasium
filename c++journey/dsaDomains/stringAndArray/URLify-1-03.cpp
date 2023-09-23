1659198d6482ba1e167dc320ef6749a5758be834
grantferowich <g.ferowich@gmail.com>
2023-09-23 14:54:50 -0500

    two sum

--- c++journey/dsaDomains/stringAndArray/URLify-1-03.cpp
+++ c++journey/dsaDomains/stringAndArray/URLify-1-03.cpp
@@ -15,6 +15,7 @@
 #include <vector>
 #include <chrono>
 #include <iomanip> 
+#include <stack>
 using namespace std;
 using namespace std::chrono;
 
@@ -24,6 +25,16 @@ class Solution{
             string newStr = "%20";
             int xInt = 0;
             string outputStr = "";
+            int kInt = inputStr.size() - 1;
+            while (kInt >= 0){
+                if (inputStr[kInt] == ' '){
+                    inputStr.erase(inputStr[kInt]);
+                } else {
+                    break;
+                }
+                kInt--;
+            }
+            cout << "inputStr: " << inputStr << endl;
             for (char xChar:inputStr){
                 if (xChar == ' '){
                     outputStr += newStr;
@@ -38,6 +49,12 @@ class Solution{
 int main(){
     Solution solutionX;
     string inputStr1 = "chicago illinois north america";
-    cout << "Result 1: " << solutionX.URLify(inputStr1) << endl;
+    string outputStr1 = solutionX.URLify(inputStr1);
+    cout << "Result 1: " << outputStr1 << endl;
+    cout << "Result 1 size: " << outputStr1.size();
+    string inputStr2 = "chicago illinois north america ";
+    string outputStr2 = solutionX.URLify(inputStr2);
+    cout << "Result 2: " << outputStr2 << endl;
+    cout << "Result 2 size: " << outputStr2.size();
     return 0;
 }
---
+++ c++journey/dsaDomains/stringAndArray/twoSum-1.cpp
@@ -0,0 +1,52 @@
+/* 
+Grant Ferowich
+September 23, 2023
+
+1. Two Sum
+Easy
+51.8K
+1.7K
+Companies
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numsVec, int targetInt) {
+        unordered_map<int, int> intMap;
+        for (int valInt: numsVec){
+            intMap[valInt] = targetInt - valInt;
+        }
+        for (auto &pair: intMap){
+            int valInt = targetInt - pair.first;
+            if (intMap.find(valInt) != intMap.end()){
+                int int1 = find(numsVec.begin(), numsVec.end(), valInt) - numsVec.begin();
+                int int2 = find(numsVec.begin(), numsVec.end(), pair.first) - numsVec.begin();
+                cout << "Int 1.." << int1 << endl;
+                cout << "Int 2.." << int2 << endl;
+                return {int1, int2};
+            }   
+        }
+        return {};
+    }
+};
