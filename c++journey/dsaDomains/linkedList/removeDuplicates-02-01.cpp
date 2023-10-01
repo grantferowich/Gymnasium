07cdba764317a28fc09ccbe532865c16b72c36d2
grantferowich <g.ferowich@gmail.com>
2023-10-01 15:07:52 -0500

    develop remove duplicates in c++

--- c++journey/dsaDomains/linkedList/removeDuplicates-02-01.cpp
+++ c++journey/dsaDomains/linkedList/removeDuplicates-02-01.cpp
@@ -12,16 +12,256 @@
  * frequency map. 
 
  * The function takes in the head of the linked list. 
+ * The function returns a linked list. 
  */
 #include <iostream> 
+#include <unordered_map>
 using namespace std;
+
+class ListNode {
+   public:
+    string str;
+    int valueInt;
+    vector<int> vecInt;
+    ListNode* nextNode;
+
+    ListNode(int inputInt) {
+       this->valueInt = inputInt;
+    }
+
+    ListNode(string str){
+        this->str = str;
+    }
+
+    ListNode(vector<int> vecInt){
+        this->vecInt = vecInt;
+    }
+
+    string getStringID(){
+        return this->str;
+    }
+
+    int getIntID(){
+        return this->valueInt;
+    }
+
+    vector<int> getVecIntID(){
+        return this->vecInt;
+    }
+};
+
+class LinkedList {
+  public:
+    int lengthInt;
+    ListNode *headNode, *tailNode;
+
+    LinkedList() {
+      this->headNode = nullptr;
+      this->tailNode = nullptr;
+      this->lengthInt = 0;
+    };
+
+    // Time Complexity: O(N)
+    // Auxiliary Space Complexity: O(1)
+    void insertValue(ListNode *xNode, int indexInt){
+        if (indexInt < 0 || indexInt > this->lengthInt){
+          return;
+        }
+        if (this->lengthInt == 0){
+            this->headNode = xNode;
+            xNode->nextNode = tailNode;
+            lengthInt++;
+            return;
+        }
+        if (this->lengthInt == 1){
+            this->headNode->nextNode = xNode;
+            xNode->nextNode = this->tailNode;
+            lengthInt++;
+            return;
+        }
+        if (this->lengthInt > 1){
+            ListNode *node = this->headNode;
+            int currentIndexInt = 0;
+            while (node){
+                if (currentIndexInt == lengthInt - 1){
+                    node->nextNode = xNode;
+                    xNode->nextNode = this->tailNode;
+                    lengthInt++;
+                    return;
+                }
+                node = node->nextNode;
+                currentIndexInt++;
+            }
+        }
+    }
+
+    // Time Complexity: O(N)
+    // Auxiliary Space Complexity: O(1)
+    void appendValue(int valueInt){
+      ListNode *xNode = new ListNode(valueInt);
+      this->insertValue(xNode, this->lengthInt);
+    };
+
+    void appendValue(string inputStr){
+      ListNode *xNode = new ListNode(inputStr);
+      this->insertValue(xNode, lengthInt);
+    };
+
+    void appendValue(vector<int> inputVec){
+      ListNode *xNode = new ListNode(inputVec);
+      this->insertValue(xNode, lengthInt);
+    };
+
+    // Time Complexity:O(N)
+    // Auxiliary Space Complexity: O(1)
+    ListNode* deleteNode(int indexInt){
+      if (indexInt < 0 || indexInt >= this->lengthInt){
+        return nullptr;
+      }
+      ListNode *toDeleteNode = nullptr;
+
+      if (indexInt == 0){
+        toDeleteNode = this->headNode;
+        this->headNode = this->headNode->nextNode;
+        if (this->lengthInt == 1){
+          this->tailNode = nullptr;
+        }
+      } else {
+        ListNode *node = this->headNode;
+        int currentIndexInt = 0;
+        while (node){
+          if (currentIndexInt == indexInt - 1){
+            toDeleteNode = node->nextNode;
+            node->nextNode = node->nextNode->nextNode;
+            if (indexInt == this->lengthInt - 1){
+              this->tailNode = node;
+            }
+            break;
+          }
+          node = node->nextNode;
+          currentIndexInt++;
+        }
+      }
+      lengthInt--;
+      return toDeleteNode;
+    };
+
+    // Time Complexity: o(N)
+    // Auxiliary Space Complexity: O(1)
+    bool containsValue(int valueInt){
+      ListNode *node = this->headNode;
+      while (node){
+        if (node->valueInt == valueInt){
+          return true;
+        }
+        node = node->nextNode;
+      }
+      return false;
+    }
+
+    bool containsStr(string inputStr){
+      ListNode *node = this->headNode;
+      while (node){
+        if (node->getStringID() == inputStr){
+          return true;
+        }
+        node = node->nextNode;
+      }
+      return false;
+    }
+
+    void printLinkedListInt(){
+        ListNode *node = this->headNode;
+        cout << "Printing out the linked list: " << endl;
+        cout << "{ ";
+        while (node){
+            cout << node->valueInt;
+            node = node->nextNode;
+            if (node){
+              cout << ", ";
+            }
+        }
+        cout << " }" << endl;
+    }
+
+    void printLinkedListStr(){
+      ListNode *node = this->headNode;
+      cout << "Printing out the linked list: " << endl;
+      cout << "{ ";
+      while (node){
+        cout << node->getStringID();
+        node = node->nextNode;
+        if (node){
+          cout << ", ";
+        }
+      }
+      cout << " }" << endl;
+    }
+
+    void printLinkedListVec(){
+      ListNode *node = this->headNode;
+      cout << "Printing out the linked list: " << endl;
+      cout << "{ ";
+      while (node){
+        vector<int> vec = node->getVecIntID();
+        int xInt = 0;
+        while (xInt < vec.size()){
+          cout << vec[xInt];
+          if (xInt < vec.size() - 1 || node->nextNode != nullptr){
+            cout << ", ";
+          }
+          xInt++;
+        }
+        node = node->nextNode;
+      }
+      cout << " }" << endl;
+    };
+};
 
 class Solution{
     public: 
+    void removeDuplicates(LinkedList* inputList){
+        unordered_map<int, int> frequencyMap;
+        ListNode *node = inputList->headNode;
+        
+        while (node){
+            int valueInt = node->getIntID();
+            if (frequencyMap.find(valueInt) == frequencyMap.end()){
+                frequencyMap[valueInt] = 1;
+            } else {
+                int frequencyInt = frequencyMap[valueInt];
+                frequencyMap[valueInt] = frequencyInt + 1;
+            }
+            node = node->nextNode;
+        }
 
-}
+        node = inputList->headNode;
+        while (node){
+
+            if (frequencyMap[node->valueInt] > 1){
+                inputList->deleteNode(node->valueInt);
+                int frequencyInt = frequencyMap[node->valueInt];
+                frequencyMap[node->valueInt] = frequencyInt - 1;
+            }
+            node = node->nextNode;
+        }
+    }
+};
 
  int main(){
     Solution solutionX;
+    LinkedList listK;
+    listK.appendValue(1);
+    listK.appendValue(1);
+    listK.appendValue(3);
+    listK.appendValue(3);
+    listK.appendValue(2); 
+    listK.appendValue(2);
+    cout << "Printing linked list before removeDuplicates()...";
+    listK.printLinkedListInt();
+    cout << endl;
+    solutionX.removeDuplicates(&listK);
+    cout << "Linked List after removeDuplicates()...";
+    listK.printLinkedListInt();
     return 0;
  }
