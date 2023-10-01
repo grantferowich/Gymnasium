9b6a5a96254a0306301eddbbb7647189f7804a91
grantferowich <g.ferowich@gmail.com>
2023-10-01 15:31:04 -0500

    develop kth from last

---
+++ c++journey/dsaDomains/linkedList/returnKthToLast-02-02.cpp
@@ -0,0 +1,258 @@
+
+#include <iostream> 
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class ListNode {
+   public:
+    string str;
+    int valueInt;
+    vector<int> vecInt;
+    ListNode* nextNode;
+
+    ListNode(int inputInt) {
+       this->valueInt = inputInt;
+    }
+
+    ListNode(string str){
+        this->str = str;
+    }
+
+    ListNode(vector<int> vecInt){
+        this->vecInt = vecInt;
+    }
+
+    string getStringID(){
+        return this->str;
+    }
+
+    int getIntID(){
+        return this->valueInt;
+    }
+
+    vector<int> getVecIntID(){
+        return this->vecInt;
+    }
+};
+
+class LinkedList {
+  public:
+    int lengthInt;
+    ListNode *headNode, *tailNode;
+
+    LinkedList() {
+      this->headNode = nullptr;
+      this->tailNode = nullptr;
+      this->lengthInt = 0;
+    };
+
+    // Time Complexity: O(N)
+    // Auxiliary Space Complexity: O(1)
+    void insertValue(ListNode *xNode, int indexInt){
+        if (indexInt < 0 || indexInt > this->lengthInt){
+          return;
+        }
+        if (this->lengthInt == 0){
+            this->headNode = xNode;
+            xNode->nextNode = tailNode;
+            lengthInt++;
+            return;
+        }
+        if (this->lengthInt == 1){
+            this->headNode->nextNode = xNode;
+            xNode->nextNode = this->tailNode;
+            lengthInt++;
+            return;
+        }
+        if (this->lengthInt > 1){
+            ListNode *node = this->headNode;
+            int currentIndexInt = 0;
+            while (node){
+                if (currentIndexInt == lengthInt - 1){
+                    node->nextNode = xNode;
+                    xNode->nextNode = this->tailNode;
+                    lengthInt++;
+                    return;
+                }
+                node = node->nextNode;
+                currentIndexInt++;
+            }
+        }
+    }
+
+    // Time Complexity: O(N)
+    // Auxiliary Space Complexity: O(1)
+    void appendValue(int valueInt){
+      ListNode *xNode = new ListNode(valueInt);
+      this->insertValue(xNode, this->lengthInt);
+    };
+
+    void appendValue(string inputStr){
+      ListNode *xNode = new ListNode(inputStr);
+      this->insertValue(xNode, lengthInt);
+    };
+
+    void appendValue(vector<int> inputVec){
+      ListNode *xNode = new ListNode(inputVec);
+      this->insertValue(xNode, lengthInt);
+    };
+
+    // Time Complexity:O(N)
+    // Auxiliary Space Complexity: O(1)
+    ListNode* deleteNode(int indexInt){
+      if (indexInt < 0 || indexInt >= this->lengthInt){
+        return nullptr;
+      }
+      ListNode *toDeleteNode = nullptr;
+
+      if (indexInt == 0){
+        toDeleteNode = this->headNode;
+        this->headNode = this->headNode->nextNode;
+        if (this->lengthInt == 1){
+          this->tailNode = nullptr;
+        }
+      } else {
+        ListNode *node = this->headNode;
+        int currentIndexInt = 0;
+        while (node){
+          if (currentIndexInt == indexInt - 1){
+            toDeleteNode = node->nextNode;
+            node->nextNode = node->nextNode->nextNode;
+            if (indexInt == this->lengthInt - 1){
+              this->tailNode = node;
+            }
+            break;
+          }
+          node = node->nextNode;
+          currentIndexInt++;
+        }
+      }
+      lengthInt--;
+      return toDeleteNode;
+    };
+
+    // Time Complexity: o(N)
+    // Auxiliary Space Complexity: O(1)
+    bool containsValue(int valueInt){
+      ListNode *node = this->headNode;
+      while (node){
+        if (node->valueInt == valueInt){
+          return true;
+        }
+        node = node->nextNode;
+      }
+      return false;
+    }
+
+    bool containsStr(string inputStr){
+      ListNode *node = this->headNode;
+      while (node){
+        if (node->getStringID() == inputStr){
+          return true;
+        }
+        node = node->nextNode;
+      }
+      return false;
+    }
+
+    void printLinkedListInt(){
+        ListNode *node = this->headNode;
+        cout << "Printing out the linked list: " << endl;
+        cout << "{ ";
+        while (node){
+            cout << node->valueInt;
+            node = node->nextNode;
+            if (node){
+              cout << ", ";
+            }
+        }
+        cout << " }" << endl;
+    }
+
+    void printLinkedListStr(){
+      ListNode *node = this->headNode;
+      cout << "Printing out the linked list: " << endl;
+      cout << "{ ";
+      while (node){
+        cout << node->getStringID();
+        node = node->nextNode;
+        if (node){
+          cout << ", ";
+        }
+      }
+      cout << " }" << endl;
+    }
+
+    void printLinkedListVec(){
+      ListNode *node = this->headNode;
+      cout << "Printing out the linked list: " << endl;
+      cout << "{ ";
+      while (node){
+        vector<int> vec = node->getVecIntID();
+        int xInt = 0;
+        while (xInt < vec.size()){
+          cout << vec[xInt];
+          if (xInt < vec.size() - 1 || node->nextNode != nullptr){
+            cout << ", ";
+          }
+          xInt++;
+        }
+        node = node->nextNode;
+      }
+      cout << " }" << endl;
+    };
+};
+/* 
+* Grant Ferowich
+* Started development on October 1, 2023
+* Cracking the Coding Interview
+* Chapter 2, question 2: Return Kth To Last
+
+* Implement an algorithm to find the kth to last element 
+* of a singly linked list. 
+
+* Example
+* inputList1: 7 - 8 - 13 - 21 - 34 - 55
+* kInt1: 3
+* output: 21
+* The node whose value we want is located at the list.length() - k position. So, if length is 6,
+* k is three, we want node at place three, which here has the value 21, which is correct 
+- call inputList.returnKthToLast
+ */
+
+class Solution{
+    public:
+    int returnKthFromLast(LinkedList inputList, int kInt){
+        if (kInt > inputList.lengthInt){
+            return -1;
+        }
+        ListNode *node = inputList.headNode;
+        int listLengthInt = inputList.lengthInt;
+        int removeInt = listLengthInt - kInt;
+        int indexInt = 0;
+        while (node){   
+            if (removeInt == indexInt){
+                return node->valueInt;
+            }
+            indexInt++;
+            node = node->nextNode;
+        }
+        return -1;
+    }
+};
+
+int main(){
+    Solution solutionX;
+    LinkedList listK;
+    listK.appendValue(7);
+    listK.appendValue(8);
+    listK.appendValue(13);
+    listK.appendValue(21);
+    listK.appendValue(34);
+    listK.appendValue(55);
+    // return the 3rd from last, aka 21
+    int outputInt1 = solutionX.returnKthFromLast(listK, 3);
+    cout << "Result 1: " << outputInt1 << ". Expect 21." << endl;
+    return 0;
+}
