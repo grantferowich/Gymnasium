0a36ccfc5121a8dc840ebe4206a1626c35b85344
grantferowich <g.ferowich@gmail.com>
2023-10-01 14:31:06 -0500

    remove dup

---
+++ c++journey/dsaDomains/linkedList/removeDuplicates-02-01.cpp
@@ -0,0 +1,27 @@
+/* 
+ * Grant Ferowich
+ * Developed Sunday October 1, 2023.
+ * Chapter 2, question 1: Remove Duplicates
+
+ * Write code to remove duplicates from an unsorted linked list.
+
+ * For example, 3 - 3 - 2 - 2 - 1 - 1 becomes 3 - 2 - 1. 
+
+ * The approach here is to make a hash map of integers and integers to determine the frequency of each key.
+ * Delete the keys when the frequency is above one. When deleting a node, decrement the frequency value in the 
+ * frequency map. 
+
+ * The function takes in the head of the linked list. 
+ */
+#include <iostream> 
+using namespace std;
+
+class Solution{
+    public: 
+
+}
+
+ int main(){
+    Solution solutionX;
+    return 0;
+ }
