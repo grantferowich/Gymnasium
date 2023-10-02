fe08907198242af2576df604bd82643f9c3fdbe6
grantferowich <g.ferowich@gmail.com>
2023-10-02 14:40:38 -0500

    start

---
+++ c++journey/dsaDomains/linkedList/partition-02-04.cpp
@@ -0,0 +1,14 @@
+/* 
+* Grant Ferowich 
+* Developed on Monday October 2, 2023
+* Cracking the Coding Interview
+* Chapter 2, question 4: Partitiom 
+* Return a list which is partitioned around a specific input value.
+* In the returned list, all of the values to the right of the returned
+* list should be larger than the partition value. In the 
+* returned list, all of the values to the left of the partition
+* value should be less than or equal to the partition value.
+
+
+*  
+*/
