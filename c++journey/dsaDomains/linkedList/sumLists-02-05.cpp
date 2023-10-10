0881e4ccfd1ec46fc0cba7f078c6be9d53591198
grantferowich <g.ferowich@gmail.com>
2023-10-09 19:14:52 -0500

    sum list

--- c++journey/dsaDomains/linkedList/sumLists-02-05.cpp
+++ c++journey/dsaDomains/linkedList/sumLists-02-05.cpp
@@ -1,6 +1,6 @@
 /* 
  * Grant Ferowich
- * Developed Tuesday Oct 3, 2023
+ * Developed Monday Oct 9, 2023
  * Cracking the Coding Interview
  * Chapter 2, question 5: Sum Lists
  * * *
@@ -9,4 +9,19 @@
  * integer2 = { 5, 9, 2 } OR 295
  * Output1 = { 2, 1, 9} OR 912
  
+ * You have two numbers represented by a linked list,
+ * where each node contains a single digit. The digits are
+ * are stored in reverse order, such that the 1's digit
+ * is at the head of the list. Write a function that adds
+ * the two numbers and returns the sum as a linked list. 
+ * (You are not allowed to "cheat" and just convert the linked
+ * list to an integer.)
+ * Input: Linked list
+ * Output: Linked list
+ * Constraints: 
+ * Edge: 
+ * Time complexity: O(k) where k is the number of nodes in the 
+ * longest linked list of the input linked lists.
+ * 
  */
+
