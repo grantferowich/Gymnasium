18b39e3e4ecb163eb7785d8704d68ed29605d742
grantferowich <g.ferowich@gmail.com>
2024-02-04 09:28:08 -0600

    2024

--- outco/decreaseAndConquer/euclidsAlgorithm.js
+++ outco/decreaseAndConquer/euclidsAlgorithm.js
@@ -2,11 +2,16 @@
 *Euclid's Algorithm
 
 * Successfully tested the function 03/17/2023.
-
+* Successfully tested the function on 02/04/2024.
 *Given A and B take the difference of A and B to find the greatest common divisor.
 * To find the GCD of 78 and 52 
 
-* (A , B)
+* 1, (A , B)
+* 2, A - B = C
+* 3, (B, C)
+* 4, repeat 1-3 until C === 0
+* 5, when C === 0 return A
+
 * (78, 52 ) 78 - 52 = 26
 * (52, 26) 52 -26 = 26
 * (26, 26) 26-26 = 0
@@ -23,12 +28,10 @@ const gcd = (aInt, bInt) => {
     // ensure inputs are positive
     if (aInt < 0) { aInt *= -1};
     if (bInt < 0) { bInt *= -1};
-
     // ensure A is larger than B
     if ( aInt < bInt){
         [aInt, bInt] = [bInt, aInt]
     }
-
     // for (A,B) take the difference of the two integers
     // then set the new pair to (b, difference)
     while (bInt > 0){
@@ -36,9 +39,10 @@ const gcd = (aInt, bInt) => {
         tempInt = aInt - bInt;
         [aInt, bInt] = [bInt,tempInt];
     }
-    return console.log(aInt);
+    return aInt;
 }
-
-gcd(78,52)
+console.log('hi')
+const int1 = gcd(78,52)
+console.log(`Int 1 is: ${int1}`);
 // expected output: 26
 // actual output: 26
