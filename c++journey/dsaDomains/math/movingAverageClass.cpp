9077b031243cc55aaa622e7d2c35ad594630b5cc
grantferowich <g.ferowich@gmail.com>
2024-03-03 07:36:55 -0600

    movingAverage class in cpp

---
+++ c++journey/dsaDomains/math/movingAverageClass.cpp
@@ -0,0 +1,440 @@
+#include <cstddef>
+#include <string>
+#include <sys/resource.h>
+#include <typeinfo>
+#include <type_traits>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class ListNode {
+   public:
+    string str;
+    int valueInt;
+    vector<int> vecInt;
+    ListNode* nextNode;
+
+    ListNode(int inputInt) {
+       this->valueInt = inputInt;
+    }
+
+    ListNode(string str){
+        this->str = str;
+    }
+
+    ListNode(vector<int> vecInt){
+        this->vecInt = vecInt;
+    }
+
+    string getStringID(){
+        return this->str;
+    }
+
+    int getIntID(){
+        return this->valueInt;
+    }
+
+    vector<int> getVecIntID(){
+        return this->vecInt;
+    }
+};
+
+class LinkedList {
+  public:
+    int lengthInt;
+    ListNode *headNode, *tailNode;
+
+    LinkedList() {
+      this->headNode = nullptr;
+      this->tailNode = nullptr;
+      this->lengthInt = 0;
+    };
+
+    // Time Complexity: O(N)
+    // Auxiliary Space Complexity: O(1)
+    void insertValue(ListNode *xNode, int indexInt){
+        if (indexInt < 0 || indexInt > this->lengthInt){
+          return;
+        }
+        if (this->lengthInt == 0){
+            this->headNode = xNode;
+            xNode->nextNode = tailNode;
+            lengthInt++;
+            return;
+        }
+        if (this->lengthInt == 1){
+            this->headNode->nextNode = xNode;
+            xNode->nextNode = this->tailNode;
+            lengthInt++;
+            return;
+        }
+        if (this->lengthInt > 1){
+            ListNode *node = this->headNode;
+            int currentIndexInt = 0;
+            while (node){
+                if (currentIndexInt == lengthInt - 1){
+                    node->nextNode = xNode;
+                    xNode->nextNode = this->tailNode;
+                    lengthInt++;
+                    return;
+                }
+                node = node->nextNode;
+                currentIndexInt++;
+            }
+        }
+    }
+
+    // Time Complexity: O(N)
+    // Auxiliary Space Complexity: O(1)
+    void appendValue(int valueInt){
+      ListNode *xNode = new ListNode(valueInt);
+      this->insertValue(xNode, this->lengthInt);
+    };
+
+    void appendValue(string inputStr){
+      ListNode *xNode = new ListNode(inputStr);
+      this->insertValue(xNode, lengthInt);
+    };
+
+    void appendValue(vector<int> inputVec){
+      ListNode *xNode = new ListNode(inputVec);
+      this->insertValue(xNode, lengthInt);
+    };
+
+    // Time Complexity:O(N)
+    // Auxiliary Space Complexity: O(1)
+    ListNode* deleteNode(int indexInt){
+      if (indexInt < 0 || indexInt >= this->lengthInt){
+        return nullptr;
+      }
+      ListNode *toDeleteNode = nullptr;
+
+      if (indexInt == 0){
+        toDeleteNode = this->headNode;
+        this->headNode = this->headNode->nextNode;
+        if (this->lengthInt == 1){
+          this->tailNode = nullptr;
+        }
+      } else {
+        ListNode *node = this->headNode;
+        int currentIndexInt = 0;
+        while (node){
+          if (currentIndexInt == indexInt - 1){
+            toDeleteNode = node->nextNode;
+            node->nextNode = node->nextNode->nextNode;
+            if (indexInt == this->lengthInt - 1){
+              this->tailNode = node;
+            }
+            break;
+          }
+          node = node->nextNode;
+          currentIndexInt++;
+        }
+      }
+      lengthInt--;
+      return toDeleteNode;
+    };
+
+    // Time Complexity: o(N)
+    // Auxiliary Space Complexity: O(1)
+    bool containsValue(int valueInt){
+      ListNode *node = this->headNode;
+      while (node){
+        if (node->valueInt == valueInt){
+          return true;
+        }
+        node = node->nextNode;
+      }
+      return false;
+    }
+
+    bool containsStr(string inputStr){
+      ListNode *node = this->headNode;
+      while (node){
+        if (node->getStringID() == inputStr){
+          return true;
+        }
+        node = node->nextNode;
+      }
+      return false;
+    }
+
+    int sum(){
+        ListNode *node = this->headNode;
+        int sumInt = 0;
+        while (node){
+            sumInt += node->valueInt;
+            node = node->nextNode;
+        }
+        return sumInt;
+    }
+
+    void printLinkedListInt(){
+        ListNode *node = this->headNode;
+        cout << "Printing out the linked list: " << endl;
+        cout << "{ ";
+        while (node){
+            cout << node->valueInt;
+            node = node->nextNode;
+            if (node){
+              cout << ", ";
+            }
+        }
+        cout << " }" << endl;
+    }
+
+    void printLinkedListStr(){
+      ListNode *node = this->headNode;
+      cout << "Printing out the linked list: " << endl;
+      cout << "{ ";
+      while (node){
+        cout << node->getStringID();
+        node = node->nextNode;
+        if (node){
+          cout << ", ";
+        }
+      }
+      cout << " }" << endl;
+    }
+
+    void printLinkedListVec(){
+      ListNode *node = this->headNode;
+      cout << "Printing out the linked list: " << endl;
+      cout << "{ ";
+      while (node){
+        vector<int> vec = node->getVecIntID();
+        int xInt = 0;
+        while (xInt < vec.size()){
+          cout << vec[xInt];
+          if (xInt < vec.size() - 1 || node->nextNode != nullptr){
+            cout << ", ";
+          }
+          xInt++;
+        }
+        node = node->nextNode;
+      }
+      cout << " }" << endl;
+    };
+};
+
+class Queue {
+    public: 
+        LinkedList *linkedList = new LinkedList();
+        int lengthInt = 0;    
+        
+        void enqueue(int valueInt){
+            this->linkedList->appendValue(valueInt);
+            this->lengthInt++;
+        }
+
+        void enqueue(string inputStr){
+            this->linkedList->appendValue(inputStr);
+            this->lengthInt++;
+        }
+
+        void enqueue(vector<int> inputVec){
+            this->linkedList->appendValue(inputVec);
+            this->lengthInt++;
+        }
+
+        string dequeueStringID(){
+            if (this->lengthInt == 0){
+                return NULL;
+            }
+            this->lengthInt--;
+            return this->linkedList->deleteNode(0)->getStringID();
+        }
+
+        int dequeueIntID(){
+            if (this->lengthInt == 0){
+                return -1;
+            }
+            this->lengthInt--;
+            return this->linkedList->deleteNode(0)->getIntID();
+        }
+
+        vector<int> dequeueVecInt(){
+          if (this->lengthInt == 0){
+            return vector<int>({-1});
+          }
+            this->lengthInt--;
+            return this->linkedList->deleteNode(0)->getVecIntID();
+        }
+        
+        bool isEmpty(){
+            return this->lengthInt == 0;
+        }
+
+        int sum(){
+            return this->linkedList->sum();
+        }
+
+        string peekStringID(){
+            return this->linkedList->headNode->getStringID();
+        }
+
+        int peekIntID(){
+            return this->linkedList->headNode->getIntID();
+        }
+    
+        vector<int> peekVecInt(){
+            return this->linkedList->headNode->getVecIntID();
+        }
+
+        int size(){
+            return this->lengthInt;
+        }
+
+        void printQueueInt() {
+            ListNode* node = this->linkedList->headNode;
+            cout << "Printing out the queue of ints: " << endl;
+            cout << "{ ";
+            while (node) {
+                cout << node->valueInt;
+                if (node->nextNode) {
+                    cout << ", ";
+                }
+                node = node->nextNode;
+            }
+            cout << " }" << endl;
+        }
+
+        void printQueueStr(){
+          ListNode* node = this->linkedList->headNode;
+          cout << "Printing out the queue of strings" << endl;
+          cout << "{ ";
+          while (node){
+            cout << node->getStringID();
+            if (node->nextNode){
+              cout << ", ";
+            }
+            node = node->nextNode;
+          }
+          cout << " }" << endl;
+        }
+        
+        void printVector(const vector<int>& vec){
+            cout << "{ ";
+            int xInt = 0;
+            while (xInt < vec.size()){
+                int valueInt = vec[xInt];
+                if (xInt < vec.size() - 1){
+                    cout << valueInt << ", ";
+                } 
+                if (xInt == vec.size() - 1){
+                    cout << valueInt;
+                }
+                xInt++;
+            }
+            cout << " }" << endl;
+        }
+
+        void printQueueVecInt(){
+          ListNode* node = this->linkedList->headNode;
+          cout << "Printing out the queue of vectors..." << endl;
+          cout << "{ ";
+          while (node){
+            vector<int> vec = node->getVecIntID();
+            printVector(vec);
+            if (node->nextNode){
+              cout << ", ";
+            }
+            node = node->nextNode;
+          }
+          cout << " }" << endl;
+        }
+};
+
+string printToFToString(bool inputToF){
+        if (inputToF == 1){
+            return "true";
+        }
+        if (inputToF == 0){
+            return "false";
+        }
+        return "false";
+};
+
+/* 
+Moving average from data stream
+
+Given a stream of integers and a window size, 
+calculate the moving average of all integers in the sliding window.
+
+Implement the moving average class. 
+
+input:
+[[3], [1], [10], [3], [5]]
+output:
+[null, 1.0, 5.5, 4.66667, 6.0]
+
+class Moving Average{
+    this.size
+    this.nIntext
+}
+
+movingAverage = new MovingAverage(3);
+
+movingAverage.next(1)  // return 1 = (1 / 1)
+movingAverage.next(10) // return 5.5 = ((1 + 10) / 2)
+movingAverage.next(3) // return 4.66667 => ((1 + 10 + 3)/ 3)
+movingAverage.next(5) // return 6.0 => ((10 + 3 + 5) / 3)
+
+- perhaps a deque or queue
+- when new data comes in check if the total 
+  amount of data is greater than size
+- if the new size exceeds the pre-set capacity, 
+ then evict the first item which entered, 
+- this data management policy would be characterized
+ as first in, first out
+*/
+
+class MovingAverage{
+    public: 
+        int sizeInt;
+        int sumInt;
+        int capacityInt;
+        Queue *queue = new Queue();
+
+    MovingAverage(int capacityInt){
+        this->capacityInt = capacityInt;
+    }   
+
+    int next(int intX){
+        this->sizeInt++;
+        this->queue->enqueue(intX);
+        sumInt = this->queue->sum();
+        
+        if (this->sizeInt > this->capacityInt){
+            this->queue->dequeueIntID();
+            this->sizeInt--;
+        }
+        sumInt = this->queue->sum();
+        int movingAverageInt = sumInt / this->sizeInt;
+        return movingAverageInt;
+    }   
+};
+
+int main(){
+
+    Queue queueX;
+    queueX.enqueue(4);
+    int currSumInt = queueX.sum();
+    queueX.enqueue(8);
+    currSumInt = queueX.sum();
+    cout << "Expect 12: " << currSumInt << endl;
+
+
+    MovingAverage movingAverage(3);
+    int outputInt1 = movingAverage.next(1);
+      cout << "Output Int 1: " << outputInt1 << endl;
+
+    int outputInt2 = movingAverage.next(10);
+    int outputInt3 = movingAverage.next(3);
+    int outputInt4 = movingAverage.next(5);
+  
+    cout << "Output Int 2: " << outputInt2 << endl;
+    cout << "Output Int 3: " << outputInt3 << endl;
+    cout << "Output Int 4: " << outputInt4 << endl;
+    return 0;
+}
