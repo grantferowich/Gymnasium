e66ec76f46c1100a427aa3581eb9356979c5cc84
grantferowich <g.ferowich@gmail.com>
2024-03-17 08:16:34 -0500

    longest palindromic subsequence cpp

---
+++ c++journey/dsaDomains/dynamicProgramming/longestPalindromicSubsequence.cpp
@@ -0,0 +1,35 @@
+/* 
+* Longest palindromic subsequence
+
+* The solution considers base cases, and also, the solution moves right and left pointers closer to the center
+* of the string at each recursive call. 
+
+* Time complexity: O(N^2)
+* Space complexity: O(N^2)
+* Input:  "vtvvv"
+* Output: 4
+
+* Longest palindromic subsequence here is "vvvv"
+
+
+* Input:  "pwnnb"
+* Output: 2
+
+* Longest palindromic subsequence here is "nn"
+
+
+* Input:  "ttbtctcbt"
+* Output: 7
+
+* Longest palindromic subsequence here is "tbtctbt"
+
+* input: 'hannah'
+* output: 6
+* input: string
+* output: number
+
+* input: "g"
+* output: 1
+* number is a thing being maximized 
+
+ */
