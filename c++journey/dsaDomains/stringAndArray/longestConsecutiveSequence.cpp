4af0ceae170edf22e22d3cd013370a7a7fa10127
grantferowich <g.ferowich@gmail.com>
2024-03-17 06:38:49 -0500

    longest consecutive sequence c++

--- c++journey/dsaDomains/stringAndArray/longestConsecutiveSequence.cpp
+++ c++journey/dsaDomains/stringAndArray/longestConsecutiveSequence.cpp
@@ -9,12 +9,31 @@ return the length of the longest consecutive
 elements sequence.
 
 */
+#include <unordered_set>
+#include <vector> 
+#include <iostream> 
+using namespace std;
 
-int longestConsecutiveSequence(){
-
+int longestConsecutiveSequence(vector<int> &inputVec){
+    unordered_set<int>setG(inputVec.begin(), inputVec.end());
+    int longestSequenceInt = 0;
+    for (auto &intG: setG){
+        if (!setG.count(intG - 1)){
+            int lengthInt = 1;
+            while (setG.count(intG + lengthInt)){
+                lengthInt++;
+                longestSequenceInt = max(lengthInt, longestSequenceInt);
+            }
+        }
+    }
+    return longestSequenceInt;
 }
 
 int main(){
-
+    vector<int> vec1 = { 100, 4, 200, 1, 3, 2 };
+    // expect the longest consecutive sequence to be 
+    // 4: { 1, 2, 3, 4}
+    int outputInt1 = longestConsecutiveSequence(vec1);
+    cout << "Output Int 1: " << outputInt1 << endl;
     return 0;
 }
