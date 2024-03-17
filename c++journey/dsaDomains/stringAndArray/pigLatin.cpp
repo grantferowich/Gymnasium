63e9fe434d482b9d6af505932d69e512513d4c9e
grantferowich <g.ferowich@gmail.com>
2024-03-17 07:17:21 -0500

    pig latin c++

--- c++journey/dsaDomains/stringAndArray/pigLatin.cpp
+++ c++journey/dsaDomains/stringAndArray/pigLatin.cpp
@@ -16,7 +16,34 @@
 using namespace std;
 
 string pigLatin(string inputStr){
-
+    string vowelsStr = "aeiouy";
+    // words which start with vowel
+    for (char &charG: vowelsStr){
+        // cout << "inputStr..." << inputStr << endl;
+        // cout << "charG.." << charG << endl;
+        // cout << "inputStr[0].." << inputStr[0] << endl;
+        if (charG == inputStr[0]){
+            // add 'yay' to the end
+            return inputStr+"yay";
+        }
+    }
+    // words which start with consonant
+    int intG = 0;
+    while (intG < inputStr.size()){
+        for (char charG: vowelsStr){
+            // found the first vowel
+            if (inputStr[intG] == charG){
+                // chop the string from the start until the letter before the first
+                // consonant
+                string fragmentStr = inputStr.substr(0, intG);
+                // chop the string from the vowel to the end of the string
+                string mainStr = inputStr.substr(intG);
+                // append the fragment to the main string with "ay"
+                return mainStr + fragmentStr + "ay";
+            }
+        }
+        intG++;
+    }
     return "";
 }
 
@@ -26,15 +53,23 @@ int main(){
     string inputStr3 = "eat";
     string inputStr4 = "banana";
     string inputStr5 = "trash";
+    string inputStr6 = "yellow";
+    string inputStr7 = "why";
+
     string outputStr1 = pigLatin(inputStr1);
     string outputStr2 = pigLatin(inputStr2);
     string outputStr3 = pigLatin(inputStr3);
     string outputStr4 = pigLatin(inputStr4);
     string outputStr5 = pigLatin(inputStr5);
+    string outputStr6 = pigLatin(inputStr6);
+    string outputStr7 = pigLatin(inputStr7);
+
     cout << "Output str 1: " << outputStr1 << endl;
     cout << "Output str 2: " << outputStr2 << endl;
-    cout << "Output str 3: " << outputStr2 << endl;
-    cout << "Output str 4: " << outputStr2 << endl;
-    cout << "Output str 5: " << outputStr2 << endl;
+    cout << "Output str 3: " << outputStr3 << endl;
+    cout << "Output str 4: " << outputStr4 << endl;
+    cout << "Output str 5: " << outputStr5 << endl;
+    cout << "Output str 6: " << outputStr6 << endl;
+    cout << "Output str 7: " << outputStr7 << endl;
     return 0;
 }
